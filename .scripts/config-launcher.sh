1617e50926c7db271b378040ca72b47fef017b11
Eugene Shtoka <eshtoka@gmail.com>
2024-01-02 14:28:37 +0200

    update

--- .scripts/config-launcher.sh
+++ .scripts/config-launcher.sh
@@ -10,6 +10,7 @@ sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=File Manager" "$APPS_PATH/thuna
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=VS Code" "$APPS_PATH/code.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=DBeaver" "$APPS_PATH/io.dbeaver.DBeaver.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Chrome" "$APPS_PATH/google-chrome.desktop"
+
 sudo $SCRIPTS_PATH/replace-line.sh "theme='style-1'" "theme='style-2'" "$HOME/.config/rofi/launchers/type-2/launcher.sh"      
 
 sudo mv "$APPS_PATH/nm-connection-editor.desktop" $HIDDEN
