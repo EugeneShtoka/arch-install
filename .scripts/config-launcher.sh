6eb3392f46cdbd48a1fe5d9ae4ac912bec391153
Eugene Shtoka <eshtoka@gmail.com>
2024-01-02 14:27:58 +0200

    update

--- .scripts/config-launcher.sh
+++ .scripts/config-launcher.sh
@@ -18,4 +18,4 @@ sudo mv "$APPS_PATH/thunar-settings.desktop" $HIDDEN
 sudo mv "$APPS_PATH/lstopo.desktop" $HIDDEN
 sudo mv "$APPS_PATH/vim.desktop" $HIDDEN
 sudo mv "$APPS_PATH/rofi.desktop" $HIDDEN
-sudo mv "$APPS_PATH/vim.desktop" $HIDDEN
+sudo mv "$APPS_PATH/rofi-theme-selector.desktop" $HIDDEN
