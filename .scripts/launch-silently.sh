f36c27806abef45a50a2404e5b538e7771b1f7d1
Eugene Shtoka <eshtoka@gmail.com>
2024-01-04 21:00:14 +0200

    update

--- .scripts/launch-silently.sh
+++ .scripts/launch-silently.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "`date` launch-silently $1 $2" >> $HOME/scripts.log
-hyprctl dispatch -- exec '[title;workspace 2 silent]' $2
+hyprctl dispatch -- exec '[title;workspace 2 silent]' slack
