4af2f9abec6352bbfe660ff76cc95fb342d42908
Eugene Shtoka <eshtoka@gmail.com>
2024-01-06 16:34:34 +0200

    update

--- .scripts/config-install.sh
+++ .scripts/config-install.sh
@@ -8,12 +8,12 @@ $SCRIPTS_PATH/config-launcher.sh
 rclone config
 
 $SCRIPTS_PATH/git-set-up.sh
-$SCRIPTS_PATH/generate-ssh-key.sh $PERSONAL_EMAIL personal https://github.com/settings/ssh/new
+$SCRIPTS_PATH/generate-ssh-key.sh $PERSONAL_EMAIL personal
 gh auth login
 git remote set-url origin git@github.com:EugeneShtoka/arch-install.git
 git clone $OBSIDIAN_GIT_REPO $OBSIDIAN_PATH
 
-$SCRIPTS_PATH/generate-ssh-key.sh $WORK_EMAIL work https://gitlab.com/-/profile/keys
+$SCRIPTS_PATH/generate-ssh-key.sh $WORK_EMAIL work
 glab auth login
 read -p "Enter ssh key name for GitLab: " keyName
 glab ssh-key add .ssh/id_ed25519_work.pub -t $keyName 
