2344d7d1eb270c11808c20a779005a92c313bdc3
Eugene Shtoka <eshtoka@gmail.com>
2024-01-08 13:32:59 +0200

    update

--- .scripts/init-workspaces.sh
+++ .scripts/init-workspaces.sh
@@ -5,7 +5,6 @@ hyprctl dispatch moveworkspacetomonitor 4 DP-3
 hyprctl dispatch moveworkspacetomonitor 5 DP-3
 hyprctl dispatch moveworkspacetomonitor 6 DP-3
 hyprctl dispatch moveworkspacetomonitor 7 DP-3
-hyprctl dispatch moveworkspacetomonitor 8 DP-4
 
 hyprctl dispatch moveworkspacetomonitor 0 DP-4
 hyprctl dispatch moveworkspacetomonitor 9 DP-4
