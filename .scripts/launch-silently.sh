535508f8bf744192860959f4fcdc1cd551c784b0
Eugene Shtoka <eshtoka@gmail.com>
2024-01-08 13:43:48 +0200

    update

--- .scripts/launch-silently.sh
+++ .scripts/launch-silently.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#echo "`date` launch-silently $1 $2" >> $HOME/scripts.log
+#echo "`date` launch-silently $1 $2" >> $HOME/.scripts.log
 hyprctl dispatch -- exec [workspace $1 silent] $2
