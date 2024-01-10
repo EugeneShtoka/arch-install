a9232c4931006a69b240cc8b220befdfc75f7f26
Eugene Shtoka <eshtoka@gmail.com>
2024-01-10 09:45:53 +0200

    update

--- .scripts/replace-line.sh
+++ .scripts/replace-line.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo $LOG_PATH ${LOG_PATH} "${LOG_PATH}
+echo "" $LOG_PATH ${LOG_PATH} "${LOG_PATH}
 echo "replace line" > ~/script.log
 echo $LOG_PATH ${LOG_PATH} "${LOG_PATH} > ~/script.log
 
