fd7a2e3ad9c82fff00e53d6b497033bd3eab9640
Eugene Shtoka <eshtoka@gmail.com>
2024-01-11 00:36:22 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -12,3 +12,10 @@ hyprctl dispatch exec '[workspace 2 silent]' code
 
 
 hyprctl dispatch movetoworkspacesilent 1, beeper
+hyprctl dispatch movetoworkspacesilent 1, beeper
+
+hyprctl dispatch movetoworkspacesilent 1, beeper
+hyprctl dispatch movetoworkspacesilent 1, beeper
+
+hyprctl dispatch movetoworkspacesilent 1, beeper
+hyprctl dispatch movetoworkspacesilent 1, beeper
