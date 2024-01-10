54ea5a2c0668c9fd345c6fb1b23859d62aa9c747
Eugene Shtoka <eshtoka@gmail.com>
2024-01-11 00:34:20 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -8,6 +8,7 @@ hyprctl dispatch exec '[workspace 2 silent]' slack
 hyprctl dispatch exec '[workspace 2 silent]' slack
 
 hyprctl dispatch exec '[workspace 2 silent]' slack
+hyprctl dispatch exec '[workspace 2 silent]' slack
 
 
 hyprctl dispatch movetoworkspacesilent 2,zoom  
