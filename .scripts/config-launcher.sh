3383997f2b6271c33aca09f9d85654688ec1bbc1
Eugene Shtoka <eshtoka@gmail.com>
2024-01-11 00:20:41 +0200

    update

--- .scripts/config-launcher.sh
+++ .scripts/config-launcher.sh
@@ -15,11 +15,11 @@ sudo $SCRIPTS_PATH/replace-line.sh "Exec=" "Exec=mailspring --password-store='gn
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=File Manager" "$APPS_PATH/thunar.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=VS Code" "$APPS_PATH/code.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=DBeaver" "$APPS_PATH/io.dbeaver.DBeaver.desktop"
-sudo $SCRIPTS_PATH/replace-line.sh "Exec=" "Exec=$SCRIPTS_PATH/" "$APPS_PATH/io.dbeaver.DBeaver.desktop"
+sudo $SCRIPTS_PATH/replace-line.sh "Exec=" "Exec=$SCRIPTS_PATH/start-dbe" "$APPS_PATH/io.dbeaver.DBeaver.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Chrome" "$APPS_PATH/google-chrome.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Docker" "$APPS_PATH/docker-desktop.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Torrent" "$APPS_PATH/org.qbittorrent.qBittorrent.desktop"
-sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Steam" "$APPS_PATH/steam.desktop"
+sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Steam" "$aver.sAPPS_PATH/steam.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Together" "$APPS_LOCAL_PATH/dont-starve-together.desktop"
 
 sudo mv "$APPS_PATH/nm-connection-editor.desktop" $HIDDEN
