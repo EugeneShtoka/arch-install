10be5f369016d00a861bb2ac76d2f834be8ad48d
Eugene Shtoka <eshtoka@gmail.com>
2024-01-11 00:34:26 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo "`date` work-setup" >> $LOG_PATH
-hyprctl dispatch exec '[workspace 2 silent]' slack
+hyprctl dispatch exec '[workspace 2 silent]' beeper
 hyprctl dispatch exec '[workspace 2 silent]' slack
 
 hyprctl dispatch exec '[workspace 2 silent]' slack
