d83b703c41164bcc65e7f047a095964898464cca
Eugene Shtoka <eshtoka@gmail.com>
2024-01-11 10:52:49 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #echo "`date` work-setup" >> $LOG_PATH
-hyprctl dispatch exec '[workspace 1 silent]' beeper
 $SCRIPTS_PATH/mailspring-start.sh
+hyprctl dispatch exec '[workspace 1 silent]' beeper
 
 hyprctl dispatch exec '[workspace 2 silent]' zoom
 hyprctl dispatch exec '[workspace 2 silent]' slack
@@ -10,7 +10,7 @@ hyprctl dispatch exec '[workspace 2 silent]' slack
 hyprctl dispatch exec '[workspace 4 silent]' google-chrome-stable
 hyprctl dispatch exec '[workspace 4 silent]' code
 
-sleep 2
+sleep 5
 
 hyprctl dispatch movetoworkspacesilent 1, Beeper
 hyprctl dispatch movetoworkspacesilent 1, mailspring
@@ -18,5 +18,5 @@ hyprctl dispatch movetoworkspacesilent 1, mailspring
 hyprctl dispatch movetoworkspacesilent 2, zoom
 hyprctl dispatch movetoworkspacesilent 2, Slack
 
-hyprctl dispatch movetoworkspacesilent 4, google-chrome-stable
+hyprctl dispatch movetoworkspacesilent 4, Google-chrome
 hyprctl dispatch movetoworkspacesilent 4, Code
