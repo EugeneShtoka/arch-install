51547cde466245bccfa8ac7005d307aa1e4ba3f1
Eugene Shtoka <eshtoka@gmail.com>
2024-01-14 00:14:02 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -3,12 +3,11 @@
 #echo "`date` work-setup" >> $LOG_PATH
 hyprctl dispatch workspace 4
 hyprctl dispatch -- exec $SCRIPTS_PATH/mailspring-start.sh
+hyprctl dispatch -- exec $SCRIPTS_PATH/obsidian-start.sh
 hyprctl dispatch -- exec beeper
-hyprctl dispatch -- exec zoom
 hyprctl dispatch -- exec vivaldi-snapshot
 hyprctl dispatch -- exec code
-hyprctl dispatch -- exec $SCRIPTS_PATH/obsidian-start.sh
-
+hyprctl dispatch -- exec zoom
 sleep 5
 hyprctl dispatch -- exec slack
 
