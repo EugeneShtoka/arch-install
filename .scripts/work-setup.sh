d1a2c7cd95fc97d8447af0349cca44b77e4f7b29
Eugene Shtoka <eshtoka@gmail.com>
2024-01-13 22:46:52 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -8,15 +8,16 @@ hyprctl dispatch -- exec zoom
 hyprctl dispatch -- exec slack
 hyprctl dispatch -- exec vivaldi-snapshot
 hyprctl dispatch -- exec code
-sleep 5
 
-hyprctl dispatch movetoworkspacesilent "1,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Mailspring") | .address')"
-hyprctl dispatch movetoworkspacesilent "1,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Beeper") | .address')"
-hyprctl dispatch movetoworkspacesilent "2,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "zoom") | .address')"
-hyprctl dispatch movetoworkspacesilent "2,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Slack") | .address')"
-hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Vivaldi-snapshot") | .address')"
-hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Code") | .address')"
 hyprctl dispatch workspace 4
 
+for i in {1..7}; do
+    sleep 1
 
-
+    hyprctl dispatch movetoworkspacesilent "1,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Mailspring") | .address')"
+    hyprctl dispatch movetoworkspacesilent "1,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Beeper") | .address')"
+    hyprctl dispatch movetoworkspacesilent "2,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "zoom") | .address')"
+    hyprctl dispatch movetoworkspacesilent "2,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Slack") | .address')"
+    hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Vivaldi-snapshot") | .address')"
+    hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Code") | .address')"
+done
