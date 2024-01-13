bb0027c84a9be32c84730d99f241966ec1ec9574
Eugene Shtoka <eshtoka@gmail.com>
2024-01-13 02:09:47 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -12,6 +12,5 @@ hyprctl dispatch -- exec '[title;workspace 4 silent]' code
 
 sleep 5
 
-hyprctl dispatch movetoworkspacesilent "2,address:0x5610b819b6e0"
-hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq '.[] | select(.class == "Code") | .address')"
+hyprctl dispatch movetoworkspacesilent "2,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "zoom") | .address')"
 hyprctl dispatch movetoworkspacesilent "4,address:$(hyprctl clients -j | jq -r '.[] | select(.class == "Code") | .address')"
