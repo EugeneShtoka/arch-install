dc1bc406da09fd06a8e73047f13433be1dfefc09
Eugene Shtoka <eshtoka@gmail.com>
2024-01-14 00:19:10 +0200

    update

--- .scripts/work-setup.sh
+++ .scripts/work-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 #echo "`date` work-setup" >> $LOG_PATH
-hyprctl dispatch workspace 4
+hyprctl dispatch workspace 
 hyprctl dispatch -- exec $SCRIPTS_PATH/mailspring-start.sh
 hyprctl dispatch -- exec $SCRIPTS_PATH/obsidian-start.sh
-hyprctl dispatch -- exec beeper
 hyprctl dispatch -- exec vivaldi-snapshot
+hyprctl dispatch -- exec beeper
 hyprctl dispatch -- exec code
 hyprctl dispatch -- exec zoom
 sleep 5
