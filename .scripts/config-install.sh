a0d3effbd2bc8402a0362b68815bb085606814dc
Eugene Shtoka <eshtoka@gmail.com>
2024-01-20 15:26:08 +0200

    update

--- .scripts/config-install.sh
+++ .scripts/config-install.sh
@@ -17,3 +17,5 @@ $SCRIPTS_PATH/generate-ssh-key.sh $WORK_EMAIL work
 glab auth login
 read -p "Enter ssh key name for GitLab: " keyName
 glab ssh-key add .ssh/id_ed25519_work.pub -t $keyName 
+
+gsettings set org.gnome.desktop.interface gtk-theme Pop
