a5cc17250c285d7e2e834e10c31ebe8b5d319876
Eugene Shtoka <eshtoka@gmail.com>
2024-01-20 15:22:07 +0200

    update

--- .scripts/auto-sync-scripts.sh
+++ .scripts/auto-sync-scripts.sh
@@ -3,6 +3,6 @@
 #echo "`date` auto-sync-scripts" >> $LOG_PATH
 inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.config/hypr/hyprland.conf $HOME/.config/systemd/user/start-up-routine.service $HOME/.gitignore $ZSHFN_PATH $SCRIPTS_PATH | while read DIR EVENT FILE
 do
-    echo "`date` auto-sync-scripts $EVENT on $DIR$FILE" >> $HOME/scripts.log
+    echo "`date` auto-sync-scripts $EVENT on $DIR$FILE" >> $SCRIPTS_PATH
     $SCRIPTS_PATH/custom-scripts-push.sh
 done
