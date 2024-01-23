259198b0f4ad1169ff46fcd6e82d4aa95326046b
Eugene Shtoka <eshtoka@gmail.com>
2024-01-23 10:52:38 +0200

    update

--- .scripts/auto-sync-scripts.sh
+++ .scripts/auto-sync-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo "`date` auto-sync-scripts" >> $LOG_PATH
-inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.config/i3 $HOME/.config/autokey $HOME/.config/systemd/user/start-up-routine.service $HOME/.gitignore $ZSHFN_PATH $SCRIPTS_PATH | while read DIR EVENT FILE
+inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.config/i3 $HOME/.config/autokey $HOME/.config/systemd/user/start-up-routine.service $HOME/.gitignore $ZSHFN_PATH $SCRIPTS_PATH $SERVICES_PATH | while read DIR EVENT FILE
 do
     echo "`date` auto-sync-scripts $EVENT on $DIR$FILE" >> $SCRIPTS_PATH
     $SCRIPTS_PATH/custom-scripts-push.sh
