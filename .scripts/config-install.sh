0277ebb59f53856a90c728dd8c3941f8bbdcb291
Eugene Shtoka <eshtoka@gmail.com>
2024-01-23 10:54:21 +0200

    update

--- .scripts/config-install.sh
+++ .scripts/config-install.sh
@@ -2,6 +2,7 @@
 
 cd ~
 
+sudo ln -s $SERVICES_PATH/org.freedesktop.Notifications.service /usr/share/dbus-1/services/org.freedesktop.Notifications.service
 $SCRIPTS_PATH/rofi-theme-install.sh
 $SCRIPTS_PATH/config-launcher.sh
 
