c8c42bbf1940204cd27574c3f826acd845724281
Eugene Shtoka <eshtoka@gmail.com>
2024-01-23 12:46:57 +0200

    update

--- .scripts/config-launcher.sh
+++ .scripts/config-launcher.sh
@@ -22,6 +22,7 @@ sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Torrent" "$APPS_PATH/org.qbitto
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Messenger" "$APPS_PATH/beeper.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Notes" "$APPS_PATH/obsidian.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Slack" "$APPS_PATH/slack.desktop"
+sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Keyboard Shortcuts" "$APPS_PATH/autokey-gtk.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Steam" "$APPS_PATH/steam.desktop"
 sudo $SCRIPTS_PATH/replace-line.sh "Name=" "Name=Together" "$APPS_LOCAL_PATH/dont-starve-together.desktop"
 
