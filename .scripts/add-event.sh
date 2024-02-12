35d7e28a35173f9b07fcd6c52a5379fa9b2b4628
Eugene Shtoka <eshtoka@gmail.com>
2024-02-12 17:20:00 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -1,10 +1,10 @@
 GEMINI_API_KEY=$(bw get item 'Gemini API Key' | jq '.notes' |  tr -d \")
 
-BODY='{"contents": [{"parts":[{"text": "Translate '$1' to english"}]}]}'
+BODY=$1
 
 echo $BODY
 
 curl https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$GEMINI_API_KEY \
     -H 'Content-Type: application/json' \
     -X POST \
-    -d '{"contents": [{"parts":[{"text": "Translate '$1' to english"}]}]}' 2> /dev/null
+    -d '{"contents": [{"parts":[{"text": "Translate '$BODY' to english"}]}]}' 2> /dev/null
