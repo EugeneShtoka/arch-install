df1553a33dae49aaeaea8177ce9c8142d5c5dd29
Eugene Shtoka <eshtoka@gmail.com>
2024-02-13 00:41:06 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -32,13 +32,6 @@ json_data=$(curl https://generativelanguage.googleapis.com/v1beta/models/gemini-
     -X POST \
     -d "$BODY" 2> /dev/null | jq '.candidates[].content.parts[0].text' | jq 'fromjson')
 
-json_data='{
-  "SUMMARY": "Thyroid Ultrasound",
-  "LOCATION": "96 Yigal Alon St. Building C, Tel Aviv, 3rd Floor",
-  "DTSTART": "2024-02-22T16:00:00Z",
-  "DTEND": "2024-02-22T17:30:00Z"
-}'
-
 summary=$(echo "$json_data" | jq -r '.SUMMARY')
 location=$(echo "$json_data" | jq -r '.LOCATION')
 dtstart=$(echo "$json_data" | jq -r '.DTSTART')
