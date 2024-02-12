abbf565704235364903bb033e5bc029c71e61061
Eugene Shtoka <eshtoka@gmail.com>
2024-02-12 16:53:55 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -1,1 +1,11 @@
+GEMINI_API_KEY=$(bw get item 'Gemini API Key' | jq '.notes' |  tr -d \")
+
 echo $1
+
+curl https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$API_KEY \
+    -H 'Content-Type: application/json' \
+    -X POST \
+    -d '{
+      "contents": [{
+        "parts":[{
+          "text": "Write a story about a magic backpack."}]}]}' 2> /dev/null
