39a74d264a454e8931b695c9e7644c06f7759e74
Eugene Shtoka <eshtoka@gmail.com>
2024-02-12 17:16:19 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -2,7 +2,7 @@ GEMINI_API_KEY=$(bw get item 'Gemini API Key' | jq '.notes' |  tr -d \")
 
 CONTENT="Translate $1 to english"
 
-BODY='{"contents": [{"parts":[{"text": $(CONTENT)}]}]}'
+echo '{"contents": [{"parts":[{"text": ' + $CONTENT + '}]}]}'
 
 echo $BODY
 
