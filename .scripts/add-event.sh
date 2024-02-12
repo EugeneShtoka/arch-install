016328b15a2e4d396532492448ed1cc9ae2b77ef
Eugene Shtoka <eshtoka@gmail.com>
2024-02-13 00:37:02 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -13,7 +13,8 @@ END:VCALENDAR'
 
 GEMINI_API_KEY=$(bw get item 'Gemini API Key' | jq '.notes' |  tr -d \")
 
-BODY='{"contents": [{"parts":[{"text": "today date"}]}], "model": "gemini-ultra"}'
+BODY='{"contents": [{"parts":[{"text": "today 'date' what is tomorrow date"}]}], "model": "gemini-ultra"}'
+
 echo $BODY
 
 json_data=$(curl https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$GEMINI_API_KEY \
