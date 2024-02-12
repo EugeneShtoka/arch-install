2caa6280cbd58775b16519cff25240b223cb2e3c
Eugene Shtoka <eshtoka@gmail.com>
2024-02-12 17:18:46 +0200

    update

--- .scripts/add-event.sh
+++ .scripts/add-event.sh
@@ -1,8 +1,6 @@
 GEMINI_API_KEY=$(bw get item 'Gemini API Key' | jq '.notes' |  tr -d \")
 
-CONTENT="Translate $1 to english"
-
-BODY='{"contents": [{"parts":[{"text": "'$CONTENT'"}]}]}'
+BODY='{"contents": [{"parts":[{"text": "Translate '$CONTENT' to english"}]}]}'
 
 echo $BODY
 
