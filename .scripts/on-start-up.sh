8bd60d294cf0e2080be2a49a1286f6ec88522133
Eugene Shtoka <eshtoka@gmail.com>
2024-02-19 14:11:11 +0200

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -5,6 +5,7 @@ eval "$(ssh-agent)"
 $SCRIPTS_PATH/ssh-add-keys.sh
 $SCRIPTS_PATH/custom-scripts-pull.sh
 $SCRIPTS_PATH/library-sync-from-gdrive.sh
+$SCRIPTS_PATH/launcher-config.sh
 sleep 10
 $SCRIPTS_PATH/custom-scripts-auto-sync.sh
 $SCRIPTS_PATH/library-auto-sync.sh
