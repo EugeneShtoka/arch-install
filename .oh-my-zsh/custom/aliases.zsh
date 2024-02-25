0b082daf8b422bb53072912f88fef2caca8b4041
Eugene Shtoka <eshtoka@gmail.com>
2024-02-25 23:49:51 +0200

    update

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,10 +1,13 @@
 #!/bin/zsh
 
-alias gmn='setsid vivaldi-snapshot https://gemini.google.com/app &>/dev/null'
-alias ytb='setsid vivaldi-snapshot https://www.youtube.com &>/dev/null'
-alias ghb='setsid vivaldi-snapshot https://github.com &>/dev/null'
-alias glb='setsid vivaldi-snapshot https://gitlab.com &>/dev/null'
+alias gmn='setsid vivaldi-snapshot "https://gemini.google.com/app" &>/dev/null'
+alias ytb='setsid vivaldi-snapshot "https://www.youtube.com" &>/dev/null'
+alias ghb='setsid vivaldi-snapshot "https://github.com" &>/dev/null'
+alias glb='setsid vivaldi-snapshot "https://gitlab.com" &>/dev/null'
 alias gcp='setsid vivaldi-snapshot "https://console.cloud.google.com/welcome?pli=1&project=swapp-v1-1564402864804&authuser=1" &>/dev/null'
+alias gdp='setsid vivaldi-snapshot "https://drive.google.com/drive/u/0/my-drive" &>/dev/null'
+alias gdw='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-drive" &>/dev/null'
+alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/software/c/projects/SWP/boards/1?assignee=712020%3Aee767fac-fcf2-4a03-a591-ccc8a59a097b" &>/dev/null'
 
 alias qbt='setsid qbittorrent &>/dev/null'
 alias obs='setsid obsidian &>/dev/null'
@@ -28,6 +31,7 @@ alias postal-code='source $SCRIPTS_PATH/postal-code-get.sh'
 
 alias gsm='git switch main'
 alias grf='git checkout main --'
+alias gsp='git switch -'
 alias gpc=$SCRIPTS_PATH/git-push-commit.sh
 alias gcpr=$SCRIPTS_PATH/git-create-pull-request.sh
 alias gcmr=$SCRIPTS_PATH/git-create-merge-request.sh
