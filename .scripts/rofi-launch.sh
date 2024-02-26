2e6e81c7b9a7c97fbf95b9052a33890c09f2590c
Eugene Shtoka <eshtoka@gmail.com>
2024-02-26 18:31:26 +0200

    update

--- .scripts/rofi-launch.sh
+++ .scripts/rofi-launch.sh
@@ -7,4 +7,4 @@ theme='style-10'
 rofi \
     -show drun \
     -theme ${dir}/${theme}.rasi \
-    -run-list-command ". $SCRIPTS_PATH/list-functions.sh" -run-command "/bin/zsh -i -c '{cmd}'" -rnow
+    -run-list-command ". $SCRIPTS_PATH/functions-list.sh" -run-command "/bin/zsh -i -c '{cmd}'" -rnow
