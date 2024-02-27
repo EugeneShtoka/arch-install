4827559d1686d8b75e9df7d3e6e0f1e36ca7bf3c
Eugene Shtoka <eshtoka@gmail.com>
2024-02-28 01:11:05 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -32,6 +32,6 @@ alias wdr='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-drive"
 alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/software/c/projects/SWP/boards/1?assignee=712020%3Aee767fac-fcf2-4a03-a591-ccc8a59a097b" &>/dev/null'
 
 alias spnd='systemctl suspend'
-alias stdn='shutdown -now'
+alias stdn='shutdown now'
 alias test5=$SCRIPTS_PATH/rofi-launch.sh
 
