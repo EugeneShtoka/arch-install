e8a6abd4706966d9b8a62a247934ae80c1af3601
Eugene Shtoka <eshtoka@gmail.com>
2024-02-27 15:53:14 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -6,7 +6,7 @@ alias ghb='setsid vivaldi-snapshot "https://github.com" &>/dev/null'
 alias glb='setsid vivaldi-snapshot "https://gitlab.com" &>/dev/null'
 alias gcp='setsid vivaldi-snapshot "https://console.cloud.google.com/welcome?pli=1&project=swapp-v1-1564402864804&authuser=1" &>/dev/null'
 alias gdp='setsid vivaldi-snapshot "https://drive.google.com/drive/u/0/my-drive" &>/dev/null'
-alias gdw='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-drive" &>/dev/null'
+alias gdw='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-=drive" &>/dev/null'
 alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/software/c/projects/SWP/boards/1?assignee=712020%3Aee767fac-fcf2-4a03-a591-ccc8a59a097b" &>/dev/null'
 
 alias torrent='setsid qbittorrent &>/dev/null'
