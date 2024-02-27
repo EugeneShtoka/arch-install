f50ed4f999ea35f8dc6a379dfef0f8450ba62734
Eugene Shtoka <eshtoka@gmail.com>
2024-02-28 01:01:49 +0200

    update

--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -6,13 +6,13 @@ volumeLevel="60%"
 blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
 if ([[ "$blStatus" == *"Device $HEADPHONES_MAC_ADDR not available"* ]]); then
     notify-send "Headphones not available" "Volume: $volumeLevel" -i volume-high  
-fi
-
-isConnected=$(echo $blStatus | grep Connected | awk '{print $2}')
-if [[ "$bl-status" == "yes" ]]; then
-    echo $(bluetoothctl disconnect $HEADPHONES_MAC_ADDR)
-    notify-send "Headphones disconnected" "Volume: $volumeLevel" -i headphones-disconnect 
 else
-    echo $(bluetoothctl connect $HEADPHONES_MAC_ADDR)
-    notify-send "Headphones connected" "Volume: $volumeLevel" -i headphones    
+    isConnected=$(echo $blStatus | grep Connected | awk '{print $2}')
+    if [[ "$bl-status" == "yes" ]]; then
+        echo $(bluetoothctl disconnect $HEADPHONES_MAC_ADDR)
+        notify-send "Headphones disconnected" "Volume: $volumeLevel" -i headphones-disconnect 
+    else
+        echo $(bluetoothctl connect $HEADPHONES_MAC_ADDR)
+        notify-send "Headphones connected" "Volume: $volumeLevel" -i headphones    
+    fi
 fi
