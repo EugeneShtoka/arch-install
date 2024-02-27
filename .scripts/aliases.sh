7df5f85fcc0762565819ed36872a5ce0b63a78d4
Eugene Shtoka <eshtoka@gmail.com>
2024-02-27 20:29:29 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,14 +1,5 @@
 #!/bin/zsh
 
-alias gmn='setsid vivaldi-snapshot "https://gemini.google.com/app" &>/dev/null'
-alias ytb='setsid vivaldi-snapshot "https://www.youtube.com" &>/dev/null'
-alias ghb='setsid vivaldi-snapshot "https://github.com" &>/dev/null'
-alias glb='setsid vivaldi-snapshot "https://gitlab.com" &>/dev/null'
-alias gcp='setsid vivaldi-snapshot "https://console.cloud.google.com/welcome?pli=1&project=swapp-v1-1564402864804&authuser=1" &>/dev/null'
-alias gdp='setsid vivaldi-snapshot "https://drive.google.com/drive/u/0/my-drive" &>/dev/null'
-alias gdw='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-drive" &>/dev/null'
-alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/software/c/projects/SWP/boards/1?assignee=712020%3Aee767fac-fcf2-4a03-a591-ccc8a59a097b" &>/dev/null'
-
 alias torrent='setsid qbittorrent &>/dev/null'
 alias notes='setsid obsidian &>/dev/null'
 alias db-tool='setsid $SCRIPTS_PATH/dbeaver-start.sh &>/dev/null'
@@ -29,6 +20,16 @@ alias monitors-init=$SCRIPTS_PATH/monitors-init.sh
 alias play=$SCRIPTS_PATH/music-play.sh
 alias stop=$SCRIPTS_PATH/music-stop.sh
 alias zrc=$SCRIPTS_PATH/zsh-reload-config.sh
+
+alias gmn='setsid vivaldi-snapshot "https://gemini.google.com/app" &>/dev/null'
+alias ytb='setsid vivaldi-snapshot "https://www.youtube.com" &>/dev/null'
+alias ghb='setsid vivaldi-snapshot "https://github.com" &>/dev/null'
+alias glb='setsid vivaldi-snapshot "https://gitlab.com" &>/dev/null'
+alias gcp='setsid vivaldi-snapshot "https://console.cloud.google.com/welcome?pli=1&project=swapp-v1-1564402864804&authuser=1" &>/dev/null'
+alias gdp='setsid vivaldi-snapshot "https://drive.google.com/drive/u/0/my-drive" &>/dev/null'
+alias gdw='setsid vivaldi-snapshot "https://drive.google.com/drive/u/1/my-drive" &>/dev/null'
+alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/software/c/projects/SWP/boards/1?assignee=712020%3Aee767fac-fcf2-4a03-a591-ccc8a59a097b" &>/dev/null'
+
 
 alias spnd='systemctl suspend'
 
