11fcb983c230b576a6cddd2fe7fa54e174b15954
Eugene Shtoka <eshtoka@gmail.com>
2024-02-27 16:27:41 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -20,26 +20,7 @@ alias daily=$SCRIPTS_PATH/zoom-join-daily.sh
 alias monitors-init=$SCRIPTS_PATH/monitors-init.sh
 alias play=$SCRIPTS_PATH/music-play.sh
 alias stop=$SCRIPTS_PATH/music-stop.sh
-alias yayi=$SCRIPTS_PATH/auto-yay.sh
 alias zrc=$SCRIPTS_PATH/zsh-reload-config.sh
 
-alias bw-unlock='source $SCRIPTS_PATH/bw-unlock.sh'
-alias bw-create='source $SCRIPTS_PATH/bw-create.sh'
-alias bw-item='source $SCRIPTS_PATH/bw-item.sh'
-alias bw-pass='source $SCRIPTS_PATH/bw-unlock.sh && $SCRIPTS_PATH/bw-pass.sh'
-alias postal-code='source $SCRIPTS_PATH/postal-code-get.sh'
-
-alias gsm='git switch main'
-alias grf='git checkout main --'
-alias gsp='git switch -'
-alias gpc=$SCRIPTS_PATH/git-push-commit.sh
-alias gcpr=$SCRIPTS_PATH/git-create-pull-request.sh
-alias gcmr=$SCRIPTS_PATH/git-create-merge-request.sh
-
-alias ls='eza --icons -a'
-alias ll='eza --icons -al'
-alias copy='xclip -selection c'
-alias paste='xclip -out -selection c'
-alias yayr='sudo pacman -R'
 alias spnd='systemctl suspend'
 alias stdn='shutdown now'
