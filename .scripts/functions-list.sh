37a5b2f68f809de6ecb22ede6ad0b8eed76a6675
Eugene Shtoka <eshtoka@gmail.com>
2024-02-27 19:21:30 +0200

    update

--- .scripts/functions-list.sh
+++ .scripts/functions-list.sh
@@ -4,7 +4,7 @@
 #alias | awk -F'[ =]' '{print $1}'
 
 # fetch all zsh functions
-#print -l ${(ok)functions}
+print -l ${(ok)functions}
 
 # fetch user defineed aliases
 grep -oP '(?<=alias )[a-z,-]*(?==)' .scripts/aliases.sh
