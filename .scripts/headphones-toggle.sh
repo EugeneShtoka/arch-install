cd90cab89b635445a408e6e8177d11f28c810468
Eugene Shtoka <eshtoka@gmail.com>
2024-02-28 00:40:06 +0200

    update

--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -4,6 +4,7 @@ echo "`date` switch-headphones" >> $LOG_PATH
 
 blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR | grep Connected | awk '{print $2}')
 echo satus $blStatus
+
 if [[ "$bl-status" == "yes" ]]; then
     echo $(bluetoothctl disconnect $HEADPHONES_MAC_ADDR)
 else
