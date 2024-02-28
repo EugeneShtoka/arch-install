2c7812390c986593d4e5e33c384fe411dfd68c3e
Eugene Shtoka <eshtoka@gmail.com>
2024-02-28 19:32:14 +0200

    update

---
+++ .scripts/get-battery-icon.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Battery icon, depending on battery level
+get_battery_icon() {
+  if (( $1 >= 80 )); then
+    printf "\uf240\n"
+  elif (( $1 >= 60 )); then
+    printf "\uf241\n"
+  elif (( $1 >= 40 )); then
+    printf "\uf242\n"
+  elif (( $1 >= 20 )); then 
+   printf "\uf243\n"
+  else
+    printf "\uf244\n"
+  fi
+}
--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -1,18 +1,33 @@
 #!/bin/zsh
 
+# Volume information (assumes PulseAudio)
+source $SCRIPTS_PATH/get-volume-level.sh
+source $SCRIPTS_PATH/get-battery-icon.sh
+
 echo "`date` switch-headphones" >> $LOG_PATH
-volumeLevel="60%"
+volumeLevel="$(printf '\uf028\n') $(get-volume-level)%"
 
 blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
+
 if ([[ "$blStatus" == *"Device $HEADPHONES_MAC_ADDR not available"* ]]); then
     notify-send "Headphones not available" "Volume: $volumeLevel" -i volume-high  
 else
     isConnected=$(echo $blStatus | grep Connected | awk '{print $2}')
-    if [[ "$bl-status" == "yes" ]]; then
+
+    if [[ "$isConnected" == "yes" ]]; then
+        name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
+        battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
+        message="$(get_battery_icon $battery_level) $battery_level% $volumeLevel"
         echo $(bluetoothctl disconnect $HEADPHONES_MAC_ADDR)
-        notify-send "Headphones disconnected" "Volume: $volumeLevel" -i headphones-disconnect 
+        notify-send "$name disconnected" "$message" -i headphones-disconnect 
     else
         echo $(bluetoothctl connect $HEADPHONES_MAC_ADDR)
-        notify-send "Headphones connected" "Volume: $volumeLevel" -i headphones    
+        sleep 2
+        blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
+        echo "$blStatus"
+        name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
+        battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
+        message="$(get_battery_icon $battery_level) $battery_level% $volumeLevel"
+        notify-send "$name connected" "$message" -i headphones
     fi
 fi
