2c0506d7a134eb007e06b0d7b3d77734e49484c6
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 09:57:27 +0200

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -31,17 +31,18 @@ function get_audio_icon() {
 battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | tr -d %)
 
 # Bluetooth headphones status (requires some setup - see notes below)
-bt_status=$(bluetoothctl info | grep 'Connected' | awk '{print $2}')
+# bt_status=$(bluetoothctl info | grep 'Connected' | awk '{print $2}')
 
 # CPU, RAM, IO usage
-#cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-#ram_usage=$(free -m | awk 'NR==2{printf "%.0f%%", $3*100/$2 }')
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+ram_usage=$(free -m | awk 'NR==2{printf "%.0f%%", $3*100/$2 }')
+io_stats=$(iostat -d -x 1 2 | awk 'NR==4{printf "r/s: %.1f kB/s w/s: %.1f kB/s", $2, $3}') 
 
 # Construct the message for notify-send
-# message="<span font='40px'>$(date +%H:%M)</span>
-# <span font='25px'>$(print_glyph $(get_audio_icon)) $(get-volume-level)%
-# $(print_glyph 'f1eb') $(get_wifi_signal_strength)% <span font='20px'>$(iwgetid -r)</span>
-# $(get_battery_icon $battery_level) $battery_level% $(print_glyph 'f013') $cpu_usage $(print_glyph 'f2db') $ram_usage</span>"
+message="<span font='40px'>$(date +%H:%M)</span>
+<span font='25px'>$(print_glyph $(get_audio_icon)) $(get-volume-level)%
+$(print_glyph 'f1eb') $(get_wifi_signal_strength)% <span font='20px'>$(iwgetid -r)</span>
+$(get_battery_icon $battery_level) $battery_level% $(print_glyph 'f013') $cpu_usage $(print_glyph 'f2db') $ram_usage</span>"
 
-# # Send the notification
-# notify-send "$(date +%d.%m.%Y)" "$message"
+# Send the notification
+notify-send "$(date +%d.%m.%Y)" "$message"
