29b009eb80b3e9c158b015d9b704799cafe5af63
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 15:15:27 +0200

    update

--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -10,11 +10,11 @@ volumeLevel="$(printf '\uf028\n') $(get-volume-level)%"
 blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
 
 function show-headphones-message() {
-            blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
-        name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
-        battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
-        message="$(get_battery_icon $battery_level) $battery_level% $(get_audio_status)"
-        notify-send "$name connected" "$message" -i headphones
+    blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
+    name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
+    battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
+    message="$(get_battery_icon $battery_level) $battery_level% $(get_audio_status)"
+    notify-send "$name connected" "$message" -i headphones
 }
 
 if ([[ "$blStatus" == *"Device $HEADPHONES_MAC_ADDR not available"* ]]); then
