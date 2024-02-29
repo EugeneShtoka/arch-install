e64edbfe5ff244b4a24d454280ff81003aa9161c
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 09:54:51 +0200

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -35,7 +35,7 @@ bt_status=$(bluetoothctl info | grep 'Connected' | awk '{print $2}')
 
 # CPU, RAM, IO usage
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-ram_usage=$(free -m | awk 'NR==2{printf "%.0f%%", $3*100/$2 }')
+#ram_usage=$(free -m | awk 'NR==2{printf "%.0f%%", $3*100/$2 }')
 
 # Construct the message for notify-send
 # message="<span font='40px'>$(date +%H:%M)</span>
