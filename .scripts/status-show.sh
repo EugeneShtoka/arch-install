912fee67c666694d17c0a132ad93e82917011d9f
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 13:46:09 +0200

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -34,7 +34,7 @@ battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep pe
 bt_status=$(bluetoothctl info | grep 'Connected' | awk '{print $2}')
 
 # CPU, RAM, IO usage
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1".0f%"}')
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%.0f%%", 100 - $1"%"}')
 ram_usage=$(free -m | awk 'NR==2{printf "%.0f%%", $3*100/$2 }')
 
 hardware_info="$(print_glyph 'f013') $cpu_usage $(print_glyph 'f2db') $ram_usage</span>"
