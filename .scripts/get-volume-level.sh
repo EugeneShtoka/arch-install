2942dba82c3521c19dcd7c34a9a2a3d6142c77e1
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 11:29:26 +0200

    update

--- .scripts/get-volume-level.sh
+++ .scripts/get-volume-level.sh
@@ -2,5 +2,5 @@
 
 # Volume information (assumes PulseAudio)
 get-volume-level() {
-    printf $(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+    printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1)
 }
