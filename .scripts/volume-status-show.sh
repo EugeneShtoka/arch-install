42d293c705f960b48c2070f860dff79f6277c1db
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 15:20:43 +0200

    update

--- .scripts/volume-status-show.sh
+++ .scripts/volume-status-show.sh
@@ -1,22 +1,5 @@
 #!/bin/zsh
 
-# Volume information (assumes PulseAudio)
-get-volume-level() {
-    printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1 | tr -d %)
-}
+source $SCRIPTS_PATH/volume.sh
 
-function get_audio_icon() {
-  if [[ $1 == "yes" ]]; then
-    echo "f025"
-  else
-    if [[ $(pactl get-sink-mute $(pactl get-default-sink) | awk '{print $2}') == "no" ]]; then
-      echo "f028"
-    else
-      echo "f026"
-    fi
-  fi
-}
-
-function get_audio_status() {
-    printf "\u$(get_audio_icon $bt_status) $(get-volume-level)%%"
-}
+notify-sed $(get_audio_status)
