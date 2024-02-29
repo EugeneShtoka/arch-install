307d465b0786ae053e1b8c08ab578bc41c2d1c6f
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 15:06:04 +0200

    update

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -2,7 +2,7 @@
 
 # Volume information (assumes PulseAudio)
 get-volume-level() {
-    printf "$(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1)%"
+    printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1 | tr -d %)
 }
 
 function get_audio_icon() {
