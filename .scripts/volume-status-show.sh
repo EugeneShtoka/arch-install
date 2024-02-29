a92942d3469a5cb83523574ef881aec608605894
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 15:34:29 +0200

    update

--- .scripts/volume-status-show.sh
+++ .scripts/volume-status-show.sh
@@ -2,16 +2,19 @@
 
 source $SCRIPTS_PATH/volume.sh
 
-if [[ "$1" == "Volume"]]
-  if [[ "$2" == "decreased" ]]
+target=$1
+action=$2
+
+if [[ "$target" == "Volume"]]
+  if [[ "$action" == "decreased" ]]
     icon=volume-low
-  elif [[ "$2" == "muted" ]]
+  elif [[ "$action" == "muted" ]]
     icon=volume-mute
   else
     icon=volume-high
   fi
-elif [[ "$1" == "Microphone" ]]
-  if [[ "$2" == "muted" ]]
+elif [[ "$target" == "Microphone" ]]
+  if [[ "$action" == "muted" ]]
     icon=microphone-mute
   else
     icon=microphone
