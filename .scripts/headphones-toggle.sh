d0e45fcc6f80a3665ac351b97c1e7812413090e6
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 15:15:10 +0200

    update

--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -9,6 +9,14 @@ volumeLevel="$(printf '\uf028\n') $(get-volume-level)%"
 
 blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
 
+function show-headphones-message() {
+            blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
+        name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
+        battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
+        message="$(get_battery_icon $battery_level) $battery_level% $(get_audio_status)"
+        notify-send "$name connected" "$message" -i headphones
+}
+
 if ([[ "$blStatus" == *"Device $HEADPHONES_MAC_ADDR not available"* ]]); then
     notify-send "Headphones not available" "$(get_audio_status)" -i headphones-disconnect  
 else
@@ -23,10 +31,5 @@ else
     else
         echo $(bluetoothctl connect $HEADPHONES_MAC_ADDR)
         sleep 2
-        blStatus=$(bluetoothctl info $HEADPHONES_MAC_ADDR)
-        name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
-        battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
-        message="$(get_battery_icon $battery_level) $battery_level% $(get_audio_status)"
-        notify-send "$name connected" "$message" -i headphones
     fi
 fi
