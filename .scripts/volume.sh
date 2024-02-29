11678567591113b08afde3c31f58aea347526097
Eugene Shtoka <eshtoka@gmail.com>
2024-02-29 14:58:09 +0200

    update

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -4,3 +4,15 @@
 get-volume-level() {
     printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1)
 }
+
+function get_audio_icon() {
+  if [[ $1 == "yes" ]]; then
+    echo "f025"
+  else
+    if [[ $(pactl get-sink-mute $(pactl get-default-sink) | awk '{print $2}') == "no" ]]; then
+      echo "f028"
+    else
+      echo "f026"
+    fi
+  fi
+}
