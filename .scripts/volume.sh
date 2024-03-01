0c0d07bdb23af88050a2b227a432aeb7173144ee
Eugene Shtoka <eshtoka@gmail.com>
2024-03-01 23:19:14 +0200

    update

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -23,7 +23,7 @@ function get_microphone_icon() {
   if [[ $(is_muted source) == "no" ]]; then
     echo "f130$(get_level source)"; 
   else
-    printf "<span color=\"magenta\">\uf131$(get_level source)</span>";
+    printf "<span color=\"magenta\">\uf131$(get_level source)%</span>";
   fi
 }
 
