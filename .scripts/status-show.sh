19b1770b8df9bf8805d1a118c7829b72126feb3a
Eugene Shtoka <eshtoka@gmail.com>
2024-03-01 23:05:01 +0200

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -30,7 +30,7 @@ fi
 # Construct the message for notify-send
 message="<span font='20px'>$(date +%d.%m.%Y)
 $(get_audio_status)
-$(print_glyph 'f1eb') $(get_wifi_signal_strength)% <span font='20px'>$(iwgetid -r)
+$(print_glyph 'f1eb') $(get_wifi_signal_strength)% $(iwgetid -r)
 $hardware_info</span>"
 
 # Send the notification
