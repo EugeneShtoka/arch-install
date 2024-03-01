9b00fa9c62f5cdb62332d44f8ee22dcf5a53bc35
Eugene Shtoka <eshtoka@gmail.com>
2024-03-01 09:11:19 +0200

    update

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -7,20 +7,16 @@ get-audio-level() {
     printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1 | tr -d %)
 }
 
+get-microphone-level() {
+    printf $(pactl get-sink-volume $(pactl get-default-sink) | grep -Pom 1 '[0-9]*%' | head -1 | tr -d %)
+}
+
 function is_audio_muted() {
   echo $(pactl get-sink-mute $(pactl get-default-sink) | awk '{print $2}')
 }
 
 function is_microphone_muted() {
   echo $(pactl get-source-mute $(pactl get-default-source) | awk '{print $2}')
-}
-
-function get_audio_icon() {
-  if [[ $(is_bluetooth_connected) == "yes" ]]; then
-    echo "f025"
-  else
-    if [[ $(is_audio_muted) == "no" ]]; then echo "f028"; else echo "f026"; fi
-  fi
 }
 
 function get_audio_icon() {
