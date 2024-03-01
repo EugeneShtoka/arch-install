f188e9842f32d3d616337fae19835a55ae80043d
Eugene Shtoka <eshtoka@gmail.com>
2024-03-01 23:04:41 +0200

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -28,8 +28,8 @@ if ([[ $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -oP '(?<=
 fi
 
 # Construct the message for notify-send
-message="<span font='25px'>$(date +%d.%m.%Y)
-<span font='25px'>$(get_audio_status)
+message="<span font='20px'>$(date +%d.%m.%Y)
+$(get_audio_status)
 $(print_glyph 'f1eb') $(get_wifi_signal_strength)% <span font='20px'>$(iwgetid -r)
 $hardware_info</span>"
 
