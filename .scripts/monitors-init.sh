afa807b099a2a7f7ab031d2ed4e4a933451e3d3c
Eugene Shtoka <eshtoka@gmail.com>
2024-03-06 17:06:13 +0200

    update

--- .scripts/monitors-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-monitors=($(xrandr | grep ' connected' | awk '{print $1}'))
-
-echo connecting monitor ${monitors[2]}
-xrandr --output ${monitors[2]} --auto --left-of ${monitors[1]} 
-sleep 2
-echo connecting monitor ${monitors[3]}
-xrandr --output ${monitors[3]} --auto --left-of ${monitors[2]}        
---
+++ .scripts/single-monitor-setup.sh
@@ -0,0 +1,2 @@
+xrandr --output $MONITOR_WORK_2 --off
+xrandr --output $MONITOR_WORK_3 --off
