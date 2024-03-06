673b9426359482dfe361eada34a4266f535e80b0
Eugene Shtoka <eshtoka@gmail.com>
2024-03-06 13:20:31 +0200

    update

---
+++ .scripts/monitors-single.sh
@@ -0,0 +1,12 @@
+monitorName=eDP-1
+
+i3-msg 'workspace $ws1 output $monitorName'
+i3-msg 'workspace $ws2 output $monitorName'
+i3-msg 'workspace $ws3 output $monitorName'
+i3-msg 'workspace $ws4 output $monitorName'
+i3-msg 'workspace $ws5 output $monitorName'
+i3-msg 'workspace $ws6 output $monitorName'
+i3-msg 'workspace $ws7 output $monitorName'
+i3-msg 'workspace $ws8 output $monitorName'
+i3-msg 'workspace $ws9 output $monitorName'
+i3-msg 'workspace $ws10 output $monitorName'
