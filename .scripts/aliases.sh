146a1b317815bc3659cb8beb31cb9a7cc85b1f0a
Eugene Shtoka <eshtoka@gmail.com>
2024-03-13 21:52:24 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -31,3 +31,4 @@ alias jira='setsid vivaldi-snapshot "https://swapp-ai.atlassian.net/jira/softwar
 alias spnd='systemctl suspend'
 alias stdn='$(microphone-unmute) shutdown now'
 
+alias db-proxy='pkill cloud-sql-proxy && setsid cloud-sql-proxy swapp-v1-1564402864804:us-central1:swapp-postgres-db -p 5433 -c /usr/share/credentials/swapp-v1-1564402864804.json &>/dev/null'
