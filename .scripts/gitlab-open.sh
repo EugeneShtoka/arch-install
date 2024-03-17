90dd12d31cdb04cd8637fa48d2bac159d8f5b16a
Eugene Shtoka <eshtoka@gmail.com>
2024-03-17 14:09:22 +0200

    update

--- .scripts/gitlab-open.sh
+++ .scripts/gitlab-open.sh
@@ -1,14 +1,9 @@
 #!/bin/zsh
 
-# Set the required prefix
 prefix="$HOME/dev/work"
-
-# Get the current directory
 current_dir=$PWD
 
-# Check if the current directory starts with the prefix
 if [[ $current_dir == $prefix* ]]; then
-  # Extract the first folder after the prefix
   first_folder="${current_dir#$prefix/}"
   first_folder="${first_folder%%/*}"  
   setsid vivaldi-snapshot "https://gitlab.com/buildos/$first_folder" &>/dev/null
