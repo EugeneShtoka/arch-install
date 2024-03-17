60203cbcd6cf7157a3af3915d48bcbe0d1c59a39
Eugene Shtoka <eshtoka@gmail.com>
2024-03-17 14:09:45 +0200

    update

--- .scripts/gitlab-open.sh
+++ .scripts/gitlab-open.sh
@@ -7,8 +7,6 @@ if [[ $current_dir == $prefix* ]]; then
   first_folder="${current_dir#$prefix/}"
   first_folder="${first_folder%%/*}"  
   setsid vivaldi-snapshot "https://gitlab.com/buildos/$first_folder" &>/dev/null
-  echo "Current directory starts with the prefix."
-  echo "First folder after the prefix: $first_folder"
 else
-  echo "Current directory does not start with the prefix."
+  setsid vivaldi-snapshot "https://gitlab.com/buildos/" &>/dev/null
 fi
