59a4a98a00f1fc513dd19da300b88c76da11bd79
Eugene Shtoka <eshtoka@gmail.com>
2024-03-17 13:42:51 +0200

    update

--- .scripts/gitlab-open.sh
+++ .scripts/gitlab-open.sh
@@ -1,7 +1,19 @@
 #!/bin/zsh
 
-script_path="${0:a}"
-echo "path $PWD"
+# Set the required prefix
+prefix="$HOME/dev/work"
 
-script_dir="${script_path:h}"
-echo "The script is located in: $script_dir"
+# Get the current directory
+current_dir=$PWD
+
+# Check if the current directory starts with the prefix
+if [[ $current_dir == $prefix* ]]; then
+  # Extract the first folder after the prefix
+  first_folder="${current_dir#$prefix/}"
+  first_folder="${first_folder%%/*}"  
+
+  echo "Current directory starts with the prefix."
+  echo "First folder after the prefix: $first_folder"
+else
+  echo "Current directory does not start with the prefix."
+fi
