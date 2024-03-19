0a6ed3b060a7fe0d03a79d31ea07b24881dd3158
Eugene Shtoka <eshtoka@gmail.com>
2024-03-19 23:06:59 +0200

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -5,5 +5,5 @@ if grep -q '^\-\-\-' "$filepath"; then
   sed -i "/^\-\-\-/s/$/\ntags: test rest best\n/" "$filepath" 
 else
   # Tags line doesn't exist, add it to the beginning
-  echo -e "---\ntags :$tags\n---\n$(cat "$filepath")" > "$filepath"
+  echo -e "---\ntags: $tags\n---\n$(cat "$filepath")" > "$filepath"
 fi
