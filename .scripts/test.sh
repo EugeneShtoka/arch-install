6ae7afa6d1af20deb9558be671195fded3d29534
Eugene Shtoka <eshtoka@gmail.com>
2024-03-20 00:01:31 +0200

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
+tags="test rest best"
+tags=${tags// /\\n   - }
 filepath="Obsidian/Resources/Professional/Vertical Scaling.md"
 if grep -q '^\-\-\-' "$filepath"; then
-  sed -i "0,/^\-\-\-/s/$/\ntags:\n -test\n - rest\n - best/" "$filepath" 
+  sed -i "0,/^\-\-\-/s/$/\ntags:\n  -$tags/" "$filepath" 
 else
   # Tags line doesn't exist, add it to the beginning
   echo -e "---\ntags: $tags\n---\n$(cat "$filepath")" > "$filepath"
