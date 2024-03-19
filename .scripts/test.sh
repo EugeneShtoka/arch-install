cace55b1c6e84b73692e70904c44aa6a48e5a678
Eugene Shtoka <eshtoka@gmail.com>
2024-03-19 22:55:45 +0200

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-sed -i "/^tags:/s/test rest best" "Obsidian/Resources/Professional/Architecture/Components/GeoDNS.md"
+sed -i "/^tags:/s/$/test rest best" "Obsidian/Resources/Professional/Architecture/Components/GeoDNS.md"
