da0f4cf259cf0c8e5e67ab56c8ce80a3fe21c318
Eugene Shtoka <eshtoka@gmail.com>
2024-03-20 00:10:16 +0200

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,11 +1,18 @@
 #!/bin/zsh
 
 tags="test rest best"
-tags=${tags// /\\n  - }
 filepath="Obsidian/Resources/Vertical Scaling.md"
-if grep -q '^\-\-\-' "$filepath"; then
-  sed -i "0,/^\-\-\-/s/$/\ntags:\n  - $tags/" "$filepath" 
+
+tags=${tags// /\\n  - }
+# Add the tags (prepend existing content as needed)
+if grep -q '^tags:' "$filepath"; then
+  # Tags line exists, insert our new tags before it
+  sed -i "/^tags:/s/$/\n  - $tags/" "$filepath" 
 else
-  # Tags line doesn't exist, add it to the beginning
-  echo -e "---\ntags:\n  - $tags\n---\n$(cat "$filepath")" > "$filepath"
+  if grep -q '^---' "$filepath"; then
+    sed -i "0,/^\-\-\-/s/$/\ntags:\n  - $tags/" "$filepath" 
+  else
+    # Tags line doesn't exist, add it to the beginning
+    echo -e "---\ntags:\n  - $tags\n---\n$(cat "$filepath")" > "$filepath"
+  fi
 fi
