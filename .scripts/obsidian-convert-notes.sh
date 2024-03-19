7ca2260feefcd5c6e6f0be8752a6bda86b09cc25
Eugene Shtoka <eshtoka@gmail.com>
2024-03-20 00:15:54 +0200

    update

--- .scripts/obsidian-convert-notes.sh
+++ .scripts/obsidian-convert-notes.sh
@@ -25,18 +25,18 @@ process_md_file() {
 
   tags=${tags// /\\n  - }
   # Add the tags (prepend existing content as needed)
-  if grep -q '^tags:' "$filepath"; then
-    # Tags line exists, insert our new tags before it
-    sed -i "/^tags:/s/$/\n  - $tags/" "$filepath" 
-  else
-    if grep -q '^---' "$filepath"; then
-      sed -i "0,/^\-\-\-/s/$/\ntags:\n  - $tags/" "$filepath" 
-    else
-      # Tags line doesn't exist, add it to the beginning
-      echo -e "---\ntags:\n  - $tags\n---\n$(cat "$filepath")" > "$filepath"
-    fi
-  fi
-  #mv "$filepath" "$source_dir/"
+  # if grep -q '^tags:' "$filepath"; then
+  #   # Tags line exists, insert our new tags before it
+  #   sed -i "/^tags:/s/$/\n  - $tags/" "$filepath" 
+  # else
+  #   if grep -q '^---' "$filepath"; then
+  #     sed -i "0,/^\-\-\-/s/$/\ntags:\n  - $tags/" "$filepath" 
+  #   else
+  #     # Tags line doesn't exist, add it to the beginning
+  #     echo -e "---\ntags:\n  - $tags\n---\n$(cat "$filepath")" > "$filepath"
+  #   fi
+  # fi
+  mv "$filepath" "$source_dir/"
 }
 
 export -f process_md_file
