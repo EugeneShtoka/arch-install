2a3484ecd44a175ec257362956b4f34387337062
Eugene Shtoka <eshtoka@gmail.com>
2024-03-19 23:04:06 +0200

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-sed -i "/^tags:/s/$/ test rest best/" "Obsidian/Resources/Professional/Architecture/Components/GeoDNS.md"
+filepath="Obsidian/Resources/Professional/Architecture/Terms/Vertical Scaling.md"
+if grep -q '^\-\-\-' "$filepath"; then
+  sed -i "/^\-\-\-/s/$/tags: $tags\n" "$filepath" 
+else
+  # Tags line doesn't exist, add it to the beginning
+  echo -e "---\ntags :$tags\n---\n$(cat "$filepath")" > "$filepath"
+fi
