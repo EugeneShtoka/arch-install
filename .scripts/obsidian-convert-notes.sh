f0d3922f6d9005364d5338f1f6b620b2d09400bb
Eugene Shtoka <eshtoka@gmail.com>
2024-03-20 07:48:43 +0200

    update

--- .scripts/obsidian-convert-notes.sh
+++ .scripts/obsidian-convert-notes.sh
@@ -22,20 +22,7 @@ process_md_file() {
     dirpath=$(dirname "$dirpath") 
   done
 
-  tags=${tags,,}
-  tags=${tags// /\\n  - }
-  # Add the tags (prepend existing content as needed)
-  if grep -q '^tags:' "$filepath"; then
-    # Tags line exists, insert our new tags before it
-    sed -i "0,/^tags:/s/$/\n  - $tags/" "$filepath" 
-  else
-    if grep -q '^---' "$filepath"; then
-      sed -i "0,/^\-\-\-/s/$/\ntags:\n  - $tags/" "$filepath" 
-    else
-      # Tags line doesn't exist, add it to the beginning
-      echo -e "---\ntags:\n  - $tags\n---\n$(cat "$filepath")" > "$filepath"
-    fi
-  fi
+  $SCRIPT_PATH/obsidian-add-tags-to-note.sh "$filepath" "$tags"
   #mv "$filepath" "$source_dir/"
 }
 
