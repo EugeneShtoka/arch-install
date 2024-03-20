376f7cb106535c4f31e1f66f2480435e72759fc5
Eugene Shtoka <eshtoka@gmail.com>
2024-03-20 20:50:46 +0200

    update

--- .scripts/obsidian-add-tags-to-note.sh
+++ .scripts/obsidian-add-tags-to-note.sh
@@ -9,14 +9,11 @@ tags="\n  - ${(L)tags//|/\\n  - }"
 # Add the tags (prepend existing content as needed)
 if grep -q '^tags:' "$filepath"; then
   # Tags line exists, insert our new tags before it
-  echo "case 1"
   sed -i "0,/^tags:/s/$/$tags/" "$filepath" 
 else
   if [[ $first_line == "---" ]]; then
-    echo "case 2"
     sed -i "0,/^---/s/$/\ntags:$tags/" "$filepath" 
   else
-    echo "case 3"
     # Tags line doesn't exist, add it to the beginning
     echo -e "---\ntags:$tags\n---\n$(cat "$filepath")" > "$filepath"
   fi
