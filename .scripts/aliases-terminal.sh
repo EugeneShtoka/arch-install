c324917c8212e7a7cc44bfcd9f638285fa028b2d
Eugene Shtoka <eshtoka@gmail.com>
2024-03-21 18:07:05 +0200

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -19,6 +19,7 @@ alias gcpr=$SCRIPTS_PATH/git-create-pull-request.sh
 alias gcmr=$SCRIPTS_PATH/git-create-merge-request.sh
 
 alias home='cd ~ && code . && clear'
+alias home='cd $HOME/dev/gcalcli && code . && clear'
 alias aps='cd $WORK_REPOS_PATH/swapp=aps && code . && clear'
 alias workflows='cd $WORK_REPOS_PATH/workflows && code . && clear'
 alias backend='cd $WORK_REPOS_PATH/swappbackend && code . && clear'
