8cc9b797790f7847d40172f1354f4c134a171906
Eugene Shtoka <eshtoka@gmail.com>
2024-03-21 16:14:52 +0200

    update

--- .scripts/gitlab-open.sh
+++ .scripts/gitlab-open.sh
@@ -1,10 +1,9 @@
 #!/bin/zsh
 
-prefix="$HOME/dev/work"
 current_dir=$PWD
 
-if [[ $current_dir == $prefix* ]]; then
-  first_folder="${current_dir#$prefix/}"
+if [[ $current_dir == $WORK_REPOS_PATH* ]]; then
+  first_folder="${current_dir#$WORK_REPOS_PATH/}"
   first_folder="${first_folder%%/*}"  
   setsid vivaldi-snapshot "https://gitlab.com/buildos/$first_folder" &>/dev/null
 else
