2c624ce23555fb020e784f039210b727a2833f3e
Eugene Shtoka <eshtoka@gmail.com>
2024-03-24 09:32:27 +0200

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -7,6 +7,7 @@ alias bw-create='source $SCRIPTS_PATH/bw-create.sh'
 alias bw-item='source $SCRIPTS_PATH/bw-item.sh'
 alias bw-pass='source $SCRIPTS_PATH/bw-unlock.sh && $SCRIPTS_PATH/bw-pass.sh'
 alias postal-code='source $SCRIPTS_PATH/postal-code-get.sh'
+alias zrc='source $SCRIPTS_PATH/zsh-reload-config.sh'
 
 alias gsm='git switch main'
 alias grf='git checkout main --'
