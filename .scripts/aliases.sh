91b2fcc74457d759717957d05a7bee20e2cb70e3
Eugene Shtoka <eshtoka@gmail.com>
2024-03-29 21:31:16 +0300

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -11,6 +11,7 @@ alias notes='setsid obsidian &>/dev/null'
 alias torrent='setsid qbittorrent &>/dev/null'
 alias file-manager='setsid thunar &>/dev/null'
 alias bulk-rename='setsid thunar --bulk-rename &>/dev/null'
+alias keyboard-trainer='setsid tipp10 &>/dev/null'
 
 alias cs-pull=$SCRIPTS_PATH/custom-scripts-pull.sh
 alias cs-push=$SCRIPTS_PATH/custom-scripts-push.sh
