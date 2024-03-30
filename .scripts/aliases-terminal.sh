ed766656be295f3bc078a739f651497ac3bcd833
Eugene Shtoka <eshtoka@gmail.com>
2024-03-30 15:12:06 +0300

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -23,7 +23,7 @@ alias gcmr=$SCRIPTS_PATH/git-create-merge-request.sh
 alias home='cd ~ && code . && clear'
 alias gcalcli='cd $HOME/dev/gcalcli && code . && clear'
 alias aps='cd $WORK_REPOS_PATH/swapp=aps && code . && clear'
-alias plugin='cd $WORK_REPOS_PATH/plugin && code . && clear'
+alias plugin='cd $WORK_REPOS_PATH/revit-plugin && code . && clear'
 alias workflows='cd $WORK_REPOS_PATH/workflows && code . && clear'
 alias backend='cd $WORK_REPOS_PATH/swappbackend && code . && clear'
 alias roomassignment='cd $WORK_REPOS_PATH/roomassignment && code . && clear'
