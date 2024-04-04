e55e812f418709cf0a95749b56c197b4a24c07c7
Eugene Shtoka <eshtoka@gmail.com>
2024-04-04 10:51:31 +0300

    update

--- .scripts/apps-install.sh
+++ .scripts/apps-install.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
-sudo pacman -Syi
-yes | sudo pacman -S inotify-tools thunar rofi bluez bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim
+yes | sudo pacman -Syi inotify-tools thunar rofi bluez bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim
 
 $SCRIPTS_PATH/yay-install.sh
 
