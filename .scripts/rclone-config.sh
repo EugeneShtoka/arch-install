13374d366c4212cb75251a6564b2b3dce14cbfbc
Eugene Shtoka <eshtoka@gmail.com>
2024-04-05 16:48:26 +0300

    update

--- .scripts/rclone-config.sh
+++ .scripts/rclone-config.sh
@@ -1,8 +1,10 @@
 source $SCRIPTS_PATH/bw-unlock.sh
+
 confPath=$HOME/.config/rclone
+mldir -p $confPath
 
 conf=$(bw get item rclone | jq '.notes')
 conf="${conf//\\n/\n}"
 conf="${conf//\\\"/\"}"
 
-echo $conf > ~/.config/rclone/rclone.conf
+echo $conf > $confPath/rclone.conf
