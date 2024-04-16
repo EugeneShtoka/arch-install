1d75e17995a27d229d24197cb309ab21c0462eee
Eugene Shtoka <eshtoka@gmail.com>
2024-04-16 10:33:54 +0300

    update

---
+++ .scripts/monitor-toggle.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+num_monitors=$(xrandr --listactivemonitors | grep -c 'Monitors:') 
+
+if [[ $num_monitors -eq 1 ]]; then
+    $SCRIPTS_PATH/monitor-multi-setup.sh
+else 
+    $multi_monitor_script
+fi
