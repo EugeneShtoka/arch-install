5c840225ad7825acd7701dd5033383545d3ed96e
Eugene Shtoka <eshtoka@gmail.com>
2024-04-16 10:34:45 +0300

    update

--- .scripts/monitor-toggle.sh
+++ .scripts/monitor-toggle.sh
@@ -6,5 +6,6 @@ if [[ $num_monitors -eq 1 ]]; then
     #$SCRIPTS_PATH/monitor-multi-setup.sh
     echo switch to multi monitor
 else 
+    #$SCRIPTS_PATH/monitor-single-setup.sh
     echo switch to single monitor
 fi
