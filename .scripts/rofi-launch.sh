192eb95836482a5705f9ded76665308d10072923
Eugene Shtoka <eshtoka@gmail.com>
2024-04-20 18:11:51 +0300

    update

--- .scripts/rofi-launch.sh
+++ .scripts/rofi-launch.sh
@@ -3,5 +3,5 @@
 dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-10'
 
-command=$(. $SCRIPTS_PATH/functions-list.sh | rofi -theme ${dir}/${theme}.rasi -dmenu -mathcing fuzzy)
+command=$(. $SCRIPTS_PATH/functions-list.sh | rofi -theme ${dir}/${theme}.rasi -dmenu -matching fuzzy)
 zsh -ic $command
