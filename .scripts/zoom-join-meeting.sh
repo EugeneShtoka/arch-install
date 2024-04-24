9be8ef2e250ff9a4e526edbba4ecc6387438daef
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 23:21:13 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -41,7 +41,7 @@ else
 		availableMeetings+=("$name")
 	done
 	dir="$HOME/.config/rofi/launchers/type-4"
-	theme='style-3'
+	theme='style-1'
 	choice=$(printf '%s\n' "${availableMeetings[@]}" | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 	echo "$choice"
 	#choice=$(printf '%s\n' "${availableMeetings[@]}" | dmenu -p "Select item:")
