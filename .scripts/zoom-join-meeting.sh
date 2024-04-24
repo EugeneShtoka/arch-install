e41e851877db5ed759e5ae5e6d460e88619162f4
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 20:36:08 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-json_data='[{"name": "Alice", "age": 30}]'
+json_data='[{"name": "Alice", "age": 30},{"name": "Bob", "age": 25}]'
 
 topLimit=$(date -d '+5 days' +'%Y-%m-%dT%H:%M:%S%z')
 meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit --eventTypes default )
