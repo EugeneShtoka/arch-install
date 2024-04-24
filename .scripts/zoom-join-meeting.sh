12c6d7ff34c09bad1046d9d43d777cd4ccaed854
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 22:16:03 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -26,7 +26,7 @@ if [[ $meetingCount -eq 0 ]]; then
 	nextMeeting=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxResults 1 --eventTypes default | jq "$eventMap" | jq '.[0]')	
 	dunstify "Auto Join meetings" "No meetings scheduled soon.<br>Next meeting:<br>  $(converertDate $nextMeeting)";
 elif [[ $meetingCount -eq 1 ]]; then
-	conf=${$(echo $meetings | jq '.[] | .url')##*/}
+	conf=${$(echo $meetings | jq '.[0] | .url')##*/}
 	conf=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
 	setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" >/dev/null 2>&1 < /dev/null &
 else
