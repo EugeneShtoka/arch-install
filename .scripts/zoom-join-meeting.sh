2791ad9cf86633263769f8f8029c1a205030bcdd
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 19:37:57 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -5,16 +5,16 @@ meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxS
 meetingArr=$(echo $meetings | jq '.[0]' | tr -d '\"')
 meetingCount=${#meetingArr[@]}
 
-if [[ $meetingCount -eq 0 ]]; then
+if [ $meetingCount -eq 0 ]; then
 	nextMeeting=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxResults 1 --eventTypes default | jq '.[] | [.summary, .start.dateTime, .end.dateTime]')	
 	meetingName=$(echo $nextMeeting | jq '.[0]' | tr -d '\"' | tr -d  ' ')
 	date_string=$(echo $nextMeeting | jq '.[1]' | tr -d '\"')
 	date_only=$(date -d $date_string +'%d-%m-%Y')
 	today=`date +'%d-%m-%Y'`
 	echo  "$date_only" "$today"
-	if [[ "$date_string" == "$today"]]; then
+	if [ "$date_string" == "$today"]; then
 		formatted_date=Today
-	elif [[ "$date_string" == "`date -d '+1 day' +'%d-%m-%Y'`"]]; then
+	elif [ "$date_string" == "`date -d '+1 day' +'%d-%m-%Y'`"]; then
 		formatted_date=Tomorrow
 	else
 		formatted_date=$(date -d $date_string +'%A, %d %B %Y %H:%M')
@@ -22,7 +22,7 @@ if [[ $meetingCount -eq 0 ]]; then
 
 	echo $meetingName - $formatted_date
 	dunstify "Auto Join meetings" "no meetings found"
-elif [[ $meetingCount -eq 1 ]]; then
+elif [ $meetingCount -eq 1 ]; then
 	conf=${$(echo $meetings | jq '.[3]')##*/}
 	conf=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
 	setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" >/dev/null 2>&1 < /dev/null &
