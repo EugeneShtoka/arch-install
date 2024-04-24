2d33ade9047f34afd9a59172e4339795e481da77
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 18:00:26 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -9,8 +9,8 @@ meetingCount=${#meetingArr[@]}
 if [[ $meetingCount -eq 0 ]]; then
 	dunstify "Auto Join meetings" "no meetings found"
 elif [[ $meetingCount -eq 1 ]]; then
-	conf=${$(echo $meetings | jq '.[3]')##*/} | tr -d '\"'
-	echo $conf
+	conf=${$(echo $meetings | jq '.[3]')##*/%\"}
+	conf echo $conf = 
 	setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" &>/dev/null
 else
 	dunstify "Auto Join meetings" "more than one meeting found"
