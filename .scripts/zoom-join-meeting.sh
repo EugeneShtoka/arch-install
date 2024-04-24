0f4a6cbf1f58e080afae237cd15802eb8bea7c53
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 21:45:47 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -17,8 +17,9 @@ function converertDate() {
 
 json_data='[{"name": "Alice", "age": 30},{"name": "Bob", "age": 25}]'
 
+eventMap="map({ summary, start, end, url: .conferenceData.entryPoints.[0].uri })"
 topLimit=$(date -d '+5 days' +'%Y-%m-%dT%H:%M:%S%z')
-meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit --eventTypes default | jq 'map({ summary, start, end, url: .conferenceData.entryPoints.[0].uri })')
+meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit --eventTypes default | jq '')
 # Iterate over items using a while loop
 echo "$meetings"
 echo "$meetings" | jq -c '.[]' | while read item; do
