a0016e4c371d5c617c867defeb3013a05dcb91e3
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 23:03:21 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -16,7 +16,7 @@ function converertDate() {
 }
 
 function connectToMeeting() {
-	meeting=$
+	meeting=$1
 	conf=${$(echo $meeting | jq '.url')##*/}
 	conf=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
 	setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" >/dev/null 2>&1 < /dev/null &
