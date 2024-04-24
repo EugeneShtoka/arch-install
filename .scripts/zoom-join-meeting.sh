c703d4cc83da4bf3ec3852e54c6cc5ad8af3e7b8
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 23:04:37 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -34,7 +34,6 @@ if [[ $meetingCount -eq 0 ]]; then
 	dunstify "Auto Join meetings" "No meetings scheduled soon.<br>Next meeting:<br>  $(converertDate $meeting)";
 elif [[ $meetingCount -eq 1 ]]; then
 	meeting=$(echo $meetings | jq -c '.[0]')
-	echo "$meeting"
 	$(connectToMeeting $meeting)
 else
 	availableMeetings=()
