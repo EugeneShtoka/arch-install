38915cbd15640c3d32ff4273eb4970d8cce9f0d9
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 17:31:41 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
-meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --eventTypes default | jq '.[] | [.summary, .start.dateTime, .end.dateTime, .conferenceData.entryPoints.[0].uri]')
+topLimit=$(date -d '+5 minutes' +'%Y-%m-%dT%H:%M:%S%z')
+echo $topLimit
+meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime --eventTypes default | jq '.[] | [.summary, .start.dateTime, .end.dateTime, .conferenceData.entryPoints.[0].uri]')
 echo $meetings | jq '.[0]'
