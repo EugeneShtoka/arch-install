64fd02a805bf35eb96821eeaecfa7ed12dd1eb33
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 22:19:56 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -35,6 +35,7 @@ elif [[ $meetingCount -eq 1 ]]; then
 else
 	availableMeetings=()
 	echo "$meetings" | jq -c '.[]' | while read meeting; do
+		echo $meeting
 		name=$(converertDate "$meeting")
 		availableMeetings+=("$name")
 	done
