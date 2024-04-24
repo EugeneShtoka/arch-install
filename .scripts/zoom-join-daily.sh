4ed350e7d6ed0753ef89cffecfc871f7d2b873a9
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 17:24:04 +0300

    update

--- .scripts/zoom-join-daily.sh
+++ .scripts/zoom-join-daily.sh
@@ -1,8 +1,4 @@
 #!/bin/zsh
 
-daily() {
-	pass=$(secret-tool lookup service zoom)
-	setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=75993015411&pwd=$pass" &>/dev/null
-}
-
-daily
+pass=$(secret-tool lookup service zoom)
+setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=75993015411&pwd=$pass" &>/dev/null
