0c1e918d8d133df801bd7bab93526534dd9740d2
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 18:19:06 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -7,7 +7,7 @@ meetingCount=${#meetingArr[@]}
 
 if [[ $meetingCount -eq 0 ]]; then
 	nextMeeting=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxResults 1 --eventTypes default | jq '.[] | [.summary, .start.dateTime, .end.dateTime]')	
-	echo $nextMeeting
+	echo $(echo $nextMeeting | jq '.[0]')
 	dunstify "Auto Join meetings" "no meetings found"
 elif [[ $meetingCount -eq 1 ]]; then
 	conf=${$(echo $meetings | jq '.[3]')##*/}
