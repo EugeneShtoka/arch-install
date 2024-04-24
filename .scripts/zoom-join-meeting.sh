d6b929a409c0a4b4cb55f06fe8d33f11bfcf58c9
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 22:00:01 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -22,7 +22,7 @@ meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxS
 echo "$meetings" | jq -c '.[]' | while read meeting; do
 	name=$(converertDate "$meeting")
 	conf=${$(echo $meeting | jq '.url')##*/}
-	conf=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
+	command=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
 	#command="setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" >/dev/null 2>&1 < /dev/null &"
 	echo $name #$command
 done
