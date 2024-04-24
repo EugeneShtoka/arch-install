dbc9e72a8524228d169aea6b7748b1ffa8398c9b
Eugene Shtoka <eshtoka@gmail.com>
2024-04-24 17:46:52 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -3,5 +3,5 @@
 topLimit=$(date -d '+5 days' +'%Y-%m-%dT%H:%M:%S%z')
 echo $topLimit
 meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit --eventTypes default | jq '.[] | [.summary, .start.dateTime, .end.dateTime, .conferenceData.entryPoints.[0].uri]')
-meetingArr=($(echo $meetings | jq '.[0]'))
+meetingArr=($(${echo $meetings | jq '.[0]'//\}))
 echo "${meetingArr[@]}"
