e828ed0ce3b4eced803e02999c9a761f7b3a3a08
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 23:00:53 +0300

    update

--- .scripts/agenda-show.sh
+++ .scripts/agenda-show.sh
@@ -2,5 +2,6 @@
 
 source $SCRIPTS_PATH/meetings.sh
 
-bindsym $mod+a notify-send "Agenda" "<span font='15px'>$(agenda 7)</span>"
+daysToShow=$1
+bindsym $mod+a notify-send "Agenda" "<span font='15px'>$(agenda $1)</span>"
 
