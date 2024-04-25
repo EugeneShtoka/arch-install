2441887f110853fa6d0745fd050c10847d8bc5b4
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 18:09:52 +0300

    update

--- .scripts/agenda.sh
+++ .scripts/agenda.sh
@@ -31,7 +31,6 @@ function connectToMeeting() {
 
 eventMap='map({ summary, start: .start.dateTime, end: .end.dateTime, conferenceType: .conferenceData.conferenceSolution.name, url: .conferenceData.entryPoints.[0].uri })'
 topLimit=$(date -d '+10 days' +'%Y-%m-%dT%H:%M:%S%z')
-echo $topLimit
 meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit --eventTypes default | jq "$eventMap")
 meetings=$(echo "$meetings" | jq '[ .[] | select(.conferenceType == "Google Meet" or .conferenceType == "Zoom Meeting" )]')
 
