0386d182f667ee6be5fd7d4999739808a9768ce6
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 23:07:04 +0300

    update

--- .scripts/meetings.sh
+++ .scripts/meetings.sh
@@ -43,7 +43,6 @@ function agenda() {
     daysToShow=$1
 	eventMap='map({ summary, start, end, conferenceType: .conferenceData.conferenceSolution.name, url: .conferenceData.entryPoints.[0].uri })'
 	topLimit=$(date -d "+$daysToShow days" +'%Y-%m-%dT00:00:00%z')
-    echo $topLimit
 	meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit | jq "$eventMap")
 
 	meetingCount=$(echo "$meetings" | jq '. | length')
