009d39fbb32a3863d41e75ed01329ca5da0322f3
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 18:22:14 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -39,7 +39,8 @@ else
 	choice=$(printf '%s\n' "${availableMeetings[@]}" | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 	echo $choice
 	echo "$meetings" | jq -c '.[]' | while read meeting; do
-		name=$(converertDate "$meeting")
+		name=$(	dunstify "Auto Join meetings" "No meetings scheduled soon.<br>Next meeting:<br>  $(formatMeeting $meeting)";
+ "$meeting")
 		if [[ "$name" == "$choice" ]]; then
 			$(connectToMeeting "$meeting")
 		fi
