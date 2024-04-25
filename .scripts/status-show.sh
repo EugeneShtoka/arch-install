88811923f470e313ec3f4752ef6dddc7df5d419c
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 22:34:55 +0300

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -28,7 +28,8 @@ if ([[ $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -oP '(?<=
 fi
 
 $SCRIPTS_PATH/agenda.sh>agenda
-echo agenda
+#echo agenda
+
 # Construct the message for notify-send
 message="<span font='20px'>$(date +%d.%m.%Y)
 $(date +%A)
