f21e13b217fbbc99940fcdd388504572f1f68c6d
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 18:19:00 +0300

    update

--- .scripts/meetings.sh
+++ .scripts/meetings.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-function converertDate() {
+function formatMeeting() {
 	meeting=$1
 	
 	date_string=$(echo $meeting | jq '.start' | tr -d '\"')
@@ -16,7 +16,17 @@ function converertDate() {
 }
 
 function formatMeeting() {
-
+	meeting=$1
+	
+	date_string=$(echo $meeting | jq '.start' | tr -d '\"')
+	date_only=$(date -d $date_string +'%d-%m-%Y')
+	if [[ "$date_only" == "`date +'%d-%m-%Y'`" ]]; then
+		formatted_date="Today at $(date -d $date_string +'%H:%M')"
+	elif [[ "$date_only" == "`date -d '+1 day' +'%d-%m-%Y'`" ]]; then
+		formatted_date="Tomorrow at $(date -d $date_string +'%H:%M')"
+	else
+		formatted_date=$(date -d $date_string +'%A, %d %B %Y at %H:%M')
+	fi
 }
 
 function getName() {
