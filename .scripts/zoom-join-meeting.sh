756ec1190c90b4c6e4e643de86a9451994472df9
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 13:16:12 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -21,6 +21,7 @@ function connectToMeeting() {
 	if [[ $conferenceType == "Zoom Meeting" ]]; then
 		conf=${$(echo $meeting | jq '.url')##*/}
 		conf=$(echo $conf | tr -d '\"' | sed 's/?/\&/')
+		echo "Zoom Meeting $conf"
 		setsid xdg-open "zoommtg://zoom.us/join?action=join&video=on&confno=$conf" >/dev/null 2>&1 < /dev/null &
 	elif [[ $conferenceType == "Google Meet" ]]; then
 		url=$(echo $meeting | jq '.conferenceType' | tr -d '\"')
