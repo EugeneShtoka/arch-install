8ade08a4075cc99aabf2037ad2205a2579103a3b
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 22:19:21 +0300

    update

--- .scripts/agenda.sh
+++ .scripts/agenda.sh
@@ -16,7 +16,6 @@ echo "$meetings" | jq -c '.[]' | while read meeting; do
 	fi
 	dateTime=$(echo $meeting | jq '.start.dateTime' | tr -d '\"')
     if [[ $dateTime == "null" ]]; then
-        startTime="\t\t\t"
     else
         startTime="$(date -d $dateTime +'%H:%M')\t"
     fi
