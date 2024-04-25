c401326581836ca2463a4bbf5674f1f10c0736e1
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 21:54:19 +0300

    update

--- .scripts/meetings.sh
+++ .scripts/meetings.sh
@@ -20,7 +20,7 @@ function getDate() {
 		startDate="Tomorrow"
 	else
         echo "AAAAAAA$startDate"AAAA
-        startDate=$(date -d "2024-04-30" +'%A, %d %B %Y')
+        startDate=$(date -d $startDate +'%A, %d %B %Y')
     fi
 
     if [ -n "$dateTime" ]; then
