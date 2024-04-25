3d0bef319c4cad5980051544637aec913da61651
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 21:27:31 +0300

    update

--- .scripts/agenda.sh
+++ .scripts/agenda.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-eventMap='map({ summary, start: .start.dateTime, end: .end.dateTime, conferenceType: .conferenceData.conferenceSolution.name, url: .conferenceData.entryPoints.[0].uri })'
+eventMap='map({ summary, start, end, conferenceType: .conferenceData.conferenceSolution.name, url: .conferenceData.entryPoints.[0].uri })'
 topLimit=$(date -d '+8 days' +'%Y-%m-%dT00:00:00%z')
 echo $topLimit
 meetings=$(~/dev/gcalcli/gcalcli list events --single --orderBy startTime --maxStartTime $topLimit | jq "$eventMap")
