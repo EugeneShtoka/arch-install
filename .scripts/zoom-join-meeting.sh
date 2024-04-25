c5e84a613910dd1101e5a36cdc9e10e0e49f000c
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 06:56:31 +0300

    update

--- .scripts/zoom-join-meeting.sh
+++ .scripts/zoom-join-meeting.sh
@@ -5,7 +5,7 @@ function converertDate() {
 	meetingName=$(echo $meeting | jq '.summary' | tr -d '\"' | tr -d  ' ')
 	date_string=$(echo $meeting | jq '.start' | tr -d '\"')
 	date_only=$(date -d $date_string +'%d-%m-%Y')
-	if [[ "$date_only" == "date +'%d-%m-%Y'" ]]; then
+	if [[ "$date_only" == "`date +'%d-%m-%Y'`" ]]; then
 		formatted_date="Today at $(date -d $date_string +'%H:%M')"
 	elif [[ "$date_only" == "`date -d '+1 day' +'%d-%m-%Y'`" ]]; then
 		formatted_date="Tomorrow at $(date -d $date_string +'%H:%M')"
