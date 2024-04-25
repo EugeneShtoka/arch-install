3403a0a201d57f3bef843b546f4aee0ce862044f
Eugene Shtoka <eshtoka@gmail.com>
2024-04-25 22:48:43 +0300

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -28,7 +28,7 @@ if ([[ $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -oP '(?<=
   hardware_info="$(get_battery_icon $battery_level) $battery_level% $cpu_and_ram $hardware_info"
 fi
 
-echo "$(agenda)"
+agenda="$(agenda)"
 
 # Construct the message for notify-send
 message="<span font='20px'>$(date +%d.%m.%Y)
@@ -38,7 +38,7 @@ $(print_glyph 'f1eb') $(get_wifi_signal_strength)% $(iwgetid -r)
 $hardware_info
 <br>
 Agenda:
-$(agenda)
+$agenda
 </span>"
 
 # Send the notification
