926e75a8dd469156819ca1f62a9a1f4e6c787351
Eugene Shtoka <eshtoka@gmail.com>
2024-04-26 19:19:11 +0300

    update

--- .scripts/agenda-show.sh
+++ .scripts/agenda-show.sh
@@ -3,5 +3,7 @@
 source $SCRIPTS_PATH/meetings.sh
 
 daysToShow=$1
-notify-send "Agenda" "<span font='15px'>$(agenda $daysToShow)</span>"
+title=Agena
+
+notify-send "$title" "<span font='15px'>$(agenda $daysToShow)</span>"
 
