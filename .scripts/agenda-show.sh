614871b9f0c57580a51a04f18c44dddd2a0c9d7d
Eugene Shtoka <eshtoka@gmail.com>
2024-04-26 19:19:52 +0300

    update

--- .scripts/agenda-show.sh
+++ .scripts/agenda-show.sh
@@ -3,7 +3,12 @@
 source $SCRIPTS_PATH/meetings.sh
 
 daysToShow=$1
-title=Agena
+title=Agenda
+if [[ $daysToShow -eq 1 ]]; then
+    title=Daily
+elif [[ $daysToShow -eq 7 ]]; then
+    title=Weekly
+fi
 
 notify-send "$title" "<span font='15px'>$(agenda $daysToShow)</span>"
 
