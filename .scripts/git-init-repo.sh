d03bda89953562dcdaf69849c4b7e56121c07a84
Eugene Shtoka <eshtoka@gmail.com>
2024-04-29 16:14:29 +0300

    update

--- .scripts/git-init-repo.sh
+++ .scripts/git-init-repo.sh
@@ -2,11 +2,12 @@
 
 path=$(pwd)
 folder="${path##*/}"
-echo $folder
-# PROJECT_NAME=myAwesomeRepo
-# gh repo create $PROJECT_NAME --public
-# git init
-# git add -A
-# git commit -m 'Initial commit'
-# git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
-# git push -u -f origin main
+
+PROJECT_NAME=$1
+if [[ ]]
+gh repo create $PROJECT_NAME --public
+git init
+git add -A
+git commit -m 'Initial commit'
+git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
+git push -u -f origin main
