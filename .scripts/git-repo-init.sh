4a495e02dfaa7948f8aedf8aa161d3b1b4f88f36
Eugene Shtoka <eshtoka@gmail.com>
2024-04-29 16:06:06 +0300

    update

---
+++ .scripts/git-repo-init.sh
@@ -0,0 +1,7 @@
+PROJECT_NAME=myAwesomeRepo
+gh repo create $PROJECT_NAME --public
+git init
+git add -A
+git commit -m 'Initial commit'
+git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
+git push -u -f origin main
