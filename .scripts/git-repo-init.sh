13d3d921e615b48d220295c7a9d3986e96fbb2a7
Eugene Shtoka <eshtoka@gmail.com>
2024-04-29 16:07:25 +0300

    update

--- .scripts/git-repo-init.sh
+++ .scripts/git-repo-init.sh
@@ -1,7 +1,10 @@
-PROJECT_NAME=myAwesomeRepo
-gh repo create $PROJECT_NAME --public
-git init
-git add -A
-git commit -m 'Initial commit'
-git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
-git push -u -f origin main
+#!/bin/zsh
+
+script_dir="$( cd "$( dirname "${(%):-%N}" )" && pwd )"
+# PROJECT_NAME=myAwesomeRepo
+# gh repo create $PROJECT_NAME --public
+# git init
+# git add -A
+# git commit -m 'Initial commit'
+# git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
+# git push -u -f origin main
