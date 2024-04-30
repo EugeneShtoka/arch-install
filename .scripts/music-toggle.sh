0630017a6cf696de43b3aeedd5959b39997c4766
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 12:43:41 +0300

    update

--- .scripts/music-toggle.sh
+++ .scripts/music-toggle.sh
@@ -3,7 +3,11 @@ pid=$(ps aux | grep vlc | grep -v grep | awk '{print $2}')
 if [ -z "$pid" ]; then
 	SCRIPTS_PATH/music-play.sh
 else
-    echo $pid
+    if [[ "$(ps -o state= -p $pid)" = "T" ]]; then
+        echo suspended
+    else
+        echo playing
+    fi
 fi
 
 #setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
