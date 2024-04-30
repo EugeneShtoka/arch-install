655975cd8fbbff421d77a9abe9a315d43b24ac40
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 12:45:00 +0300

    update

--- .scripts/music-toggle.sh
+++ .scripts/music-toggle.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 pid=$(ps aux | grep vlc | grep -v grep | awk '{print $2}')
 if [ -z "$pid" ]; then
-	SCRIPTS_PATH/music-play.sh
+	$SCRIPTS_PATH/music-play.sh
 else
     if [[ "$(ps -o state= -p $pid)" = "T" ]]; then
         kill -CONT $pid
