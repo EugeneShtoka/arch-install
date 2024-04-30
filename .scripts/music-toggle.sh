b98bd7c6add91e32d4f8cc8c373fad3a45433431
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 12:37:37 +0300

    update

--- .scripts/music-toggle.sh
+++ .scripts/music-toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 pid=${ps aux | grep vlc | grep -v grep | awk '{print $2}'}
-if [ -z "$1" ]; then
+if [ -z "$pid" ]; then
 	dir="$HOME/.config/rofi/launchers/type-4"
 	theme='style-9'
     choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
