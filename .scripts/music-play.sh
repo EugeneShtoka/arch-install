0cf5a1819ae6ab1ce0f703624f64081f26c29ab2
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 11:51:29 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-setsid cvlc --random $1 > /dev/null 2>&1 &
+if [ -z "$1" ]; then
+    PLAY_PATH=
+else
+    PLAY_PATH=$MUSIC_PATH/$1
+fi
+
+setsid cvlc --random $PLAY_PATH > /dev/null 2>&1 &
