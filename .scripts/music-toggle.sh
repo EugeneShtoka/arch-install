e62a74bf19734eada6ce188e0cfc5d041a5d020d
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 12:37:53 +0300

    update

--- .scripts/music-toggle.sh
+++ .scripts/music-toggle.sh
@@ -1,12 +1,9 @@
 #!/bin/zsh
 pid=${ps aux | grep vlc | grep -v grep | awk '{print $2}'}
 if [ -z "$pid" ]; then
-	dir="$HOME/.config/rofi/launchers/type-4"
-	theme='style-9'
-    choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
-    pathToPlay=$MUSIC_PATH/$choice
+	echo empty
 else
-    pathToPlay=$MUSIC_PATH/$1
+    echo $pid
 fi
 
 setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
