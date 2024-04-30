8a550ad0e31719d8b3971e0ea863578ef8268b98
Eugene Shtoka <eshtoka@gmail.com>
2024-04-30 12:03:00 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -4,12 +4,9 @@ if [ -z "$1" ]; then
 	dir="$HOME/.config/rofi/launchers/type-4"
 	theme='style-9'
     choice=$(ls $MUSIC_PATH -a | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
-    echo $choice
     pathToPlay=$MUSIC_PATH/$choice
 else
     pathToPlay=$MUSIC_PATH/$1
 fi
-
-echo $pathToPlay
 
 #setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
