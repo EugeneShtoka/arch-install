d5ff2595e4384c76e3d0040d1296fafd77c6abcf
Eugene Shtoka <eshtoka@gmail.com>
2024-05-02 08:45:01 +0300

    update

--- .scripts/monitor-setup-multi.sh
+++ .scripts/monitor-setup-multi.sh
@@ -8,7 +8,7 @@ monitorsCount=$(xrandr --listactivemonitors | grep 'Monitors:' | awk '{print $2}
 if [[ "$monitorsCount" -eq 1 ]]; then  
     echo "`date` connecting monitor $MONITOR_WORK_2" >> $LOG_PATH
     xrandr --output $MONITOR_WORK_2 --auto --right-of $MONITOR_LAPTOP
-    sleep 2
+    sleep 1
     echo "`date` connecting monitor $MONITOR_WORK_3" >> $LOG_PATH
     xrandr --output $MONITOR_WORK_3 --auto --right-of $MONITOR_WORK_2
     
