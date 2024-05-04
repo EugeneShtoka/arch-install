23551a394dedca174dc2ed3f5ed9235c2c9b5e5f
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 00:22:34 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -1,26 +1,6 @@
 #!/bin/zsh
 
 package=$1
-    SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
-    IFS=$'\n'      # Change IFS to newline char
-    searchResults=$(yay -Ss $package)
-    packages=$(echo $searchResults | awk 'NR % 2 == 1')
-    names="$(echo $packages | awk '{print $1}')"
-
-names=($(echo $packages | awk '{print $1}')) # split the `names` string into an array by the same name
-versions=($(echo $packages | awk '{print $2}'))
-descriptions=($(echo $searchResults | awk 'NR % 2 == 0'))
-    found=()
-    for (( i=${#names[@]}; i>=1; i-- )); do
-        found+=("${names[i]} ${versions[i]} ${descriptions[i]}")
-    done
-    dir="$HOME/.config/rofi/launchers/type-4"
-	theme='style-9a'
-	choice=$(printf '%s\n' "${found[@]}" | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
-    # for i in {1..$(echo $names | wc -l)}; do
-    #   echo ${names[i]} ${versions[i]} ${descriptions[i]}
-    # done
-
 official=$(pacman -Ss "^$package$" | wc -l)
 if [[ $official -gt 0 ]]; then
   #sudo pacman -S $package
