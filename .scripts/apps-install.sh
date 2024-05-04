b0528a342e5f6194cf28be2e71a25b8701f73692
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 21:32:30 +0300

    update

--- .scripts/apps-install.sh
+++ .scripts/apps-install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian
+yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian glab
 
 $SCRIPTS_PATH/yay-install.sh
 
@@ -8,7 +8,7 @@ $SCRIPTS_PATH/auto-yay.sh slack-desktop
 $SCRIPTS_PATH/auto-yay.sh zoom
 $SCRIPTS_PATH/auto-yay.sh mailspring
 $SCRIPTS_PATH/auto-yay.sh beeper
-$SCRIPTS_PATH/auto-yay.sh glab-git
+$SCRIPTS_PATH/auto-yay.sh -git
 $SCRIPTS_PATH/auto-yay.sh github-cli
 $SCRIPTS_PATH/auto-yay.sh qbittorrent-enhanced
 $SCRIPTS_PATH/auto-yay.sh visual-studio-code-bin
