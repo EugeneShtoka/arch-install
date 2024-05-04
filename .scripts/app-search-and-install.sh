b81416dd6c632902d8a6fc6066fad08ce4cbcf36
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 22:56:17 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -12,9 +12,8 @@ else
     echo "Package $package found in AUR."
   else
     echo "Package not found in official repositories or AUR."
-    yay -Ss $package | 
-    tail -n +2 | 
-    echo # Skip the first two header lines
+    echo $(yay -Ss $package | tail -n +2)
+    # Skip the first two header lines
     # while IFS=$'/' read -rA line; do
     #     for i in $fields; do
     #         echo -ne "${line[i]}\t"  # Print the selected field with a tab
