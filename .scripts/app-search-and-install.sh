f0ea50c66f11163ef619236d8d2663851a57b8f1
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 21:52:59 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -3,10 +3,10 @@
 package=$1
 if pacman -Ss "^$package$" &> /dev/null; then
   sudo pacman -S $package
-  echo "Package found in official repositories."
+  echo "Package $package found in official repositories."
 elif yay -Ss "^$package$" &> /dev/null; then
   yay -S $package
-  echo "Package found in AUR."
+  echo "Package $package found in AUR."
 else
   echo "Package not found in official repositories or AUR."
 fi
