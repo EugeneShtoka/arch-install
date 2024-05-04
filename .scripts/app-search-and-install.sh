8ae76361c093adfc272afe987b233e3d11b5bf4f
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 23:51:27 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -7,7 +7,10 @@ package=$1
     names=${(f)"$(echo $packages | awk '{print $1}')"}
     versions=${(f)$(echo $packages | awk '{print $2}')}
     descriptions=${(f)$(echo $searchResults | awk 'NR % 2 == 0')}
-    echo $(echo $names | wc -l)
+    echo 
+    for i in {1..$(echo $names | wc -l)}; do
+      echo $i
+    done
     for i in "${!names}"; do  # Get indices of array1
       echo ${names[i]} ${versions[i]} ${descriptions[i]}
     done
