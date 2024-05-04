b8fecb4f797ad03aa25b5706c4fa62c147b8f727
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 23:41:07 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -5,6 +5,7 @@ package=$1
     searchResults=$(yay -Ss $package)
     packages=$(echo $searchResults | awk 'NR % 2 == 1')
     packageNames=$(echo $packages | awk '{print $1}')
+    packageVersions=$(echo $packages | awk '{print $2}')
     echo $packageNames
 
 official=$(pacman -Ss "^$package$" | wc -l)
