3076b1d69155b10f60be9f50aa8c5a644edf3d53
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 18:58:11 +0300

    update

--- .scripts/monitor-setup-hook.sh
+++ .scripts/monitor-setup-hook.sh
@@ -5,5 +5,5 @@ source $USR_HOME/.env
 
 mode="$1"
 user="$(who -u | grep -F '(:0)' | head -n 1 | awk '{print $1}')"
-su -c "DISPLAY=:0.0 zsh -x $USR_HOME/.scripts/monitor-setup-$mode.sh" "$user"
+su -c "DISPLAY=:0.0 zsh -x $SCRIPTS_PATH/monitor-setup-$mode.sh" "$user"
 
