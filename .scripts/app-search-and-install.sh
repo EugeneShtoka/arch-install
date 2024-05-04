3a0825047789732fbc97daa4839bb4f6f4cbee4d
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 23:00:01 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -1,6 +1,17 @@
 #!/bin/zsh
 
 package=$1
+
+    yay -Ss $package | 
+    tail -n +2 | 
+    echo # Skip the first two header lines
+    while IFS=$'/' read -rA line; do
+        for i in $fields; do
+            echo -ne "${line[i]}\t"  # Print the selected field with a tab
+        done
+        echo # Newline
+    done 
+
 official=$(pacman -Ss "^$package$" | wc -l)
 if [[ $official -gt 0 ]]; then
   #sudo pacman -S $package
