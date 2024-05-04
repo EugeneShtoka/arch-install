c5d6b00d42d74178e4a62ba1a332c79f4b854839
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 00:16:21 +0300

    update

--- .scripts/app-search-and-install.sh
+++ .scripts/app-search-and-install.sh
@@ -1,16 +1,13 @@
 #!/bin/zsh
 
 package=$1
-    IFS=$'\n'
     searchResults=$(yay -Ss $package)
     packages=$(echo $searchResults | awk 'NR % 2 == 1')
     names="$(echo $packages | awk '{print $1}')"
 SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
-IFS=$'\n'      # Change IFS to newline char
 names=($(echo $packages | awk '{print $1}')) # split the `names` string into an array by the same name
 versions=($(echo $packages | awk '{print $2}'))
 descriptions=($(echo $searchResults | awk 'NR % 2 == 0'))
-IFS=$SAVEIFS   # Restore original IFS
     for (( i=1; i<${#names[@]}; i++ ))
     do
         echo "${names[i]} ${versions[i]} ${descriptions[i]}"
