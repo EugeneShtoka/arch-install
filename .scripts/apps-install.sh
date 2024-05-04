5088a6e62a744ab9e8d6de7fc4a74dd7f1e7d0b8
Eugene Shtoka <eshtoka@gmail.com>
2024-05-04 21:35:39 +0300

    update

--- .scripts/apps-install.sh
+++ .scripts/apps-install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian glab github-cli
+yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian glab github-cli qbittorrent
 
 $SCRIPTS_PATH/yay-install.sh
 
@@ -8,7 +8,6 @@ $SCRIPTS_PATH/auto-yay.sh slack-desktop
 $SCRIPTS_PATH/auto-yay.sh zoom
 $SCRIPTS_PATH/auto-yay.sh mailspring
 $SCRIPTS_PATH/auto-yay.sh beeper
-$SCRIPTS_PATH/auto-yay.sh qbittorrent-enhanced
 $SCRIPTS_PATH/auto-yay.sh visual-studio-code-bin
 $SCRIPTS_PATH/auto-yay.sh docker-desktop
 $SCRIPTS_PATH/auto-yay.sh cloud-sql-proxy
