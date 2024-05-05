00c73e241d1823906d0551c6270d6b33c0f49908
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 17:58:11 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -9,16 +9,16 @@ html=`cat $html_file`
 parts=(${(s/title>/)html})
 title=${parts[2]}
 
-# KEY=$(secret-tool lookup provider gemini key-pair secret)
-# echo $KEY
-# request=""
-# gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='$KEY
-# echo $gmnUrl
-# echo $request
-# contents='{"contents":[{"parts":[{"text":"From title get artist and song: '$title', json response, resonse in single line"}]}]}'
-# respose=$(curl -H 'Content-Type: application/json' -d $contents -X POST $gmnUrl)
-# resJson=$(echo $respose | jq '.candidates[0].content.parts[0].text')
-# artist=$(echo $(echo $resJson | jq 'fromjson | .artist'))
-# song=$(echo $(echo $resJson | jq 'fromjson | .song'))
-# yt-dlp -x --audio-format mp3 $url -o Music/$author/$song.mp3
-# id3v2 -a $author Music/$author/$song.mp3
+KEY=$(secret-tool lookup provider gemini key-pair secret)
+echo $KEY
+request=""
+gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='$KEY
+echo $gmnUrl
+echo $request
+contents='{"contents":[{"parts":[{"text":"From title get artist and song: '$title', json response, resonse in single line"}]}]}'
+respose=$(curl -H 'Content-Type: application/json' -d $contents -X POST $gmnUrl)
+resJson=$(echo $respose | jq '.candidates[0].content.parts[0].text')
+artist=$(echo $(echo $resJson | jq 'fromjson | .artist'))
+song=$(echo $(echo $resJson | jq 'fromjson | .song'))
+yt-dlp -x --audio-format mp3 $url -o Music/$author/$song.mp3
+id3v2 -a $author Music/$author/$song.mp3
