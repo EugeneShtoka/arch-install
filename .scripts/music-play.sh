3e6f899e064115626cf7696d74919c07f7b54d82
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 11:34:50 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -5,9 +5,11 @@ if [ -z "$1" ]; then
     choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
         pathToPlay=$MUSIC_PATH/$choice
+        setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
     fi
 else
     pathToPlay=$MUSIC_PATH/$1
+    setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
 fi
 
-setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
+
