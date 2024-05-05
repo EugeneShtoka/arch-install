238a563a83d7a557cef92a5cdb2893e118d8a197
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 12:39:32 +0300

    update

--- .scripts/check-music.sh
+++ .scripts/check-music.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for file in $MUSIC_PATH/*; do
+for file in $MUSIC_PATH/**/*; do
     str=$(file $file)
     parts=(${(s/,/)str})
     echo $parts[1] $parts[2] $parts[3] $parts[4] $parts[5] $parts[6] $parts[7]
