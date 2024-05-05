d6cbc4339205628ad0342456408c6c3e3c6e17ff
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 13:47:55 +0300

    update

--- .scripts/check-music.sh
+++ .scripts/check-music.sh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 
-for file in $MUSIC_PATH/**/*; do
-    str=$(file $file)
-    parts=(${(s/,/)str})
-    echo $parts[1] $parts[5]
-done
-
 author=$1
 song=$2
 url=$3
 yt-dlp -x --audio-format mp3 $url -o Music/$author/$song.mp3
+id3v2 -a $author Music/$author/$song.mp3
