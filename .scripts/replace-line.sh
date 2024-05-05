ad247948b443d6395a8a4f7daeb5affcd1372716
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 15:00:21 +0300

    update

--- .scripts/replace-line.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-OLD_LINE_PATTERN=$1;
-NEW_LINE=$2;
-FILE=$3
-
-NEW=$(echo "${NEW_LINE}" | sed 's/\//\\\//g')
-sed -i.bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
-mv "${FILE}.bak" /tmp/
