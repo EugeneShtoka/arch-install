f044b223e3fd0356e26c7629b9542c38fa9672a0
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 15:33:13 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -4,6 +4,10 @@ url=$1
 mkdir ~/yt-temp
 wget $url -O ~/yt-temp/song.html
 
+title=$(sed -n 's/.*<title>\(.*\)<\/title>.*/\1/p' "$html_file" | \
+        grep -Eo '<title>.*</title>' | \
+        sed -e 's/<[^>]*>//g')
+
 author=$1
 song=$2
 url=$3
