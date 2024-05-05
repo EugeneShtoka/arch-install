cf3c32e950e59e76866d596aca24a74c6e68b091
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 20:14:04 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -11,16 +11,12 @@ titleRaw=${parts[2]}
 title=${titleRaw::-2}
 
 KEY=$(secret-tool lookup provider gemini key-pair secret)
-echo $KEY
 gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='$KEY
-echo $gmnUrl
 contents='{"contents":[{"parts":[{"text":"From title get artist and song: '$title', json response, resonse in single line"}]}]}'
-echo \'$contents\'
 curl -H 'Content-Type: application/json' -d $contents -X POST $gmnUrl
 response=$(curl -H 'Content-Type: application/json' -d $contents -X POST $gmnUrl)
 resJson=$(echo $response | jq '.candidates[0].content.parts[0].text')
 artist=$(echo $(echo $resJson | jq 'fromjson | .artist' | tr -d '"'))
 song=$(echo $(echo $resJson | jq 'fromjson | .song' | tr -d '"'))
-echo $artist $song
 yt-dlp -x --audio-format mp3 $url -o Music/$artist/$song.mp3
 id3v2 -a $author Music/$author/$song.mp3
