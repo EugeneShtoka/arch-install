a8c67d40bd532d88446b0ef64c6038c081d8e884
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 20:56:27 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -14,8 +14,8 @@ if [[ -n $title ]]; then
     KEY=$(secret-tool lookup provider gemini key-pair secret)
     resJson=$(gemini-cli --key $KEY prompt "From title get artist and song: '$title', json response, resonse in single line")
     if [[ -n $resJson ]]; then
-        artist=$(echo $(echo $resJson | jq 'fromjson | .artist' | tr -d '"'))
-        song=$(echo $(echo $resJson | jq 'fromjson | .song' | tr -d '"'))
+        artist=$(echo $(echo $resJson | jq '.artist' | tr -d '"'))
+        song=$(echo $(echo $resJson | jq '.song' | tr -d '"'))
         if [[ -n $artist && -n $song ]]; then
             yt-dlp -x --audio-format mp3 $url -o Music/$artist/$song.mp3
             id3v2 -a $artist Music/$artist/$song.mp3
