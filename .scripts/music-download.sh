7a1a3e7b670308d20afe6539e2b349735223639e
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 17:06:47 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -14,7 +14,7 @@ echo $title
 KEY=$(secret-tool lookup provider gemini key-pair secret)
 echo $KEY
 request="From title get artist and song: $title, return result as json"
-gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=$KEY'
+gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='$KEY
 echo $gmnUrl
 echo $request
 respose=$(curl -H 'Content-Type: application/json' -d $'{"contents":[{"parts":[{"text":$request}]}]}' -X POST $gmnUrl)
