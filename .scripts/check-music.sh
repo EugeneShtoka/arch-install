b91bca52d11be98ee2150d56bbdf70e634e6e09d
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 12:40:00 +0300

    update

--- .scripts/check-music.sh
+++ .scripts/check-music.sh
@@ -3,5 +3,5 @@
 for file in $MUSIC_PATH/**/*; do
     str=$(file $file)
     parts=(${(s/,/)str})
-    echo $parts[1] $parts[2] $parts[3] $parts[4] $parts[5] $parts[6] $parts[7]
+    echo $parts[1] $parts[5]
 done
