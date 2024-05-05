af3449a943af58d4a763293a43961b638f168524
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 16:58:01 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -9,6 +9,7 @@ title=$(sed -n 's/.*<title>\(.*\)<\/title>.*/\1/p' "$html_file" | \
         grep -Eo '<title>.*</title>' | \
         sed -e 's/<[^>]*>//g')
 
+echo $title
 parseWithGemini=$(gemini 'From title get artist and song "$title", return result as json')
 
 KEY=$(secret-tool lookup provider gemini key-pair secret)
