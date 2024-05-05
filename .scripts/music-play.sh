c778d2b03abad5714d8848fe63275bc993fa251b
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 08:41:57 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -3,6 +3,10 @@ if [ -z "$1" ]; then
 	dir="$HOME/.config/rofi/launchers/type-4"
 	theme='style-9'
     choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+    if [[ -n $choice ]]; then
+        package="${$(echo $choice | awk '{print $1}')##*/}"
+        $SCRIPTS_PATH/app-search-and-install.sh $package
+    fi
     pathToPlay=$MUSIC_PATH/$choice
 else
     pathToPlay=$MUSIC_PATH/$1
