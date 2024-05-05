af6d2b8759408f97a6ef0a30702bcf0f4b801f94
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 16:48:56 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -10,8 +10,11 @@ title=$(sed -n 's/.*<title>\(.*\)<\/title>.*/\1/p' "$html_file" | \
 
 parseWithGemini=$(gemini 'From title get artist and song "$title", return result as json')
 
+curl -H 'Content-Type: application/json' -d '{"contents":[{"parts":[{"text":"From title get artist and song: $title, return result as json"}]}]}' -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=YOUR_API_KEY'
 author=$1
 song=$2
 url=$3
 yt-dlp -x --audio-format mp3 $url -o Music/$author/$song.mp3
 id3v2 -a $author Music/$author/$song.mp3
+
+secret-tool store --label="gemini key" provider gemini key-pair secret
