dfcc82b455aad5ffaea58bebc496563636c50546
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 08:42:11 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -4,10 +4,8 @@ if [ -z "$1" ]; then
 	theme='style-9'
     choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
-        package="${$(echo $choice | awk '{print $1}')##*/}"
-        $SCRIPTS_PATH/app-search-and-install.sh $package
+        pathToPlay=$MUSIC_PATH/$choice
     fi
-    pathToPlay=$MUSIC_PATH/$choice
 else
     pathToPlay=$MUSIC_PATH/$1
 fi
