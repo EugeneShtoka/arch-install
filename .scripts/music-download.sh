7c8a20d2cd43585f63839aa38ce2f2fa3dcf8c44
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 17:52:45 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -5,7 +5,7 @@ mkdir ~/yt-temp
 html_file=~/yt-temp/song.html
 #wget $url -O $html_file
 
-title=$(sed 's/.*\<title\>\(.*\)\</title\>.*/\1/g' $html_file)
+title=$(sed 's/.*\<title\>\(.*\)\<\/title\>.*/\1/g' $html_file)
 
 echo $title
 title="Manu Chao – Clandestino (Official Music Video)"
