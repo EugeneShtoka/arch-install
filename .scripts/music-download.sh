288c23ab55645a0390000aa7236adb3c88b25119
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 20:54:56 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -12,7 +12,7 @@ title=${titleRaw::-2}
 
 if [[ -n $title ]]; then
     KEY=$(secret-tool lookup provider gemini key-pair secret)
-    resJson=$(gemini-cli --key AIzaSyBX0F5dCV1VUNtblZ_Hy2j_grsd1e1uIU4 prompt "From title get artist and song: '$title', json response, resonse in single line")
+    resJson=$(gemini-cli --key $KEY prompt "From title get artist and song: '$title', json response, resonse in single line")
 
     gmnUrl='https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key='$KEY
     contents='{"contents":[{"parts":[{"text":"From title get artist and song: '$title', json response, resonse in single line"}]}]}'
