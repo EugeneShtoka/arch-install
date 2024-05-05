46edc57ea74f40c9a929c4090246aa6572a5d2f9
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 11:27:18 +0300

    update

--- .scripts/music-play.sh
+++ .scripts/music-play.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 if [ -z "$1" ]; then
 	dir="$HOME/.config/rofi/launchers/type-4"
-	theme='style-9'
+	theme='style-9-columns'
     choice=$(ls $MUSIC_PATH -a | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
         pathToPlay=$MUSIC_PATH/$choice
