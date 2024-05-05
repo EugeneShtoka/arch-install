1d09dd6755681105e6186f7c879083edcc1df78e
Eugene Shtoka <eshtoka@gmail.com>
2024-05-05 16:49:12 +0300

    update

--- .scripts/music-download.sh
+++ .scripts/music-download.sh
@@ -17,4 +17,4 @@ url=$3
 yt-dlp -x --audio-format mp3 $url -o Music/$author/$song.mp3
 id3v2 -a $author Music/$author/$song.mp3
 
-secret-tool store --label="gemini key" provider gemini key-pair secret
+secret-tool store --label="gemini key" provider gemini key-pair secret AIzaSyDJ7aZ-nVVpNzBgV2o3Mq3E5x5SDVhl404
