38d6590121cd16d09f45ec63483e784891812ab6
Eugene Shtoka <eshtoka@gmail.com>
2024-05-06 11:48:45 +0300

    update

--- .scripts/apps-install.sh
+++ .scripts/apps-install.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
-yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian glab github-cli qbittorrent hugo bind yt-dlp id3v2
-
+yes | sudo pacman -Syi inotify-tools thunar rofi bluz bluez-utils brightnessctl docker-compose gnome-keyring bitwarden-cli jq python-packaging gvfs p7zip dunst upower xclip cups ghostscript go adobe-source-code-pro-fonts ttf-nerd-fonts-symbols ttf-font-awesome terraform aws-cli kubectl expac maim vlc bc eza upx vivaldi vivaldi-ffmpeg-codecs npm rclone qemu-full obsidian glab github-cli qbittorrent hugo bind yt-dlp id3v2 eza
 $SCRIPTS_PATH/yay-install.sh
 
 $SCRIPTS_PATH/auto-yay.sh slack-desktop
