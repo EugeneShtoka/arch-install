522eaec1fe3c7d0fe531e107f0261d7e2bc79a14
Eugene Shtoka <eshtoka@gmail.com>
2024-05-07 11:04:01 +0300

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -23,7 +23,7 @@ alias git-alias-main='git symbolic-ref refs/heads/main refs/heads/$(git rev-pars
 
 alias home='cd ~ && code . && clear'
 alias figoro='cd $HOME/dev/figoro && code . && clear'
-alias figoro='cd $HOME/dev/blog && code . && clear'
+alias blog='cd $HOME/dev/blog && code . && clear'
 alias aps='cd $WORK_REPOS_PATH/swapp-aps && code . && clear'
 alias plugin='cd $WORK_REPOS_PATH/revit-plugin && code . && clear'
 alias workflows='cd $WORK_REPOS_PATH/workflows && code . && clear'
