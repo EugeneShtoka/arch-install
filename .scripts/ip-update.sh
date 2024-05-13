337cd77a0d08b812dcac795e853bf5169a5a1638
Eugene Shtoka <eshtoka@gmail.com>
2024-05-13 22:47:40 +0300

    update

--- .scripts/ip-update.sh
+++ .scripts/ip-update.sh
@@ -1,6 +1,7 @@
 cd ~/dev/cloudblock/aws/
 
 ip=$(curl -s http://checkip.amazonaws.com)
-sed -i -e "s#^mgmt_cidr = .*#mgmt_cidr = \"$1/32\"#" aws.tfvars
+echo $ip
+#sed -i -e "s#^mgmt_cidr = .*#mgmt_cidr = \"$1/32\"#" aws.tfvars
 
-terraform apply -var-file="aws.tfvars"
+#terraform apply -var-file="aws.tfvars"
