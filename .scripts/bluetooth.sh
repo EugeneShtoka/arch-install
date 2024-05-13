3bb5ded6032ee3e55bede7d4ae10fb5cec73e583
Eugene Shtoka <eshtoka@gmail.com>
2024-05-13 20:04:48 +0300

    update

--- .scripts/bluetooth.sh
+++ .scripts/bluetooth.sh
@@ -7,8 +7,8 @@ function is_bluetooth_connected() {
 }
 
 function show-headphones-message() {
-    blStatus=$(bluetooth_status)
-    name=$(echo $blStatus | grep -oP '(?<=Name: ).*' )
+    blStatus="$(bluetooth_status)"
+    name=$(echo "$blStatus" | grep -oP '(?<=Name: ).*' )
     battery_level=$(echo $blStatus | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
     message="$(get_battery_icon $battery_level) $battery_level% $(get_audio_status)"
     notify-send "$name $1" "$message" -i headphones
