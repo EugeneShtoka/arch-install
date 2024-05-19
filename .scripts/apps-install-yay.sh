c6195b2abbf2ffcb673ba2ec965b6216db44438a
Eugene Shtoka <eshtoka@gmail.com>
2024-05-19 10:18:18 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -58,3 +58,4 @@ $SCRIPTS_PATH/auto-yay.sh adw-gtk3
 $SCRIPTS_PATH/auto-yay.sh koodo-reader-bin
 $SCRIPTS_PATH/auto-yay.sh google-cloud-cli
 $SCRIPTS_PATH/auto-yay.sh google-cloud-cli-gke-gcloud-auth-plugin
+$SCRIPTS_PATH/auto-yay.sh musescore-midi
