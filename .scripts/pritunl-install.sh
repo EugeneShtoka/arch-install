2259f349eedcb88c75b16e4e75ad1b75e9e4fd49
Eugene Shtoka <eshtoka@gmail.com>
2024-06-09 08:22:36 +0300

    update

--- .scripts/pritunl-install.sh
+++ .scripts/pritunl-install.sh
@@ -5,5 +5,6 @@ EOF
 
 sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+
 sudo pacman -Sy
 sudo pacman -S pritunl-client-electron
