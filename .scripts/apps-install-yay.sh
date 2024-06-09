d2e4e164461f7ddef8a8fffc061ebd334e304cfa
Eugene Shtoka <eshtoka@gmail.com>
2024-06-09 08:19:40 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -63,7 +63,4 @@ $SCRIPTS_PATH/auto-yay.sh musescore-midi
 $SCRIPTS_PATH/auto-yay.sh proton
 $SCRIPTS_PATH/auto-yay.sh vault
 $SCRIPTS_PATH/auto-yay.sh openvpn
-$SCRIPTS_PATH/auto-yay.sh networkmanager
-$SCRIPTS_PATH/auto-yay.sh nm-connection-editor
-$SCRIPTS_PATH/auto-yay.sh networkmanager-openvpn
 $SCRIPTS_PATH/auto-yay.sh pritunl-client-electron
