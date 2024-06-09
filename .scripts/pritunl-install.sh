7e98ceaffc2d661001ead127c72a754f540f6079
Eugene Shtoka <eshtoka@gmail.com>
2024-06-09 08:26:50 +0300

    update

--- .scripts/pritunl-install.sh
+++ .scripts/pritunl-install.sh
@@ -1,14 +1,17 @@
-sudo tee -a /etc/pacman.conf << EOF
-[pritunl]
-Server = https://repo.pritunl.com/stable/pacman
-EOF
 
-sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 
-if [[ $(grep -c "\$Server = https://repo.pritunl.com/stable/pacman" "/etc/pacman.conf") -eq 0 ]]; then
-    echo "\$SCRIPTS_PATH/auto-yay.sh $package" >> $SCRIPTS_PATH/apps-install-yay.sh
-  fi
+if [[ $(grep -c "Server = https://repo.pritunl.com/stable/pacman" "/etc/pacman.conf") -eq 0 ]]; then
+    echo "[pritunl]" >> /etc/pacman.conf
+    echo "Server = https://repo.pritunl.com/stable/pacman" >> /etc/pacman.conf
+
+    sudo tee -a /etc/pacman.conf << EOF
+    [pritunl]
+    Server = https://repo.pritunl.com/stable/pacman
+    EOF
+
+    sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+    sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+fi
 
 sudo pacman -Sy
 sudo pacman -S pritunl-client-electron
