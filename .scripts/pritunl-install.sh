4eb69c78b148e80a0b53c6661970da06908a8b0a
Eugene Shtoka <eshtoka@gmail.com>
2024-06-09 08:26:55 +0300

    update

--- .scripts/pritunl-install.sh
+++ .scripts/pritunl-install.sh
@@ -4,11 +4,6 @@ if [[ $(grep -c "Server = https://repo.pritunl.com/stable/pacman" "/etc/pacman.c
     echo "[pritunl]" >> /etc/pacman.conf
     echo "Server = https://repo.pritunl.com/stable/pacman" >> /etc/pacman.conf
 
-    sudo tee -a /etc/pacman.conf << EOF
-    [pritunl]
-    Server = https://repo.pritunl.com/stable/pacman
-    EOF
-
     sudo pacman-key --keyserver hkp://keyserver.ubuntu.com -r 7568D9BB55FF9E5287D586017AE645C0CF8E292A
     sudo pacman-key --lsign-key 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 fi
