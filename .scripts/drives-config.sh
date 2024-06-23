cce15577c61adcd9bc7df2b789d5c02448b8bb15
Eugene Shtoka <eshtoka@gmail.com>
2024-06-23 10:32:12 +0300

    update

--- .scripts/drives-config.sh
+++ .scripts/drives-config.sh
@@ -1,10 +1,16 @@
 #!/bin/zsh
 
-#$SCRIPTS_PATH/drives-mount.sh
+$SCRIPTS_PATH/drives-mount.sh
 
 dirs=("dev" "Documents" "Downloads" "Library" "Music" "Notes" "Pictures" "Screenshots" "Torrents")
 for dir in "${dirs[@]}"; do
-    rm ~/$dir
-    mkdir ~/$dir
-    sudo mount --bind /mnt/Vault/$dir ~/$dir
+    rm -rf ~/$dir
+    ln -s /mnt/Vault/$dir ~/$dir
 done
+
+# Add dev entry to fstab
+{
+    echo ""
+    echo "# dev"
+    echo "/mnt/Vault/dev					/home/eugene/dev	none		bind	0  0"
+} | sudo tee -a /etc/fstab > /dev/null
