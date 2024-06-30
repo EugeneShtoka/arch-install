3e4d8afc7007a7f28cf85e80c173fa179b01b971
Eugene Shtoka <eshtoka@gmail.com>
2024-06-30 18:10:42 +0300

    update

--- .scripts/custom-scripts-auto-sync.sh
+++ .scripts/custom-scripts-auto-sync.sh
@@ -7,15 +7,3 @@ do
     echo "`date` custom-scripts-auto-sync $EVENT on $DIR$FILE" >> $LOG_PATH
     $SCRIPTS_PATH/custom-scripts-push.sh
 done
-
-inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.config/autokey | while read DIR EVENT FILE
-do
-    echo "`date` custom-scripts-auto-sync autokey $EVENT on $DIR$FILE" >> $LOG_PATH
-    $SCRIPTS_PATH/custom-scripts-push.sh
-done
-
-inotifywait -q -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.config/copyq/copyq-commands.ini $HOME/.config/copyq/copyq-filter.ini $HOME/.config/copyq/copyq.conf $HOME/.config/copyq/themes | while read DIR EVENT FILE
-do
-    echo "`date` custom-scripts-auto-sync copyq $EVENT on $DIR$FILE" >> $LOG_PATH
-    $SCRIPTS_PATH/custom-scripts-push.sh
-done
