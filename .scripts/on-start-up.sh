435dc37e4b75ae839263eb91d1160977d12950ee
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 16:51:05 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -8,6 +8,9 @@ $SCRIPTS_PATH/ssh-add-keys.sh
 # sleep 10
 $SCRIPTS_PATH/custom-scripts-pull.sh
 $SCRIPTS_PATH/sync-gdrive.sh $LIBRARY_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH
+$SCRIPTS_PATH/sync-gdrive.sh $MUSIC_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH
+$SCRIPTS_PATH/sync-gdrive.sh $DOCUMENTS_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH
+
 # sleep 10
 $SCRIPTS_PATH/custom-scripts-auto-sync.sh &
 $SCRIPTS_PATH/gdrive-auto-sync.sh $LIBRARY_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH &
