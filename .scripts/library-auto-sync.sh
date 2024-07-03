fa127d23bc79001d45b8f06201a72382d8def0b8
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 16:18:30 +0300

    update

--- .scripts/library-auto-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-echo "`date` library-auto-sync" >> $LOG_PATH
-inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $LIBRARY_PATH | while read DIR EVENT FILE
-do
-    echo "`date` library-auto-sync $EVENT on $DIR$FILE" >> $LOG_PATH
-    $SCRIPTS_PATH/library-sync-to-gdrive.sh
-done
