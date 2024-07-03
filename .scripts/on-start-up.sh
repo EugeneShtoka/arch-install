b647ba128d72d2b568149e422ae1048caf01b656
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 16:14:33 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -6,7 +6,6 @@ echo "`date` on-start-up $HOME $LOG_PATH $SCRIPTS_PATH" >> $LOG_PATH
 eval "$(ssh-agent)"
 $SCRIPTS_PATH/ssh-add-keys.sh
 sleep 10
-$SCRIPTS_PATH/library-sync-from-gdrive.sh
 $SCRIPTS_PATH/sync-gdrive.sh $LIBRARY_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH
 $SCRIPTS_PATH/custom-scripts-pull.sh
 sleep 10
