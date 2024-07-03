0af282962d69789842654d1321867e80f7e8a416
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 16:57:23 +0300

    update

--- .scripts/gdrive-auto-sync.sh
+++ .scripts/gdrive-auto-sync.sh
@@ -5,7 +5,7 @@ destination=$2
 
 echo "`date` gdrive-auto-sync from $source to $destination" >> $LOG_PATH
 
-$SCRIPTS_PATH/gdrive-sync.sh $LIBRARY_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH
+$SCRIPTS_PATH/gdrive-sync.sh $destination $source
 
 inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $source | while read DIR EVENT FILE
 do
