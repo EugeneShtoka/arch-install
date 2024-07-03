ce52b0b4939f32ccc54c5b89ffa149a4d0a42def
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 12:19:11 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -10,4 +10,4 @@ $SCRIPTS_PATH/library-sync-from-gdrive.sh
 $SCRIPTS_PATH/custom-scripts-pull.sh
 sleep 10
 setsid $SCRIPTS_PATH/library-auto-sync.sh &>> $LOG_PATH &
-setsid $SCRIPTS_PATH/custom-scripts-auto-sync.sh &>> $LOG_PATH &
+# setsid $SCRIPTS_PATH/custom-scripts-auto-sync.sh &>> $LOG_PATH &
