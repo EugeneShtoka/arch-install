e9492077ce4ef21fd49f155fbda2ac415e6a832d
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 13:34:41 +0300

    update

--- .scripts/custom-scripts-auto-sync.sh
+++ .scripts/custom-scripts-auto-sync.sh
@@ -2,10 +2,11 @@
 
 echo "`date` custom-scripts-auto-sync" >> $LOG_PATH
 
-inotifywait -q -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.gitignore $SCRIPTS_PATH | while read DIR EVENT FILE
+inotifywait -q -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $SCRIPTS_PATH | while read DIR EVENT FILE
 do
     echo "`date` custom-scripts-auto-sync $EVENT on $DIR$FILE" >> $LOG_PATH
     $SCRIPTS_PATH/custom-scripts-push.sh
 done
 
+# $HOME/.zshrc $HOME/.env $HOME/.gitignore
 # $HOME/.config/i3 $HOME/.config/systemd/user $SERVICES_PATH
