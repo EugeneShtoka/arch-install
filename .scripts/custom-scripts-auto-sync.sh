1d1a8b60d441ffa601fa67f6f73a7d98f1a6b003
Eugene Shtoka <eshtoka@gmail.com>
2024-07-03 12:23:42 +0300

    update

--- .scripts/custom-scripts-auto-sync.sh
+++ .scripts/custom-scripts-auto-sync.sh
@@ -2,8 +2,10 @@
 
 echo "`date` custom-scripts-auto-sync" >> $LOG_PATH
 
-inotifywait -q -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.gitignore $HOME/.config/i3 $HOME/.config/systemd/user $SCRIPTS_PATH $SERVICES_PATH | while read DIR EVENT FILE
+inotifywait -q -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM $HOME/.zshrc $HOME/.env $HOME/.gitignore $SCRIPTS_PATH | while read DIR EVENT FILE
 do
     echo "`date` custom-scripts-auto-sync $EVENT on $DIR$FILE" >> $LOG_PATH
     $SCRIPTS_PATH/custom-scripts-push.sh
 done
+
+# $HOME/.config/i3 $HOME/.config/systemd/user $SERVICES_PATH
--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -11,3 +11,4 @@ $SCRIPTS_PATH/custom-scripts-pull.sh
 sleep 10
 setsid $SCRIPTS_PATH/custom-scripts-auto-sync.sh &>> $LOG_PATH &
 # setsid $SCRIPTS_PATH/library-auto-sync.sh &>> $LOG_PATH &
+# test comment
