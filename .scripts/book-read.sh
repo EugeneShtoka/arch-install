d01541cbfbe5d8ec3d2b8cebd79321be2b9f7f77
Eugene Shtoka <eshtoka@gmail.com>
2024-07-17 12:34:18 +0300

    update

--- .scripts/book-read.sh
+++ .scripts/book-read.sh
@@ -5,9 +5,9 @@ if [ -z "$1" ]; then
     choice=$(ls $LIBRARY_PATH -a | grep -v '\.\.' | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
         pathToRead=$LIBRARY_PATH/$choice
-        setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
+        setsid koodo-reader $pathToPlay > /dev/null 2>&1 &
     fi
 else
     pathToPlay=$LIBRARY_PATH/$1
-    setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
+    setsid koodo-reader $pathToPlay > /dev/null 2>&1 &
 fi
