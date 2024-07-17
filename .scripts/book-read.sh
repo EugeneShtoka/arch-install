d867a9e1cd5cb17966924874407b91a1c1624459
Eugene Shtoka <eshtoka@gmail.com>
2024-07-17 12:50:39 +0300

    update

--- .scripts/book-read.sh
+++ .scripts/book-read.sh
@@ -1,13 +1,35 @@
 #!/bin/zsh
+dir="$HOME/.config/rofi/launchers/type-4"
+theme='style-9-columns'
+choice=$(ls $LIBRARY_PATH -a | grep -v '\.\.' | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+
 if [ -z "$1" ]; then
-	dir="$HOME/.config/rofi/launchers/type-4"
-	theme='style-9-columns'
-    choice=$(ls $LIBRARY_PATH -a | grep -v '\.\.' | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+	current_path=$LIBRARY_PATH
+else
+    current_path=$LIBRARY_PATH/$1
+fi
+
+while true; do
+    local choice=$(ls -a "$current_path" | grep -v '\.\.' | rofi -i -theme "$dir/$theme.rasi" -dmenu -matching prefix)
+
+    if [[ -z "$choice" ]]; then  # User cancelled
+        return 1 
+    fi
+
+    local full_path="$current_path/$choice"
+    if [[ -d "$full_path" ]]; then
+        current_path="$full_path"
+    elif [[ -f "$full_path" ]]; then
+        setsid koodo-reader "$full_path" >/dev/null 2>&1 & 
+        return 0  # Success
+    fi
+done
+
     if [[ -n $choice ]]; then
-        pathToRead=$LIBRARY_PATH/$choice
+        current_path=$LIBRARY_PATH/$choice
         setsid koodo-reader $pathToPlay > /dev/null 2>&1 &
     fi
 else
     pathToPlay=$LIBRARY_PATH/$1
     setsid koodo-reader $pathToPlay > /dev/null 2>&1 &
-fi
+
