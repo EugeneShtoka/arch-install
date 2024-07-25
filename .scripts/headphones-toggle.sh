70a49cba948ddbb84f38728e2d513ae363e2c5c0
Eugene Shtoka <eshtoka@gmail.com>
2024-07-25 15:31:33 +0300

    update

--- .scripts/headphones-toggle.sh
+++ .scripts/headphones-toggle.sh
@@ -8,7 +8,7 @@ echo "`date` switch-headphones" >> $LOG_PATH
 blStatus=$(bluetooth_status)
 
 if ([[ "$(bluetooth_status)" == *"Device $HEADPHONES_MAC_ADDR not available"* ]]); then
-    notify-send "Headphones not available" "$(get_audio_status)" -i $ICONS_PATH/headphones-disconnect -r 101047
+    notify-send "Headphones not available" "$(get_audio_status)" -i $ICONS_PATH/headphones-disconnect.png -r 101047
 else
     if [[ "$(is_bluetooth_connected)" == "yes" ]]; then
         show-headphones-message "disconnected"
