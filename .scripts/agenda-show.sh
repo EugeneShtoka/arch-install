a6c283f18c511ba359b8993d2ba5308d1e332c1f
Eugene Shtoka <eshtoka@gmail.com>
2024-07-25 15:29:28 +0300

    update

--- .scripts/agenda-show.sh
+++ .scripts/agenda-show.sh
@@ -10,5 +10,5 @@ elif [[ $daysToShow -eq 7 ]]; then
     title=Weekly
 fi
 
-notify-send "$title" "<span font='15px'>$(agenda $daysToShow)</span>" -i " " -r 101017
+notify-send "$title" "<span font='15px'>$(agenda $daysToShow)</span>" --icon " " -r 101017
 
