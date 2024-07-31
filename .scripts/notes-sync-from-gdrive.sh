8994496521982cf49aea38b7d176f6ae6bda092e
Eugene Shtoka <eshtoka@gmail.com>
2024-07-31 15:47:48 +0300

    update

--- .scripts/notes-sync-from-gdrive.sh
+++ .scripts/notes-sync-from-gdrive.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-rclone sync $GDRIVE_NAME:$NOTES_GDRIVE_PATH $NOTES_PATH  --exclude '{.git,.trash}/**' --exclude '.gitignore'
+rclone sync $GDRIVE_NAME:$NOTES_GDRIVE_PATH $NOTES_PATH --exclude '{.git,.trash}/**' --exclude '.gitignore'
