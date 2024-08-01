dbbce0e9fbf03c61f233e5bd50c7ac6c24639c61
Eugene Shtoka <eshtoka@gmail.com>
2024-08-01 16:15:59 +0300

    update

--- .scripts/gdrive-sync.sh
+++ .scripts/gdrive-sync.sh
@@ -3,5 +3,5 @@
 source=$1
 destination=$2
 
-echo "`date` gdrive-sync from $source to $destination" >> $LOG_PATH
-rclone sync $source $destination
+echo "$(date) gdrive-sync from $source to $destination" >>$LOG_PATH
+rclone sync $source $destination --exclude '{.git,.trash,.stfolder}/**' --exclude '.gitignore' --exclude '.syncthing*'
