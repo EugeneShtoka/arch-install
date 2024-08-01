b47a53000b188bf05a135732447a0e2e75d30e23
Eugene Shtoka <eshtoka@gmail.com>
2024-08-01 15:34:04 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -1,16 +1,17 @@
-#!/bin/zsh
+#!/bin/sh
 
 source ~/.env
 
-echo "`date` on-start-up $HOME $LOG_PATH $SCRIPTS_PATH" >> $LOG_PATH
+echo "$(date) on-start-up $HOME $LOG_PATH $SCRIPTS_PATH" >>"$LOG_PATH"
 eval "$(ssh-agent)"
-$SCRIPTS_PATH/ssh-add-keys.sh
+"$SCRIPTS_PATH"/ssh-add-keys.sh
 
 # sleep 10
-$SCRIPTS_PATH/custom-scripts-auto-sync.sh &
-$SCRIPTS_PATH/gdrive-auto-sync.sh $LIBRARY_PATH $GDRIVE_NAME:$LIBRARY_GDRIVE_PATH &
-$SCRIPTS_PATH/gdrive-auto-sync.sh $MUSIC_PATH $GDRIVE_NAME:$MUSIC_GDRIVE_PATH &
-$SCRIPTS_PATH/gdrive-auto-sync.sh $DOCUMENTS_PATH $GDRIVE_NAME:$DOCUMENTS_GDRIVE_PATH &
-$SCRIPTS_PATH/gdrive-auto-sync.sh $DOCUMENTS_GALINA_PATH $GDRIVE_GALINA_NAME:$DOCUMENTS_GALINA_GDRIVE_PATH &
+"$SCRIPTS_PATH"/custom-scripts-auto-sync.sh &
+"$SCRIPTS_PATH"/gdrive-auto-sync.sh "$LIBRARY_PATH" "$GDRIVE_NAME":"$LIBRARY_GDRIVE_PATH" &
+"$SCRIPTS_PATH"/gdrive-auto-sync.sh "$MUSIC_PATH" "$GDRIVE_NAME":"$MUSIC_GDRIVE_PATH" &
+"$SCRIPTS_PATH"/gdrive-auto-sync.sh "$DOCUMENTS_PATH" "$GDRIVE_NAME":"$DOCUMENTS_GDRIVE_PATH" &
+"$SCRIPTS_PATH"/gdrive-auto-sync.sh "$DOCUMENTS_GALINA_PATH" "$GDRIVE_GALINA_NAME":"$DOCUMENTS_GALINA_GDRIVE_PATH" &
+setsid syncthing 2>&1 &
 
 wait
