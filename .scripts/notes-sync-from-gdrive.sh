e9a67a973fec94f8bea86e889ec16570d925bd71
Eugene Shtoka <eshtoka@gmail.com>
2024-08-01 16:15:12 +0300

    update

--- .scripts/notes-sync-from-gdrive.sh
+++ .scripts/notes-sync-from-gdrive.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-rclone sync $GDRIVE_NAME:$NOTES_GDRIVE_PATH $NOTES_PATH --exclude '{.git,.trash,.stfolder}/**' --exclude '.gitignore' --exclude 'ttxfolder' --exclude '.syncthing*'
+rclone sync $GDRIVE_NAME:$NOTES_GDRIVE_PATH $NOTES_PATH --exclude '{.git,.trash,.stfolder}/**' --exclude '.gitignore' --exclude '.syncthing*'
