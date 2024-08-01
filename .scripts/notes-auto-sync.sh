597d7002b1cd5085d57119c6d5a1f96fefac74e8
Eugene Shtoka <eshtoka@gmail.com>
2024-08-01 15:42:54 +0300

    update

--- .scripts/notes-auto-sync.sh
+++ .scripts/notes-auto-sync.sh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 
-source=$1
-destination=$2
-
-echo "$(date) gdrive-auto-sync from $source to $destination" >>"$LOG_PATH"
+echo "$(date) notes-auto-sync" >>"$LOG_PATH"
 
 inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM "$source" | while read DIR EVENT FILE; do
     echo "$(date) gdrive-auto-sync $EVENT on $DIR$FILE" >>"$LOG_PATH"
