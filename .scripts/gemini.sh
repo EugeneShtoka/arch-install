a62470cf7c15a06c4d1e7ce2665310d2fac52430
Eugene Shtoka <eshtoka@gmail.com>
2024-08-01 12:12:15 +0300

    update

--- .scripts/gemini.sh
+++ .scripts/gemini.sh
@@ -2,5 +2,5 @@
 gemini() {
     PROMPT="$1"
     KEY=$(secret-tool lookup provider gemini key-pair secret)
-    echo "$(gemini-cli --key $KEY prompt $PROMPT)"
+    gemini-cli --key "$KEY" prompt "$PROMPT"
 }
