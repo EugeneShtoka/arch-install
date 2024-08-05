49970c1965ae8a462e46b3837dd2c5fbf9697412
Eugene Shtoka <eshtoka@gmail.com>
2024-08-05 12:22:58 +0300

    update

--- .scripts/ip-update.sh
+++ .scripts/ip-update.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 if [[ $(cat /proc/sys/kernel/hostname) != "archlinux-pc" ]]; then
-    echo 'Not home pc'
+    echo 'Not home PC'
     exit 0
 fi
 
@@ -27,4 +27,5 @@ yes | terraform apply -var-file="aws.tfvars"
 
 sudo sed -i -e "s#^nameserver .*#nameserver 10.0.0.1#" /etc/resolv.conf
 
+echo "Updated ip to $CURRENT_IP"
 echo $CURRENT_IP >$IP_FILE
