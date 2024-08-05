69e6c7b3f7ce8501d2ad317bb1b5231aeee9442f
Eugene Shtoka <eshtoka@gmail.com>
2024-08-05 11:56:14 +0300

    update

--- .scripts/ip-update.sh
+++ .scripts/ip-update.sh
@@ -2,7 +2,11 @@
 
 cd ~/dev/cloudblock/aws/
 
+sudo sed -i -e "s#^nameserver .*#nameserver 9.9.9.9#" /etc/resolv.conf
+
 ip=$(curl -s http://checkip.amazonaws.com)
 sed -i -e "s#^mgmt_cidr = .*#mgmt_cidr = \"$ip/32\"#" aws.tfvars
 
 yes | terraform apply -var-file="aws.tfvars"
+
+sudo sed -i -e "s#^nameserver .*#nameserver 10.0.0.1#" /etc/resolv.conf
