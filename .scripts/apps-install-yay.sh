faa4e771ffbbe7f7c2794b740f2e3cfda2e3d3c5
Eugene Shtoka <eshtoka@gmail.com>
2024-08-13 15:30:11 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -83,3 +83,4 @@ $SCRIPTS_PATH/auto-yay.sh openbooks-bin
 $SCRIPTS_PATH/auto-yay.sh libgen-cli-bin
 $SCRIPTS_PATH/auto-yay.sh tasker-permissions-git
 $SCRIPTS_PATH/auto-yay.sh android-sdk-platform-tools
+$SCRIPTS_PATH/auto-yay.sh fb2converter-bin
