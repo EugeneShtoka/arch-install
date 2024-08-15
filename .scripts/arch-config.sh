23d827eb993ac8a95fbfa1009bb7deef85140557
Eugene Shtoka <eshtoka@gmail.com>
2024-08-15 11:25:42 +0300

    update

--- .scripts/arch-config.sh
+++ .scripts/arch-config.sh
@@ -25,7 +25,7 @@ git remote set-url origin git@github.com:EugeneShtoka/arch-install.git
 $SCRIPTS_PATH/ssh-generate-key.sh $WORK_EMAIL work
 glab auth login
 read -p "Enter ssh key name for GitLab: (PC/Laptop)" keyName
-glab ssh-key add .ssh/id_ed25519_work.pub -t $keyName 
+glab ssh-key add .ssh/id_ed25519_work.pub -t $keyName
 
 mkdir .tmp
 
@@ -34,11 +34,11 @@ bw get item "gemini" | jq '.notes' | tr -d \" | secret-tool store --label="Gemin
 
 $SCRIPTS_PATH/atuin-install.sh
 
-bw get item 'SWAPP GCloud credentials' | jq '.notes' | jq 'fromjson' >> swapp-v1-1564402864804.json
+bw get item 'SWAPP GCloud credentials' | jq '.notes' | jq 'fromjson' >>swapp-v1-1564402864804.json
 sudo mkdir /usr/share/credentials
 sudo mv swapp-v1-1564402864804.json /usr/share/credentials/
 
-bw get item 'SWAPP GCS credentials' | jq '.notes' | jq 'fromjson' >> swapp-v1-1564402864804-storage.json
+bw get item 'SWAPP GCS credentials' | jq '.notes' | jq 'fromjson' >>swapp-v1-1564402864804-storage.json
 sudo mv swapp-v1-1564402864804-storage.json /usr/share/credentials/
 
 gcloud auth login
@@ -49,3 +49,5 @@ echo "AWS config"
 echo "AWS Access Key ID:" $(echo $awsTerraCreds | grep -A 1 '"secret access key"' | awk '{print $2}' | tail -1 | tr -d \",)
 echo "AWS Secret Access Key:" $(echo $awsTerraCreds | grep -A 1 '"access key"' | awk '{print $2}' | tail -1 | tr -d \",)
 aws configure
+
+conda config --set changeps1 false
