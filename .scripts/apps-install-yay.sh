08da32c1dcfbff8128c2d86467164bb111fa4017
Eugene Shtoka <eshtoka@gmail.com>
2024-08-26 12:24:20 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -88,6 +88,3 @@ $SCRIPTS_PATH/auto-yay.sh mambaforge
 $SCRIPTS_PATH/auto-yay.sh libcurl3-gnutls
 $SCRIPTS_PATH/auto-yay.sh steamcmd
 $SCRIPTS_PATH/auto-yay.sh ruby
-$SCRIPTS_PATH/auto-yay.sh afdko
-$SCRIPTS_PATH/auto-yay.sh python-ufonormalizer
-$SCRIPTS_PATH/auto-yay.sh python-ufonormalizer-git
