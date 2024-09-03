8a61ea8bcd065aa61024cc68594fcafb9c65a19c
Eugene Shtoka <eshtoka@gmail.com>
2024-09-03 14:56:00 +0300

    update

--- .scripts/passport-get.sh
+++ .scripts/passport-get.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 source $SCRIPTS_PATH/bw-unlock.sh
-bw get item 'passport-eugene' | jq '.notes'
-bw get item 'passport-galina' | jq '.notes'
+bw get item 'passport-eugene' | jq '.notes' | jq 'fromjson'
+bw get item 'passport-galina' | jq '.notes' | jq 'fromjson'
