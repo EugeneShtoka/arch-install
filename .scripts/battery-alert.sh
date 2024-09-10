f6b6c4e1cb7b609b8af018df95bde2fd72d7ec6d
Eugene Shtoka <eshtoka@gmail.com>
2024-09-10 23:31:19 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -4,8 +4,9 @@
 threshold=75
 
 # Get battery status using upower
+power_info=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
 battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | tr -d %)
-
+state=
 if [ $battery_level -le $threshold ]; then
     # Send a notification using your chosen daemon
     notify-send -u critical "Low Battery - $battery_level%" --icon " " -r 101029
