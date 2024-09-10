a29c65bb44d3fb8742b0550619135f4ebb3c9a16
Eugene Shtoka <eshtoka@gmail.com>
2024-09-10 23:34:19 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -7,7 +7,7 @@ threshold=75
 power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
 printf $power_info
 battery_level=$(echo $power_info | grep percentage | awk '{print $2}' | tr -d %)
-state=$(power_info | grep state | awk '{print $2}')
+state=$($power_info | grep state | awk '{print $2}')
 echo $battery_level $state
 
 if [ $battery_level -le $threshold ]; then
