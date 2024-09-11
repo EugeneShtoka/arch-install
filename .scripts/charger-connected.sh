704cb9654efd3b6f1b02b2884feb69e622bdaebd
Eugene Shtoka <eshtoka@gmail.com>
2024-09-11 21:43:35 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -1,1 +1,4 @@
+power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
+battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
+
 notify-send "$name $1" "$message" -i $ICONS_PATH/headphones.png -r 101029
