1413acc034af97daa6d3e348a77b67720558007b
Eugene Shtoka <eshtoka@gmail.com>
2024-09-11 21:26:49 +0300

    update

---
+++ .scripts/laptop-setup.sh
@@ -0,0 +1,10 @@
+# Create rule to auto-hook monitors on connect
+RULE_FILE="/etc/udev/rules.d/11-monitor.rules"
+CONTENT="SUBSYSTEM==\"drm\", ACTION==\"add\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh multi\"\nSUBSYSTEM==\"drm\", ACTION==\"remove\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh single\""
+
+echo -e "$CONTENT" | sudo tee "$RULE_FILE" > /dev/null
+
+# Reload udev rules to apply the changes
+sudo udevadm control --reload-rules && udevadm trigger
+
+echo "Udev rule file created/updated successfully!"
