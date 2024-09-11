9c9e252ad1e98eb65f0746f7aeed80825072b021
Eugene Shtoka <eshtoka@gmail.com>
2024-09-11 22:40:48 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# Battery information
+battery_level=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | tr -d %)
+
 # Battery icon, depending on battery level
 get_battery_icon() {
   if (( $1 >= 80 )); then
@@ -16,5 +19,5 @@ get_battery_icon() {
 }
 
 get_battery_info() {
-  echo "$(get_battery_icon $1) $1%%"
+  echo "$(get_battery_icon $1) $1%"
 }
