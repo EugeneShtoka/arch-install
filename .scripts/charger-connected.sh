2357d7bf08202864e23e6b8b8b7fc4adf779c99d
Eugene Shtoka <eshtoka@gmail.com>
2024-09-11 23:44:44 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -17,4 +17,4 @@ power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
 battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
 state=$(echo "$power_info" | grep state | awk '{print $2}')
 
-notify-send "Charging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
+notify-send "Charging $battery_level%" -i $ICONS_PATH/plug-solid.png -r 101029
