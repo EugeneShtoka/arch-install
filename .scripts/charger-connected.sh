02950ebc5bd9cdabc2e20a54219181c2f41811f5
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:13:33 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -3,18 +3,7 @@
 USR_HOME=/home/eugene
 source $USR_HOME/.env
 
-function root-notify-send() {
-    #Detect the name of the display in use
-    local display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-
-    #Detect the user using such display
-    local user=$(who | grep '('$display')' | awk '{print $1}' | head -n 1)
-
-    #Detect the id of the user
-    local uid=$(id -u $user)
-
-    sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send "$@"
-}
+source $SCRIPTS_PATH/root-notify-send.sh
 
 power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
 battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
