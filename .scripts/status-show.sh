523e7b1f039ccdf9da0715516c0a91b7ee1fae58
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:26:39 +0300

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -25,7 +25,7 @@ hardware_info="$(print_glyph 'f013') $cpu_usage $(print_glyph 'f2db') $ram_usage
 #   get_battery_info
 #   hardware_info="$(get_battery_status $battery_level $charge_state)$cpu_and_ram $hardware_info"
 # fi
-if [ "$IS_LAPTOP" -eq 1 ]; then
+if [ "$IS_LAPTOP" = 1 ]; then
   get_battery_info
   hardware_info="$(get_battery_status $battery_level $charge_state)$cpu_and_ram $hardware_info"
 fi
