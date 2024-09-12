c7d305a6528560f939799b18212d906db3ca352d
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:11:41 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -3,7 +3,7 @@
 USR_HOME=/home/eugene
 source $USR_HOME/.env
 
-function notify-send() {
+function root-notify-send() {
     #Detect the name of the display in use
     local display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 
@@ -20,4 +20,4 @@ power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
 battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
 state=$(echo "$power_info" | grep state | awk '{print $2}')
 
-notify-send "Charging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
+root-notify-send "Charging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
