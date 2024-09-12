bdb96f1c24e85820bc4f124da6195e982bdd43fc
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:17:52 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,5 +1,17 @@
 #!/bin/zsh
 
+
+my_function() {
+  power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
+  battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
+  state=$(echo "$power_info" | grep state | awk '{print $2}')
+
+  reply=("$battery_level" "$state")
+}
+
+my_function
+echo $reply[1] $reply[2]
+
 # Battery icon, depending on battery level
 get_battery_icon() {
   if (( "$2" = "charging" )); then
