c864aa639bd7370266eec2338b52b4b3467cb030
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:18:38 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -4,9 +4,8 @@ USR_HOME=/home/eugene
 source $USR_HOME/.env
 
 source $SCRIPTS_PATH/root-notify-send.sh
+source $SCRIPTS_PATH/battery.sh
 
-power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
-battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
-state=$(echo "$power_info" | grep state | awk '{print $2}')
-
+my_function
+echo $reply[1] $reply[2]
 root-notify-send "Charging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
