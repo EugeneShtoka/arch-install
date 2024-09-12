6f0e1e9baf03aad062b46b1168478ad79182c4f8
Eugene Shtoka <eshtoka@gmail.com>
2024-09-12 08:25:23 +0300

    update

--- .scripts/laptop-setup.sh
+++ .scripts/laptop-setup.sh
@@ -1,13 +1,13 @@
-# systemctl --user enable battery-alert.service
-# systemctl --user start battery-alert.service
-# systemctl --user enable battery-alert.timer
-# systemctl --user start battery-alert.timer
+systemctl --user enable battery-alert.service
+systemctl --user start battery-alert.service
+systemctl --user enable battery-alert.timer
+systemctl --user start battery-alert.timer
 
-# # Create rule to auto-hook monitors on connect
-# RULE_FILE="/etc/udev/rules.d/11-monitor.rules"
-# CONTENT="SUBSYSTEM==\"drm\", ACTION==\"add\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh multi\"\nSUBSYSTEM==\"drm\", ACTION==\"remove\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh single\""
+# Create rule to auto-hook monitors on connect
+RULE_FILE="/etc/udev/rules.d/11-monitor.rules"
+CONTENT="SUBSYSTEM==\"drm\", ACTION==\"add\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh multi\"\nSUBSYSTEM==\"drm\", ACTION==\"remove\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh single\""
 
-# echo -e "$CONTENT" | sudo tee "$RULE_FILE" > /dev/null
+echo -e "$CONTENT" | sudo tee "$RULE_FILE" > /dev/null
 
 RULE_FILE="/etc/udev/rules.d/12-charger.rules"
 CONTENT="SUBSYSTEM==\"power_supply\", ATTR{online}==\"1\", RUN+=\"/home/eugene/.scripts/charger-connected.sh\""
