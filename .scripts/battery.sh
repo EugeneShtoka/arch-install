5c1c96f9caf5f89ebd7a2fe6436aebd68c0cd9a7
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 10:30:04 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-
 get_battery_info() {
   power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
   battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
