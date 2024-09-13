36b388eb12ed2ca8df6b9aa6a956758eedb29c46
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 10:53:31 +0300

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,7 +1,7 @@
 source $SCRIPTS_PATH/wifi.sh
 
-echo get_wifi_icon 10
-echo get_wifi_icon 30
-echo get_wifi_icon 50
-echo get_wifi_icon 70
-echo get_wifi_icon 90
+echo $(get_wifi_icon 10)
+echo $(get_wifi_icon 30)
+echo $(get_wifi_icon 50)
+echo $(get_wifi_icon 70)
+echo $(get_wifi_icon 90)
