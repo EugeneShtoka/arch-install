e0aa6237edd37425ada09e879d153b0210f80cc4
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 11:13:39 +0300

    update

--- .scripts/laptop-setup.sh
+++ .scripts/laptop-setup.sh
@@ -4,7 +4,7 @@ systemctl --user enable battery-alert.timer
 systemctl --user start battery-alert.timer
 
 # Create rule to auto-hook monitors on connect
-RULE_FILE="/etc/udev/rules.d/11-monitor.rules"
+RULE_FILE="/etc/udev/rules.d/10-monitor.rules"
 CONTENT="SUBSYSTEM==\"drm\", ACTION==\"add\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh multi\"\nSUBSYSTEM==\"drm\", ACTION==\"remove\", RUN+=\"/home/eugene/.scripts/monitor-setup-hook.sh single\""
 
 echo -e "$CONTENT" | sudo tee "$RULE_FILE" > /dev/null
