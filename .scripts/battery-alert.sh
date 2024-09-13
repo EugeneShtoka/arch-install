ea446fe50e30b50567b541785d1e93eef49f74e0
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 11:16:16 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -15,3 +15,19 @@ if [ $((battery_level / 20)) -lt $prev_battery_level ]; then
     prev_battery_level=$((battery_level / 20))
     notify-send "$(get_battery_status $battery_level $discharging)" --icon " " -r 101033
 fi
+
+BATTERY_LEVEL_FILE="$HOME/.previous_battery_level"
+if [ -f "$BATTERY_LEVEL_FILE" ]; then
+    prev_battery_level=$(cat "$BATTERY_LEVEL_FILE")
+else
+    prev_battery_level=5
+fi
+
+# Compare first, then update
+if [ $((battery_level / 20)) -lt $prev_battery_level ]; then
+    echo AAAAAAAAAAAa
+    notify-send "$(get_battery_status $battery_level $discharging)" --icon " " -r 101033
+fi
+
+# Write updated level to file
+echo $((battery_level / 20)) > "$BATTERY_LEVEL_FILE"
