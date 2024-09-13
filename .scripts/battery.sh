fe95d28ec45bbb97009badc364ef4f59c7a61da0
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 08:29:16 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -12,7 +12,7 @@ get_battery_info() {
 # Battery icon, depending on battery level
 get_battery_icon() {
   if (( "$2" = "charging" )); then
-    printf "\uf1e6"
+    printf "\uf376"
   elif (( $1 >= 80 )); then
     printf "\uf240"
   elif (( $1 >= 60 )); then
