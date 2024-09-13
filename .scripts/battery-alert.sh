06f7125b1c40c61d1a02d6e995b30ea9918ee180
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 17:38:23 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -8,6 +8,9 @@ get_battery_info
 
 if [ $battery_level -le $threshold ] && [ "$discharging" = "discharging" ]; then
     notify-send -u critical "$(get_battery_status $battery_level $discharging) Low battery" --icon " " -r 101029
+    beep
+    beep
+    beep
 else
     dunstctl close 101029
 fi
