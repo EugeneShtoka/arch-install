b78939034c2c61a2b74a812b660442edf8975641
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 11:08:21 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -11,10 +11,10 @@ if [ $battery_level -le $threshold ] && [ "$discharging" = "discharging" ]; then
     #dunstctl close 101029
 fi
 
-echo $prev_battery_level $((battery_level / 20)) $($((battery_level / 20)) -lt $prev_battery_level)
+echo $prev_battery_level $((battery_level / 20))
 # Update environment variable with current level (divided by 20)
-export prev_battery_level=$((battery_level / 20))
 if [ $((battery_level / 20)) -lt $prev_battery_level ]; then
 echo AAAAAAAAAAAa
+    export prev_battery_level=$((battery_level / 20))
     notify-send "$(get_battery_status $battery_level $discharging)" --icon " " -r 101033
 fi
