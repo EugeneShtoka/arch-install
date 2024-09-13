61326d39aafda539b5fc04357fea4c5a8e895c08
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 08:42:52 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -14,7 +14,7 @@ get_battery_icon() {
   if (( "$2" = "charging" )); then
     printf "\uf1e6"
   elif (( $1 >= 80 )); then
-    printf "\uf240"
+    printf "󰁹"
   elif (( $1 >= 60 )); then
     printf "\uf241"
   elif (( $1 >= 40 )); then
