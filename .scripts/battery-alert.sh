36baefdc54f18a4817332f3211c5f354b0436488
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 10:16:23 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -5,8 +5,8 @@ source $SCRIPTS_PATH/battery.sh
 threshold=105
 get_battery_info
 
-if [ $battery_level -le $threshold ] && [ "$charge_state" != "charging" ]; then
-    notify-send -u critical "$(get_battery_status $battery_level $charge_state) Low battery" --icon " " -r 101029
+if [ $battery_level -le $threshold ] && [ "$charging" != "yes" ]; then
+    notify-send -u critical "$(get_battery_status $battery_level $charging) Low battery" --icon " " -r 101029
 else
     dunstctl close 101029
 fi
