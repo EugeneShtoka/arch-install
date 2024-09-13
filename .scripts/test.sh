bc4b6770c6b90929461856a73786ae2a5662f2c8
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 09:11:25 +0300

    update

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,14 +1,14 @@
 source $SCRIPTS_PATH/battery.sh
 
 get_battery_status 92 charging
-get_battery_status 83 "charging"
-get_battery_status 74 "charging"
-get_battery_status 65 "charging"
-get_battery_status 56 "charging"
-get_battery_status 47 "charging"
-get_battery_status 38 "charging"
-get_battery_status 29 "charging"
-get_battery_status 20 "charging"
-get_battery_status 11 "charging"
-get_battery_status 2 "charging"
+get_battery_status 83 discharging
+get_battery_status 74 discharging
+get_battery_status 65 discharging
+get_battery_status 56 discharging
+get_battery_status 47 discharging
+get_battery_status 38 discharging
+get_battery_status 29 discharging
+get_battery_status 20 discharging
+get_battery_status 11 discharging
+get_battery_status 2 discharging
 
