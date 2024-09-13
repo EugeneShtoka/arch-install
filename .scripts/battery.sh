3372fb72bea278fd2381ec17e55fbb8ec37bafbc
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 10:16:56 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -4,9 +4,9 @@
 get_battery_info() {
   power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
   battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
-  charge_state=$(echo "$power_info" | grep state | awk '{print $2}')
+  charging=$(echo "$power_info" | grep state | awk '{print $2}')
 
-  reply=("$battery_level" "$charge_state")
+  reply=("$battery_level" "$charging")
 }
 
 # Battery icon, depending on battery level
