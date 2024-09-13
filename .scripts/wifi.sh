1dde27ec80cb4d03012a14b7748e0dcdb7bb1a47
Eugene Shtoka <eshtoka@gmail.com>
2024-09-14 00:27:17 +0300

    update

--- .scripts/wifi.sh
+++ .scripts/wifi.sh
@@ -3,6 +3,6 @@
 get_wifi_icon() {
   local icons=( 󰤯 󰤟 󰤢 󰤥 󰤨 ) 
   local index=$(( $1 / 20 ))
-  echo $1 $index ${icons[index]}
+  echo $1 $index $icons ${icons[index]}
   printf "${icons[index]}" 
 }
