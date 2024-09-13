0037d4b60c258b7cc13559a9054da413d8ecdb86
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 11:02:45 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -5,12 +5,6 @@ source $SCRIPTS_PATH/battery.sh
 threshold=15
 get_battery_info
 
-# Compare with previous level (divided by 20) stored in environment variable
-if [ $((battery_level / 20)) -lt ${prev_battery_level:-0} ]; then
-    # Notify user (replace with your preferred notification method)
-    notify-send "$(get_battery_status $battery_level $discharging)" --icon " " -r 101033
-fi
-
 # Update environment variable with current level (divided by 20)
 export prev_battery_level=$((battery_level / 20))
 
