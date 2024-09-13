cd2026a6e36096f3c53ac28d33a4c99bed602c1a
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 08:35:47 +0300

    update

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -5,7 +5,7 @@ get_battery_info() {
   power_info="$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)"
   battery_level=$(echo "$power_info" | grep percentage | awk '{print $2}' | tr -d %)
   charge_state=$(echo "$power_info" | grep state | awk '{print $2}')
-echo $battery_level $charge_state
+
   reply=("$battery_level" "$charge_state")
 }
 
