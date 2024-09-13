cf64ce79e3e45acf745b543147f43aa3d6b3692b
Eugene Shtoka <eshtoka@gmail.com>
2024-09-13 13:27:21 +0300

    update

--- .scripts/status-show.sh
+++ .scripts/status-show.sh
@@ -15,7 +15,7 @@ get_wifi_signal_strength() {
 
   strength=$(( (current_signal - min_signal) * 100 / (max_signal - min_signal) ))
   echo $strength
-  echo $(get_wifi_icon $strength)
+  echo $(get_wifi_icon 100)
 }
 
 # CPU, RAM
