80190e8a4ad20b7f0464a2df0c5d548f5e03bd9a
Eugene Shtoka <eshtoka@gmail.com>
2024-09-18 08:08:25 +0300

    update

--- .scripts/notes-auto-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-echo "$(date) notes-auto-sync" >>"$LOG_PATH"
-
-inotifywait -q -r -m -e DELETE,CLOSE_WRITE,MOVED_TO,MOVED_FROM "$source" | while read DIR EVENT FILE; do
-    echo "$(date) notes-auto-sync $EVENT on $DIR$FILE" >>"$LOG_PATH"
-    "$SCRIPTS_PATH"/notes-save.sh
-done
