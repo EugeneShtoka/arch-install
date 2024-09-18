0a5418b53125c332f8ff428748ec4238a899cd65
Eugene Shtoka <eshtoka@gmail.com>
2024-09-18 08:08:17 +0300

    update

--- .scripts/notes-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-cd $NOTES_PATH
-git add .
-git commit -m update
-git push
--- .scripts/notes-save.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-nohup $SCRIPTS_PATH/notes-push.sh &>/dev/null
-"$SCRIPTS_PATH"/gdrive-auto-sync.sh "$NOTES_PATH" "$GDRIVE_NAME":"$NOTES_GDRIVE_PATH" &
