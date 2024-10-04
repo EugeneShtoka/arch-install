91eb7fec3ded8ed4bcd719bf3a96a6600a2a3ca0
Eugene Shtoka <eshtoka@gmail.com>
2024-10-04 11:40:31 +0300

    update

--- .scripts/ip-update.sh
+++ .scripts/ip-update.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# ip address of AWS pi-hole 51.17.181.48
+# IP Address of AWS pi-hole 51.17.181.48
 
 if [[ $(cat /proc/sys/kernel/hostname) != "archlinux-pc" ]]; then
     echo 'Not home PC'
