1067f18724a10739d17dfa3a2ee0dd5ce38c18ee
Eugene Shtoka <eshtoka@gmail.com>
2024-10-21 14:15:57 +0300

    update

--- .scripts/workspaces-work-setup.sh
+++ .scripts/workspaces-work-setup.sh
@@ -10,3 +10,7 @@ i3-msg "[workspace=$ws7] move workspace to output $MONITOR_WORK_2"
 i3-msg "[workspace=$ws8] move workspace to output $MONITOR_WORK_3"
 i3-msg "[workspace=$ws9] move workspace to output $MONITOR_WORK_3"
 i3-msg "[workspace=$ws10] move workspace to output $MONITOR_WORK_3"
+
+i3-msg workspace 1
+i3-msg workspace 10
+i3-msg workspace 4
