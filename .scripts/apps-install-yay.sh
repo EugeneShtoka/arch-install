9826bf3529b7971e61230face7f061f285152103
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 21:04:50 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -101,5 +101,6 @@ $SCRIPTS_PATH/auto-yay.sh meld
 $SCRIPTS_PATH/auto-yay.sh musescore-midi
 $SCRIPTS_PATH/auto-yay.sh vault
 $SCRIPTS_PATH/auto-yay.sh gnome-themes-extra
-$SCRIPTS_PATH/auto-yay.sh gnome-themes-extra
+$SCRIPTS_PATH/auto-yay.sh adwaita-qt5-git
+$SCRIPTS_PATH/auto-yay.sh adwaita-qt6-git
 $SCRIPTS_PATH/auto-yay.sh gnome-themes-extra
