844a0cb3e8b02f35ed729662b4003441905f6286
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 11:03:58 +0300

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-alias mail='setsid mailspring --password-store="gnome-libsecret" &>/dev/null'
+alias mail=
 alias slack='setsid /usr/bin/slack -s &>/dev/null'
 alias messenger='setsid beeper --no-sandbox --default-frame &>/dev/null'
 alias zoom='setsid /usr/bin/zoom &>/dev/null'
