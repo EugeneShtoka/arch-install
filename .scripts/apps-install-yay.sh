513e65423b7507f281e93ff6b20c728d20cf3bb3
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 14:34:52 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -98,5 +98,5 @@ $SCRIPTS_PATH/auto-yay.sh yarn
 # $SCRIPTS_PATH/auto-yay.sh jitsi
 $SCRIPTS_PATH/auto-yay.sh meld
 $SCRIPTS_PATH/auto-yay.sh musescore-midi
-
+$SCRIPTS_PATH/auto-yay.sh vault
 $SCRIPTS_PATH/auto-yay.sh proton
