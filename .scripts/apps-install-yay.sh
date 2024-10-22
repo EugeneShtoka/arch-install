1cda6b813d9e5153d8d0bb13b70da4a3b9a71341
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 20:05:28 +0300

    sync

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -1,104 +1,107 @@
 #!/bin/zsh
 
-# $SCRIPTS_PATH/pritunl-install.sh
-# $SCRIPTS_PATH/qbittorrent-install.sh
+$SCRIPTS_PATH/pritunl-install.sh
+$SCRIPTS_PATH/qbittorrent-install.sh
 
-# $SCRIPTS_PATH/auto-yay.sh inotify-tools
-# $SCRIPTS_PATH/auto-yay.sh thunar
-# $SCRIPTS_PATH/auto-yay.sh rofi
-# $SCRIPTS_PATH/auto-yay.sh bluez
-# $SCRIPTS_PATH/auto-yay.sh bluez-utils
-# $SCRIPTS_PATH/auto-yay.sh brightnessctl
-# $SCRIPTS_PATH/auto-yay.sh copyq
-# $SCRIPTS_PATH/auto-yay.sh docker
-# $SCRIPTS_PATH/auto-yay.sh docker-compose
-# $SCRIPTS_PATH/auto-yay.sh lazydocker
-# $SCRIPTS_PATH/auto-yay.sh gnome-keyring
-# $SCRIPTS_PATH/auto-yay.sh bitwarden-cli
-# $SCRIPTS_PATH/auto-yay.sh python-packaging
-# $SCRIPTS_PATH/auto-yay.sh gvfs
-# $SCRIPTS_PATH/auto-yay.sh p7zip
-# $SCRIPTS_PATH/auto-yay.sh dunst
-# $SCRIPTS_PATH/auto-yay.sh upower
-# $SCRIPTS_PATH/auto-yay.sh xclip
-# $SCRIPTS_PATH/auto-yay.sh cups
-# $SCRIPTS_PATH/auto-yay.sh ghostscript
-# $SCRIPTS_PATH/auto-yay.sh go
-# $SCRIPTS_PATH/auto-yay.sh hugo
-# $SCRIPTS_PATH/auto-yay.sh bind
-# $SCRIPTS_PATH/auto-yay.sh yt-dlp
-# $SCRIPTS_PATH/auto-yay.sh id3v2
-# $SCRIPTS_PATH/auto-yay.sh ttf-adobe-source-code-pro-fonts
-# $SCRIPTS_PATH/auto-yay.sh ttf-nerd-fonts-symbols
-# $SCRIPTS_PATH/auto-yay.sh ttf-font-awesome
-# $SCRIPTS_PATH/auto-yay.sh terraform
-# $SCRIPTS_PATH/auto-yay.sh aws-cli
-# $SCRIPTS_PATH/auto-yay.sh kubectl
-# $SCRIPTS_PATH/auto-yay.sh jq
-# $SCRIPTS_PATH/auto-yay.sh maim
-# $SCRIPTS_PATH/auto-yay.sh pipewire-jack
-# $SCRIPTS_PATH/auto-yay.sh vlc
-# $SCRIPTS_PATH/auto-yay.sh bc
-# $SCRIPTS_PATH/auto-yay.sh eza
-# $SCRIPTS_PATH/auto-yay.sh upx
-# $SCRIPTS_PATH/auto-yay.sh vivaldi
-# $SCRIPTS_PATH/auto-yay.sh vivaldi-ffmpeg-codecs
-# $SCRIPTS_PATH/auto-yay.sh npm
-# $SCRIPTS_PATH/auto-yay.sh rclone
-# $SCRIPTS_PATH/auto-yay.sh qemu-full
-# $SCRIPTS_PATH/auto-yay.sh obsidian
-# $SCRIPTS_PATH/auto-yay.sh glab
-# $SCRIPTS_PATH/auto-yay.sh github-cli
-# $SCRIPTS_PATH/auto-yay.sh expac
-# $SCRIPTS_PATH/auto-yay.sh slack-desktop
-# $SCRIPTS_PATH/auto-yay.sh zoom
-# $SCRIPTS_PATH/auto-yay.sh mailspring-bin
-# $SCRIPTS_PATH/auto-yay.sh beeper-latest-bin
-# $SCRIPTS_PATH/auto-yay.sh visual-studio-code-bin
-# $SCRIPTS_PATH/auto-yay.sh cloud-sql-proxy
-# $SCRIPTS_PATH/auto-yay.sh autokey-gtk
-# $SCRIPTS_PATH/auto-yay.sh autokey-common
-# $SCRIPTS_PATH/auto-yay.sh postman-bin
-# $SCRIPTS_PATH/auto-yay.sh adw-gtk3
-# $SCRIPTS_PATH/auto-yay.sh koodo-reader-bin
-# $SCRIPTS_PATH/auto-yay.sh google-cloud-cli
-# $SCRIPTS_PATH/auto-yay.sh google-cloud-cli-gke-gcloud-auth-plugin
-# $SCRIPTS_PATH/auto-yay.sh openvpn
-# $SCRIPTS_PATH/auto-yay.sh fzf
-# $SCRIPTS_PATH/auto-yay.sh rust
-# $SCRIPTS_PATH/auto-yay.sh atuin-git
-# $SCRIPTS_PATH/auto-yay.sh qt6-multimedia-ffmpeg
-# $SCRIPTS_PATH/auto-yay.sh anki
-# $SCRIPTS_PATH/auto-yay.sh mop
-# $SCRIPTS_PATH/auto-yay.sh syncthing
-# $SCRIPTS_PATH/auto-yay.sh neovim
-# $SCRIPTS_PATH/auto-yay.sh ripgrep
-# $SCRIPTS_PATH/auto-yay.sh lazygit
-# $SCRIPTS_PATH/auto-yay.sh gdu
-# $SCRIPTS_PATH/auto-yay.sh bottom
-# $SCRIPTS_PATH/auto-yay.sh dosfstools
-# $SCRIPTS_PATH/auto-yay.sh sysstat
-# $SCRIPTS_PATH/auto-yay.sh gparted
-# $SCRIPTS_PATH/auto-yay.sh libgen-cli-bin
-# $SCRIPTS_PATH/auto-yay.sh tasker-permissions-git
-# $SCRIPTS_PATH/auto-yay.sh android-sdk-platform-tools
-# $SCRIPTS_PATH/auto-yay.sh fb2converter-bin
-# $SCRIPTS_PATH/auto-yay.sh mambaforge
-# $SCRIPTS_PATH/auto-yay.sh libcurl3-gnutls
-# $SCRIPTS_PATH/auto-yay.sh steamcmd
-# $SCRIPTS_PATH/auto-yay.sh ruby
-# $SCRIPTS_PATH/auto-yay.sh ruff
-# $SCRIPTS_PATH/auto-yay.sh sox
-# $SCRIPTS_PATH/auto-yay.sh cuetools
-# $SCRIPTS_PATH/auto-yay.sh xdotool
-# $SCRIPTS_PATH/auto-yay.sh zip
-# $SCRIPTS_PATH/auto-yay.sh shntool
-# $SCRIPTS_PATH/auto-yay.sh ffmpeg
-# $SCRIPTS_PATH/auto-yay.sh wine
-# $SCRIPTS_PATH/auto-yay.sh yarn
-# # $SCRIPTS_PATH/auto-yay.sh jitsi
-# $SCRIPTS_PATH/auto-yay.sh meld
+$SCRIPTS_PATH/auto-yay.sh inotify-tools
+$SCRIPTS_PATH/auto-yay.sh thunar
+$SCRIPTS_PATH/auto-yay.sh rofi
+$SCRIPTS_PATH/auto-yay.sh bluez
+$SCRIPTS_PATH/auto-yay.sh bluez-utils
+$SCRIPTS_PATH/auto-yay.sh brightnessctl
+$SCRIPTS_PATH/auto-yay.sh copyq
+$SCRIPTS_PATH/auto-yay.sh docker
+$SCRIPTS_PATH/auto-yay.sh docker-compose
+$SCRIPTS_PATH/auto-yay.sh lazydocker
+$SCRIPTS_PATH/auto-yay.sh gnome-keyring
+$SCRIPTS_PATH/auto-yay.sh bitwarden-cli
+$SCRIPTS_PATH/auto-yay.sh python-packaging
+$SCRIPTS_PATH/auto-yay.sh gvfs
+$SCRIPTS_PATH/auto-yay.sh p7zip
+$SCRIPTS_PATH/auto-yay.sh dunst
+$SCRIPTS_PATH/auto-yay.sh upower
+$SCRIPTS_PATH/auto-yay.sh xclip
+$SCRIPTS_PATH/auto-yay.sh cups
+$SCRIPTS_PATH/auto-yay.sh ghostscript
+$SCRIPTS_PATH/auto-yay.sh go
+$SCRIPTS_PATH/auto-yay.sh hugo
+$SCRIPTS_PATH/auto-yay.sh bind
+$SCRIPTS_PATH/auto-yay.sh yt-dlp
+$SCRIPTS_PATH/auto-yay.sh id3v2
+$SCRIPTS_PATH/auto-yay.sh ttf-adobe-source-code-pro-fonts
+$SCRIPTS_PATH/auto-yay.sh ttf-nerd-fonts-symbols
+$SCRIPTS_PATH/auto-yay.sh ttf-font-awesome
+$SCRIPTS_PATH/auto-yay.sh terraform
+$SCRIPTS_PATH/auto-yay.sh aws-cli
+$SCRIPTS_PATH/auto-yay.sh kubectl
+$SCRIPTS_PATH/auto-yay.sh jq
+$SCRIPTS_PATH/auto-yay.sh maim
+$SCRIPTS_PATH/auto-yay.sh pipewire-jack
+$SCRIPTS_PATH/auto-yay.sh vlc
+$SCRIPTS_PATH/auto-yay.sh bc
+$SCRIPTS_PATH/auto-yay.sh eza
+$SCRIPTS_PATH/auto-yay.sh upx
+$SCRIPTS_PATH/auto-yay.sh vivaldi
+$SCRIPTS_PATH/auto-yay.sh vivaldi-ffmpeg-codecs
+$SCRIPTS_PATH/auto-yay.sh npm
+$SCRIPTS_PATH/auto-yay.sh rclone
+$SCRIPTS_PATH/auto-yay.sh qemu-full
+$SCRIPTS_PATH/auto-yay.sh obsidian
+$SCRIPTS_PATH/auto-yay.sh glab
+$SCRIPTS_PATH/auto-yay.sh github-cli
+$SCRIPTS_PATH/auto-yay.sh expac
+$SCRIPTS_PATH/auto-yay.sh slack-desktop
+$SCRIPTS_PATH/auto-yay.sh zoom
+$SCRIPTS_PATH/auto-yay.sh mailspring-bin
+$SCRIPTS_PATH/auto-yay.sh beeper-latest-bin
+$SCRIPTS_PATH/auto-yay.sh visual-studio-code-bin
+$SCRIPTS_PATH/auto-yay.sh cloud-sql-proxy
+$SCRIPTS_PATH/auto-yay.sh autokey-gtk
+$SCRIPTS_PATH/auto-yay.sh autokey-common
+$SCRIPTS_PATH/auto-yay.sh postman-bin
+$SCRIPTS_PATH/auto-yay.sh adw-gtk3
+$SCRIPTS_PATH/auto-yay.sh koodo-reader-bin
+$SCRIPTS_PATH/auto-yay.sh google-cloud-cli
+$SCRIPTS_PATH/auto-yay.sh google-cloud-cli-gke-gcloud-auth-plugin
+$SCRIPTS_PATH/auto-yay.sh openvpn
+$SCRIPTS_PATH/auto-yay.sh fzf
+$SCRIPTS_PATH/auto-yay.sh rust
+$SCRIPTS_PATH/auto-yay.sh atuin-git
+$SCRIPTS_PATH/auto-yay.sh qt6-multimedia-ffmpeg
+$SCRIPTS_PATH/auto-yay.sh anki
+$SCRIPTS_PATH/auto-yay.sh mop
+$SCRIPTS_PATH/auto-yay.sh syncthing
+$SCRIPTS_PATH/auto-yay.sh neovim
+$SCRIPTS_PATH/auto-yay.sh ripgrep
+$SCRIPTS_PATH/auto-yay.sh lazygit
+$SCRIPTS_PATH/auto-yay.sh gdu
+$SCRIPTS_PATH/auto-yay.sh bottom
+$SCRIPTS_PATH/auto-yay.sh dosfstools
+$SCRIPTS_PATH/auto-yay.sh sysstat
+$SCRIPTS_PATH/auto-yay.sh gparted
+$SCRIPTS_PATH/auto-yay.sh libgen-cli-bin
+$SCRIPTS_PATH/auto-yay.sh tasker-permissions-git
+$SCRIPTS_PATH/auto-yay.sh android-sdk-platform-tools
+$SCRIPTS_PATH/auto-yay.sh fb2converter-bin
+$SCRIPTS_PATH/auto-yay.sh mambaforge
+$SCRIPTS_PATH/auto-yay.sh libcurl3-gnutls
+$SCRIPTS_PATH/auto-yay.sh steamcmd
+$SCRIPTS_PATH/auto-yay.sh ruby
+$SCRIPTS_PATH/auto-yay.sh ruff
+$SCRIPTS_PATH/auto-yay.sh sox
+$SCRIPTS_PATH/auto-yay.sh cuetools
+$SCRIPTS_PATH/auto-yay.sh xdotool
+$SCRIPTS_PATH/auto-yay.sh zip
+$SCRIPTS_PATH/auto-yay.sh shntool
+$SCRIPTS_PATH/auto-yay.sh ffmpeg
+$SCRIPTS_PATH/auto-yay.sh wine
+$SCRIPTS_PATH/auto-yay.sh yarn
+$SCRIPTS_PATH/auto-yay.sh jitsi
+$SCRIPTS_PATH/auto-yay.sh meld
 $SCRIPTS_PATH/auto-yay.sh musescore-midi
 $SCRIPTS_PATH/auto-yay.sh vault
+$SCRIPTS_PATH/auto-yay.sh lib32-nvidia-utils
 $SCRIPTS_PATH/auto-yay.sh proton
 $SCRIPTS_PATH/auto-yay.sh gnome-themes-standard
+$SCRIPTS_PATH/auto-yay.sh afdko
+$SCRIPTS_PATH/auto-yay.sh python-ufonormalizer
