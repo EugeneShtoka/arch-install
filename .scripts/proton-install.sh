2f2abd4da9077b352b55c4a6e2f333fa0b32ada7
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 21:01:20 +0300

    update

--- .scripts/proton-install.sh
+++ .scripts/proton-install.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
 $SCRIPTS_PATH/auto-yay.sh vault
+$SCRIPTS_PATH/auto-yay.sh afdko
+$SCRIPTS_PATH/auto-yay.sh python-ufonormalizer
 $SCRIPTS_PATH/auto-yay.sh lib32-nvidia-utils
 $SCRIPTS_PATH/auto-yay.sh proton
