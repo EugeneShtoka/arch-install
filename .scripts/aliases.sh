038fc60af516b339e886ab59a2e6c625504549bd
Eugene Shtoka <eshtoka@gmail.com>
2024-10-22 11:10:12 +0300

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+alias browser=$SCRIPTS_PATH/git-squash.sh
 alias slack='setsid /usr/bin/slack -s &>/dev/null'
 alias messenger='setsid beeper --no-sandbox --default-frame &>/dev/null'
 alias zoom='setsid /usr/bin/zoom &>/dev/null'
@@ -9,7 +10,6 @@ alias file-manager='setsid thunar &>/dev/null'
 alias keyboard-trainer='setsid tipp10 &>/dev/null'
 alias music-notation='setsid /usr/bin/env XDG_SESSION_TYPE=x11 mscore &>/dev/null'
 
-alias browser=$SCRIPTS_PATH/git-squash.sh
 alias cs-pull=$SCRIPTS_PATH/custom-scripts-pull.sh
 alias cs-push=$SCRIPTS_PATH/custom-scripts-push.sh
 alias mail=$SCRIPTS_PATH/email-start.sh
