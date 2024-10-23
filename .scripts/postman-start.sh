ca297959f283adbac87e1160febc9214f917f257
Eugene Shtoka <eshtoka@gmail.com>
2024-10-23 18:36:27 +0300

    update

--- .scripts/postman-start.sh
+++ .scripts/postman-start.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 setsid /opt/postman/Postman &>/dev/null & # <--- Note the extra & at the end
-
+notify-send start
 # Wait for Postman window to appear
 while ! wmctrl -lx | grep -q "postman.Postman"; do
     sleep 0.1
 done
-
+notify-send opened
 sleep 1.5 # Additional delay if needed
 wmctrl -a Postman
 xdotool Mod4+f
