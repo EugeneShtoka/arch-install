78de284c1e932d58d77abf50cec2c6b907e1689f
Eugene Shtoka <eshtoka@gmail.com>
2024-10-24 19:40:40 +0300

    update

--- .scripts/book-read.sh
+++ .scripts/book-read.sh
@@ -3,7 +3,7 @@ dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-9'
 
 if [ -z "$1" ]; then
-	current_path=$LIBRARY_PATH
+    current_path=$LIBRARY_PATH
 else
     current_path=$1
 fi
@@ -11,15 +11,15 @@ fi
 while true; do
     local choice=$(ls -a "$current_path" | grep -v "^\.$" | rofi -i -theme "$dir/$theme.rasi" -dmenu -matching prefix)
 
-    if [[ -z "$choice" ]]; then  # User cancelled
-        return 1 
+    if [[ -z "$choice" ]]; then # User cancelled
+        return 1
     fi
 
     local full_path="$current_path/$choice"
     if [[ -d "$full_path" ]]; then
         current_path="$full_path"
     elif [[ -f "$full_path" ]]; then
-        setsid koodo-reader "$full_path" >/dev/null 2>&1 &
+        setsid cr3 "$full_path" >/dev/null 2>&1 &
         return 0
     fi
 done
