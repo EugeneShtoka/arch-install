fe9751d7fec534643a40bf520e39d71a6de1c3f0
Eugene Shtoka <eshtoka@gmail.com>
2024-11-09 15:02:02 +0200

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -107,3 +107,4 @@ $SCRIPTS_PATH/auto-yay.sh adwaita-qt5-git
 $SCRIPTS_PATH/auto-yay.sh adwaita-qt6-git
 $SCRIPTS_PATH/auto-yay.sh gnome-themes-extra
 $SCRIPTS_PATH/auto-yay.sh kanata-bin
+$SCRIPTS_PATH/auto-yay.sh kmonad
