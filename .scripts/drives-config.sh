cffb5fd2028d37042cff9931a415a733497412c7
Eugene Shtoka <eshtoka@gmail.com>
2025-01-15 15:50:06 +0200

    update

--- .scripts/drives-config.sh
+++ .scripts/drives-config.sh
@@ -3,16 +3,8 @@
 $SCRIPTS_PATH/drives-mount.sh
 VAULT_PATH=/mnt/Vault
 
-dirs=("Downloads" "Torrents")
-for dir in "${dirs[@]}"; do
-    rm -rf ~/$dir
-    ln -s $VAULT_PATH/$dir ~/$dir
-done
-
 mounts=("dev" "Documents" "Galina's Documents" "Library" "Music" "Notes" "Pictures" "Photo" "Screenshots")
 for mount in "${mounts[@]}"; do
-    rm -rf $HOME/$mount
-    mkdir $HOME/$mount
 
     escaped_vault_path=$(echo "$VAULT_PATH/$mount" | sed 's/ /\\040/g') 
     escaped_home_path=$(echo "$HOME/$mount" | sed 's/ /\\040/g') 
