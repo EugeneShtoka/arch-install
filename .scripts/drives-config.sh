aa7a601c0e7ee6a5c84215a7e7db6d84c9d35038
Eugene Shtoka <eshtoka@gmail.com>
2025-01-16 20:11:11 +0200

    update

--- .scripts/drives-config.sh
+++ .scripts/drives-config.sh
@@ -15,8 +15,6 @@ for mount in "${mounts[@]}"; do
     # rm -rf $HOME/$mount
     # mkdir $HOME/$mount
 
-    echo "$VAULT_PATH/$mount"
-    echo $(echo "$VAULT_PATH/$mount" | sed 's/ /\\040/g') 
     escaped_vault_path=$(echo "$VAULT_PATH/$mount" | sed 's/o/\\040/g') 
     escaped_home_path=$(echo "$HOME/$mount" | sed 's/ /\\040/g') 
     echo "Mounting $escaped_vault_path to $escaped_home_path"
