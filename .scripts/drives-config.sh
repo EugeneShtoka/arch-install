d21df9347836e276e105431be875ae505e566b82
Eugene Shtoka <eshtoka@gmail.com>
2025-01-16 20:17:49 +0200

    update

--- .scripts/drives-config.sh
+++ .scripts/drives-config.sh
@@ -12,12 +12,11 @@ VAULT_PATH=/mnt/Vault
 #mounts=("dev" "Documents" "Galina's Documents" "Library" "Music" "Notes" "Pictures" "Photo" "Screenshots")
 mounts=("Galina's Documents2")
 for mount in "${mounts[@]}"; do
-    # rm -rf "$HOME/$mount"
-    # mkdir "$HOME/$mount"
+    rm -rf "$HOME/$mount"
+    mkdir "$HOME/$mount"
 
     escaped_vault_path=$(echo "$VAULT_PATH/$mount" | sed 's/ /\\\\040/g') 
     escaped_home_path=$(echo "$HOME/$mount" | sed 's/ /\\\\040/g') 
-    echo "Mounting $escaped_vault_path to $escaped_home_path"
     # Add dev entry to fstab
     {
         echo ""
