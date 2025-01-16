e70ec2df72eec1a0ec67fe8d5815817866a17ccc
Eugene Shtoka <eshtoka@gmail.com>
2025-01-16 20:10:56 +0200

    update

--- .scripts/drives-config.sh
+++ .scripts/drives-config.sh
@@ -17,7 +17,7 @@ for mount in "${mounts[@]}"; do
 
     echo "$VAULT_PATH/$mount"
     echo $(echo "$VAULT_PATH/$mount" | sed 's/ /\\040/g') 
-    escaped_vault_path=$(echo "$VAULT_PATH/$mount" | sed 's/o/\\040/g') 
+    escaped_vault_path=$(echo "$VAULT_PATH/$mount" | sed 's/ /\\040/g') 
     escaped_home_path=$(echo "$HOME/$mount" | sed 's/ /\\040/g') 
     echo "Mounting $escaped_vault_path to $escaped_home_path"
     # Add dev entry to fstab
