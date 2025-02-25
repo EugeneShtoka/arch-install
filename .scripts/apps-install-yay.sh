a6a42b81fad00bc89758ea0500966b297e466f9d
Eugene Shtoka <eshtoka@gmail.com>
2025-02-25 14:57:17 +0200

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -73,7 +73,6 @@ $SCRIPTS_PATH/auto-yay.sh dosfstools
 $SCRIPTS_PATH/auto-yay.sh sysstat
 $SCRIPTS_PATH/auto-yay.sh libgen-cli-bin
 $SCRIPTS_PATH/auto-yay.sh fb2converter-bin
-$SCRIPTS_PATH/auto-yay.sh mambaforge
 $SCRIPTS_PATH/auto-yay.sh libcurl3-gnutls
 $SCRIPTS_PATH/auto-yay.sh steamcmd
 $SCRIPTS_PATH/auto-yay.sh ruby
