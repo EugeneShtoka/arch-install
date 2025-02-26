202900ace80557b646e5a4943b7866398795fb38
Eugene Shtoka <eshtoka@gmail.com>
2025-02-26 22:18:37 +0200

    update

--- .scripts/notes-start.sh
+++ .scripts/notes-start.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-setsid zsh -c "env LANGUAGE=en_GB && /home/eugene/.scripts/ssh-init.sh && obsidian" &>/dev/null
+setsid zsh -c "env LANGUAGE=en_GB && /home/eugene/.scripts/ssh-init.sh && ssh-add -l && obsidian" &>/dev/null
 
