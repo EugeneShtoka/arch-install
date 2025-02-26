4dca6bc5382e55087b05a1629cc11f0045fea4ee
Eugene Shtoka <eshtoka@gmail.com>
2025-02-26 22:43:53 +0200

    update

--- .scripts/ssh-init.sh
+++ .scripts/ssh-init.sh
@@ -7,6 +7,3 @@ check-ssh-agent || export SSH_AUTH_SOCK=~/.tmp/ssh-agent.sock
 check-ssh-agent || eval "$(ssh-agent -s -a ~/.tmp/ssh-agent.sock)" > /dev/null
 
 $SCRIPTS_PATH/ssh-add-keys.sh
-notify-send "SSH_INIT" "<span font='15px'>INITIALIZING SSH $SSH_AUTH_SOCK $SCRIPTS_PATH</span>" --icon " " -r 101017
-
-notify-send "test-inner" "<span font='15px'>$(ssh-add -l)</span>" --icon " " -r 101047 
