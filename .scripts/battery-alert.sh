bc4507fa800aec072cf39333215132b1702d53c0
Eugene Shtoka <eshtoka@gmail.com>
2025-03-02 14:04:52 +0200

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -6,7 +6,7 @@ source $SCRIPTS_PATH/beep.sh
 threshold=15
 get_battery_info
 
-printf "%s\n" "$battery_level"
+printf "%s\n" "[ $battery_level -le $threshold ]"
 if [ $battery_level -le $threshold ] && [ "$discharging" = "discharging" ]; then
     notify-send -u critical "$(get_battery_status $battery_level $discharging) Low battery" --icon " " -r 101029
     beep 0.03 440
