f6fe50cb88b275a6e3c0b5c87858ab268ee59eeb
Eugene Shtoka <eshtoka@gmail.com>
2025-04-20 15:16:12 +0300

    update

--- .scripts/battery-alert.sh
+++ .scripts/battery-alert.sh
@@ -7,7 +7,7 @@ threshold=15
 get_battery_info
 
 notify-send "[ $battery_level -le $threshold ]"  --icon " " -r 101030
-if [ $battery_level -le $threshold ] && [ "$discharging" = 'discharging' ]; then
+if [ $battery_level -le $threshold ] && [ "$discharging" = "discharging" ]; then
     notify-send -u critical "$(get_battery_status $battery_level $discharging) Low battery" --icon " " -r 101029
     beep 0.03 440
     beep 0.03 440
