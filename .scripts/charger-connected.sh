d639e424ae847dd8676feab36e9a09418f89b733
Eugene Shtoka <eshtoka@gmail.com>
2025-04-20 15:12:14 +0300

    update

--- .scripts/charger-connected.sh
+++ .scripts/charger-connected.sh
@@ -7,4 +7,6 @@ source $SCRIPTS_PATH/root-notify-send.sh
 source $SCRIPTS_PATH/battery.sh
 
 get_battery_info
-root-notify-send "Charging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
+
+
+root-notify-send "Charging $battery_level% $discharging" -i $ICONS_PATH/plug.png -r 101029
