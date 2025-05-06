6ae16fe004f91eaefcc0bc52d5350535531954c6
Eugene Shtoka <eshtoka@gmail.com>
2025-05-06 21:02:59 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -96,3 +96,4 @@ $SCRIPTS_PATH/auto-yay.sh nvm
 $SCRIPTS_PATH/auto-yay.sh pnpm
 $SCRIPTS_PATH/auto-yay.sh xorg-xrandr
 $SCRIPTS_PATH/auto-yay.sh pinta$SCRIPTS_PATH/auto-yay.sh picard
+$SCRIPTS_PATH/auto-yay.sh shntool
