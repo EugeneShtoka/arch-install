f3d0d9ea3a8a39acb00cf5f41d118e79051e9df0
Eugene Shtoka <eshtoka@gmail.com>
2025-05-08 17:30:45 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -98,3 +98,4 @@ $SCRIPTS_PATH/auto-yay.sh xorg-xrandr
 $SCRIPTS_PATH/auto-yay.sh pinta$SCRIPTS_PATH/auto-yay.sh picard
 $SCRIPTS_PATH/auto-yay.sh shntool
 $SCRIPTS_PATH/auto-yay.sh mac
+$SCRIPTS_PATH/auto-yay.sh jami-qt
