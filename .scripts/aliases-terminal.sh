484f6ac6805882c7863a8df84c8d272a36dfda48
Eugene Shtoka <eshtoka@gmail.com>
2025-05-11 11:39:43 +0300

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -19,7 +19,7 @@ alias ghr='git reset --hard HEAD^ && git pull'
 alias gsq=$SCRIPTS_PATH/git-squash.sh
 alias gpc=$SCRIPTS_PATH/git-push-commit.sh
 alias gcpr=$SCRIPTS_PATH/git-create-pull-request.sh
-alias git-alias-work-branch='git symbolic-ref refs/heads/$GIT_DEFAULT_BRANCH refs/heads/$(git rev-parse --abbrev-ref HEAD)'
+alias git-alias-default-branch='git symbolic-ref refs/heads/$GIT_DEFAULT_BRANCH refs/heads/$(git rev-parse --abbrev-ref HEAD)'
 
 alias ls='eza --icons -a'
 alias ll='eza --icons -al'
