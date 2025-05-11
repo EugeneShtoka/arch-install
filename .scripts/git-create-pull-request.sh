cae5e770e198b183f61ac4c5a1284c77cd96112d
Eugene Shtoka <eshtoka@gmail.com>
2025-05-11 11:32:12 +0300

    update

--- .scripts/git-create-pull-request.sh
+++ .scripts/git-create-pull-request.sh
@@ -131,16 +131,11 @@ mr_description="$title" # Start with the title as the base description
 
 if [[ -n "$jira_ticket" ]]; then
   if [[ -n "$JIRA_BASE_URL" ]]; then
-    # JIRA_BASE_URL environment variable is set, so append the link
-    # Ensure no double slashes if JIRA_BASE_URL ends with one and jira_ticket starts with one (unlikely for ticket IDs)
-    # Basic concatenation is usually fine here.
     mr_description="$mr_description\n\n[Jira Ticket]($JIRA_BASE_URL/$jira_ticket)"
     echo "Info: JIRA link will be added to the description using JIRA_BASE_URL."
   else
-    # --jira-ticket was provided, but JIRA_BASE_URL is not set
     echo "Warning: A JIRA ticket ('$jira_ticket') was provided, but the JIRA_BASE_URL environment variable is not set." >&2
     echo "         The JIRA link will NOT be added to the merge/pull request description." >&2
-    # mr_description remains as just the title in this specific scenario if JIRA_BASE_URL is missing
   fi
 fi
 
