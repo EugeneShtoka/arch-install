fe6f6175bcd6e1074edcb3c49908980b92d3db8f
Eugene Shtoka <eshtoka@gmail.com>
2025-05-12 11:54:39 +0300

    update

--- .scripts/music-play-list.sh
+++ .scripts/music-play-list.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]; then
 	dir="$HOME/.config/rofi/launchers/type-4"
 	theme='style-9-wide'
-    choice=$(find "$MUSIC_PATH" -type f -name "*.xspf" -printf "%f\n" | sed "s/${xspf}$//" | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+    choice=$(find "$MUSIC_PATH" -type f -name "*.xspf" -printf "%f\n" | sed "s/xspf$//" | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
         pathToPlay="$MUSIC_PATH/$choice.xspf"
         setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
