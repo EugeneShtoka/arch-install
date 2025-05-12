dd2ff36aea497907a36aa3241912b4ab4d30d869
Eugene Shtoka <eshtoka@gmail.com>
2025-05-12 11:53:41 +0300

    update

--- .scripts/music-play-list.sh
+++ .scripts/music-play-list.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
 	theme='style-9-wide'
     choice=$(find "$MUSIC_PATH" -type f -name "*.xspf" -printf "%f\n" | sed "s/${playlist_extension}$//" | rofi -i -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
     if [[ -n $choice ]]; then
-        pathToPlay=$MUSIC_PATH/$choice
+        pathToPlay="$MUSIC_PATH/$choice.xspf"
         setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
     fi
 else
