24b208a3c08697a5e4e3c3dc3dc6f1e77f674dc9
Eugene Shtoka <eshtoka@gmail.com>
2025-05-14 16:54:21 +0300

    update

--- .scripts/git-create-pull-request.sh
+++ .scripts/git-create-pull-request.sh
@@ -24,8 +24,8 @@ fetch_jira_ticket_summary() {
   echo "Info: Fetching summary from JIRA API: $api_url" >&2
 
   local http_response
-  echo 'curl --silent --write-out "HTTPSTATUS:%{http_code}" "$web_url"'
-  http_response=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" "$web_url") # Added a generic User-Agent
+  echo 'curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Accept: application/json" "$web_url"'
+  http_response=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Accept: application/json" "$web_url") # Added a generic User-Agent
 
   echo "$http_response" >> jira_page_response.log
   local http_status=$(echo "$http_response" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
