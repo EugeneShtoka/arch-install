ecb7b1dcbdb40d0db0e82cac6cd3f05b29f9f0f4
Eugene Shtoka <eshtoka@gmail.com>
2025-05-14 16:49:01 +0300

    update

--- .scripts/git-create-pull-request.sh
+++ .scripts/git-create-pull-request.sh
@@ -24,12 +24,7 @@ fetch_jira_ticket_summary() {
   echo "Info: Fetching summary from JIRA API: $api_url" >&2
 
   local http_response
-  http_response=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" \
-    -H "Accept: application/json" \
-    "$api_url")
-
   http_response=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -L \
-    "${curl_auth_args[@]}" \
     -A "Mozilla/5.0 (compatible; Script Fetcher)" \
     "$web_url") # Added a generic User-Agent
 
