0475cd28481f97eddafd841cbd7baf8c5c87bed3
Eugene Shtoka <eshtoka@gmail.com>
2025-05-18 22:03:05 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -100,3 +100,4 @@ $SCRIPTS_PATH/auto-yay.sh shntool
 $SCRIPTS_PATH/auto-yay.sh mac
 $SCRIPTS_PATH/auto-yay.sh jami-qt
 $SCRIPTS_PATH/auto-yay.sh gemini-cli-go-git
+$SCRIPTS_PATH/auto-yay.sh lib32-nvidia-utils
