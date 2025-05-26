d6a5670238e90b6a4a2c226a2cb35caf9a6b00df
Eugene Shtoka <eshtoka@gmail.com>
2025-05-26 18:49:23 +0300

    update

--- .scripts/media-player-controller.sh
+++ .scripts/media-player-controller.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# --- D-Bus Configuration for VLC ---
 DBUS_DEST="org.mpris.MediaPlayer2.vlc"
 DBUS_PATH="/org/mpris/MediaPlayer2"
 DBUS_PLAYER_INTERFACE="org.mpris.MediaPlayer2.Player"
@@ -14,9 +13,7 @@ STATUS_RAW_OUTPUT=$(dbus-send --session --print-reply --dest="$DBUS_DEST" "$DBUS
 DBUS_GET_STATUS_EXIT_CODE=$?
 
 if [ $DBUS_GET_STATUS_EXIT_CODE -ne 0 ]; then
-    echo "Error getting VLC status (exit code $DBUS_GET_STATUS_EXIT_CODE)."
-    echo "D-Bus output: $STATUS_RAW_OUTPUT"
-    echo "==> ACTION: Assuming VLC is not playing. Trigger your script to START PLAYING music."
+    $SCRIPTS_PATH/music-player-list.sh
 else
     dbus-send --session --type=method_call --dest="$DBUS_DEST" "$DBUS_PATH" "$DBUS_PLAYER_INTERFACE.PlayPause" >/dev/null
 fi
