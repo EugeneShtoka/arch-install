02045e989d6a48a764204649d4cdd70f12f6f1fb
Eugene Shtoka <eshtoka@gmail.com>
2025-05-26 18:59:58 +0300

    update

--- .scripts/music-toggle.sh
+++ .scripts/music-toggle.sh
@@ -1,13 +1,3 @@
 #!/bin/zsh
-pid=$(ps aux | grep vlc | grep -v grep | awk '{print $2}')
-if [ -z "$pid" ]; then
-	$SCRIPTS_PATH/music-play.sh
-else
-    if [[ "$(ps -o state= -p $pid)" = "T" ]]; then
-        kill -CONT $pid
-    else
-        kill -STOP $pid
-    fi
-fi
 
-#setsid cvlc --random $pathToPlay > /dev/null 2>&1 &
+dbus-send --session --type=method_call --dest="org.mpris.MediaPlayer2.vlc" "/org/mpris/MediaPlayer2" "org.mpris.MediaPlayer2.Player.PlayPause" >/dev/null
