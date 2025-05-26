35b44d5cd247ca95a8f9dfabfbd91c6901162506
Eugene Shtoka <eshtoka@gmail.com>
2025-05-26 18:49:29 +0300

    update

--- .scripts/media-player-controller.sh
+++ .scripts/media-player-controller.sh
@@ -13,7 +13,7 @@ STATUS_RAW_OUTPUT=$(dbus-send --session --print-reply --dest="$DBUS_DEST" "$DBUS
 DBUS_GET_STATUS_EXIT_CODE=$?
 
 if [ $DBUS_GET_STATUS_EXIT_CODE -ne 0 ]; then
-    $SCRIPTS_PATH/music-player-list.sh
+    $SCRIPTS_PATH/music-play-list.sh
 else
     dbus-send --session --type=method_call --dest="$DBUS_DEST" "$DBUS_PATH" "$DBUS_PLAYER_INTERFACE.PlayPause" >/dev/null
 fi
