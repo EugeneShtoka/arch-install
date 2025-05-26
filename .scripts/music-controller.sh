fd9f6c407df5f275a658e9babba9130ba6c88e9c
Eugene Shtoka <eshtoka@gmail.com>
2025-05-26 18:59:25 +0300

    update

--- .scripts/music-controller.sh
+++ .scripts/music-controller.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-DBUS_DEST="org.mpris.MediaPlayer2.vlc"
-DBUS_PATH="/org/mpris/MediaPlayer2"
-DBUS_PLAYER_INTERFACE="org.mpris.MediaPlayer2.Player"
-DBUS_PROPERTIES_INTERFACE="org.freedesktop.DBus.Properties"
-
-echo "Attempting to get VLC playback status via D-Bus..."
-
-STATUS_RAW_OUTPUT=$(dbus-send --session --print-reply --dest="$DBUS_DEST" "$DBUS_PATH" \
-                    "$DBUS_PROPERTIES_INTERFACE.Get" \
-                    string:"$DBUS_PLAYER_INTERFACE" string:"PlaybackStatus" 2>&1)
-DBUS_GET_STATUS_EXIT_CODE=$?
-
-if [ $DBUS_GET_STATUS_EXIT_CODE -ne 0 ]; then
-    $SCRIPTS_PATH/music-play-list.sh
-else
-    dbus-send --session --type=method_call --dest="$DBUS_DEST" "$DBUS_PATH" "$DBUS_PLAYER_INTERFACE.PlayPause" >/dev/null
-fi
-
-echo "Script decision logic finished."
+dbus-send --session --type=method_call --dest="org.mpris.MediaPlayer2.vlc" "/org/mpris/MediaPlayer2" "org.mpris.MediaPlayer2.Player.PlayPause" >/dev/null
