32061f91258f90d2d7d7d1414bdbdfacb83e6cf1
Eugene Shtoka <eshtoka@gmail.com>
2025-06-15 12:41:37 +0300

    update

--- .scripts/git-preppare-review.sh
+++ .scripts/git-preppare-review.sh
@@ -6,6 +6,7 @@ echo "Feature branch: $feature_branch"
 rm -rf ~/toReview
 mkdir -p ~/toReview
 
+git diff --name-only main...$feature_branch | \
 while read -r filepath; do
   cp "$filepath" "~/toReview/$(basename "$filepath").orig"
 done
