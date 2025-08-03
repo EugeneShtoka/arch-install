1ce52ddd67938967e62f84b6d815a0ed55b7dd5a
Eugene Shtoka <eshtoka@gmail.com>
2025-08-03 10:21:44 +0200

    update

--- .scripts/git-init-repo.sh
+++ .scripts/git-init-repo.sh
@@ -7,10 +7,10 @@ else
     PROJECT_NAME=$1
 fi
 
-gh repo create $PROJECT_NAME --public
+gh repo create "$PROJECT_NAME" --public
 git init
 git add -A
 git commit -m 'Initial commit'
-git remote add origin git@github.com:EugeneShtoka/$PROJECT_NAME.git
+git remote add origin git@github.com:EugeneShtoka/"$PROJECT_NAME".git
 sleep 2
 git push -u -f origin main
