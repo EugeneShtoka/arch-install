8a543652ef8ac7d3c1e4576f66a70feb5c54b938
Eugene Shtoka <eshtoka@gmail.com>
2025-08-03 10:32:04 +0200

    update

--- .scripts/git-preppare-review.sh
+++ .scripts/git-preppare-review.sh
@@ -14,13 +14,13 @@ git switch -
 echo "Copying original files to $review_dir from" $(git branch --show-current)
 
 git diff --name-only "development...$feature_branch" | while read -r filepath; do
-  if [ -f "$filepath" ]; then
-    dest_filename="orig_$(basename "$filepath")"
-    echo "  -> Copying $filepath to $review_dir/$dest_filename"
-    cp "$filepath" "$review_dir/$dest_filename"
-  else
-    echo "  -> Skipping $filepath (it may have been deleted)"
-  fi
+	if [ -f "$filepath" ]; then
+		dest_filename="orig_$(basename "$filepath")"
+		echo "  -> Copying $filepath to $review_dir/$dest_filename"
+		cp "$filepath" "$review_dir/$dest_filename"
+	else
+		echo "  -> Skipping $filepath (it may have been deleted)"
+	fi
 done
 
 echo "Copy complete."
@@ -30,13 +30,13 @@ git switch -
 echo "Copying changed files to $review_dir from" $(git branch --show-current)
 
 git diff --name-only "development...$feature_branch" | while read -r filepath; do
-  if [ -f "$filepath" ]; then
-    dest_filename="$(basename "$filepath")"
-    echo "  -> Copying $filepath to $review_dir/$dest_filename"
-    cp "$filepath" "$review_dir/$dest_filename"
-  else
-    echo "  -> Skipping $filepath (it may have been deleted)"
-  fi
+	if [ -f "$filepath" ]; then
+		dest_filename="$(basename "$filepath")"
+		echo "  -> Copying $filepath to $review_dir/$dest_filename"
+		cp "$filepath" "$review_dir/$dest_filename"
+	else
+		echo "  -> Skipping $filepath (it may have been deleted)"
+	fi
 done
 
 echo "Script finished on branch: $(git branch --show-current)"
