c150a624256e43e7bfc74861db8d6b2b02c1320f
Eugene Shtoka <eshtoka@gmail.com>
2025-08-04 17:12:16 +0200

    update

--- .scripts/beep.sh
+++ .scripts/beep.sh
@@ -1,6 +1,7 @@
 function beep() {
     local duration=${1:-0.05}
-    local sine=${2:-240}
+    local frequency=440
 
-    play -n synth $duration sine $sine &> /dev/null 
+    play -n synth $duration sine $sine &>/dev/null
+    ffplay -f lavfi -i "sine=frequency=440:duration=0.08" -autoexit -nodisp 2>/dev/null
 }
