2d6a8b0997c6bb70dad01e02feb548be2b6cee8d
Eugene Shtoka <eshtoka@gmail.com>
2025-08-04 15:04:11 +0200

    update

--- .scripts/db-tunnel.sh
+++ .scripts/db-tunnel.sh
@@ -12,12 +12,13 @@ LOCAL_PORT="5433"
 
 echo "Logging into AWS SSO..."
 
+aws ssm start-session --target i-044a14fdab0f8ad29 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters '{"host":["tipmaster-prd.clmokqmsk1un.us-east-2.rds.amazonaws.com"],"portNumber":["5432"],"localPortNumber":["5433"]}'
 if aws sso login; then
     echo "AWS SSO login successful"
     echo "Starting port forwarding session..."
     echo "Tunneling ${RDS_HOST}:${REMOTE_PORT} -> localhost:${LOCAL_PORT}"
 
-    aws ssm start-session \
+    AWS_REGION=us-east-2 aws ssm start-session \
         --target "${TARGET_INSTANCE}" \
         --document-name AWS-StartPortForwardingSessionToRemoteHost \
         --parameters "{\"host\":[\"${RDS_HOST}\"],\"portNumber\":[\"${REMOTE_PORT}\"],\"localPortNumber\":[\"${LOCAL_PORT}\"]}"
