2bcb04abf182dd7c7202defac30812b0b5ca5ef2
Eugene Shtoka <eshtoka@gmail.com>
2025-08-04 22:02:29 +0200

    update

--- .scripts/alarm-set.sh
+++ .scripts/alarm-set.sh
@@ -48,13 +48,13 @@ echo "Alarm set for today at $alarm_time"
 
 echo "Waiting for $(($seconds_to_wait / 3600))h $(($seconds_to_wait % 3600 / 60))m $(($seconds_to_wait % 60))s..."
 
-# Run the alarm in completely detached background process
-nohup bash -c "
+# Run the alarm in background process
+(
     sleep $seconds_to_wait
     beep
     beep
     beep
-" &
+) &
 
 # Get the background process ID
 alarm_pid=$!
