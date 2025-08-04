dd5b1ceb37b42d7427c6e6fc6faafc71565f7ee1
Eugene Shtoka <eshtoka@gmail.com>
2025-08-04 09:49:54 +0200

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -94,7 +94,8 @@ $SCRIPTS_PATH/auto-yay.sh slack-desktop
 $SCRIPTS_PATH/auto-yay.sh nvm
 $SCRIPTS_PATH/auto-yay.sh pnpm
 $SCRIPTS_PATH/auto-yay.sh xorg-xrandr
-$SCRIPTS_PATH/auto-yay.sh pinta$SCRIPTS_PATH/auto-yay.sh picard
+$SCRIPTS_PATH/auto-yay.sh pinta
+$SCRIPTS_PATH/auto-yay.sh picard
 $SCRIPTS_PATH/auto-yay.sh shntool
 $SCRIPTS_PATH/auto-yay.sh mac
 $SCRIPTS_PATH/auto-yay.sh jami-qt
@@ -102,7 +103,6 @@ $SCRIPTS_PATH/auto-yay.sh gemini-cli-go-git
 $SCRIPTS_PATH/auto-yay.sh redli-bin
 $SCRIPTS_PATH/auto-yay.sh kid3
 $SCRIPTS_PATH/auto-yay.sh cursor-bin
-$SCRIPTS_PATH/auto-yay.sh iw
 $SCRIPTS_PATH/auto-yay.sh lm_sensors
 $SCRIPTS_PATH/auto-yay.sh zed
 $SCRIPTS_PATH/auto-yay.sh claude-code
