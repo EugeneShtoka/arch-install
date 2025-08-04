1b1508e029336dacace773d98b3972cc9ba633fc
Eugene Shtoka <eshtoka@gmail.com>
2025-08-04 09:31:42 +0200

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -98,7 +98,8 @@ $SCRIPTS_PATH/auto-yay.sh pinta$SCRIPTS_PATH/auto-yay.sh picard
 $SCRIPTS_PATH/auto-yay.sh shntool
 $SCRIPTS_PATH/auto-yay.sh mac
 $SCRIPTS_PATH/auto-yay.sh jami-qt
-$SCRIPTS_PATH/auto-yay.sh gemini-cli-go-git$SCRIPTS_PATH/auto-yay.sh redli-bin
+$SCRIPTS_PATH/auto-yay.sh gemini-cli-go-git
+$SCRIPTS_PATH/auto-yay.sh redli-bin
 $SCRIPTS_PATH/auto-yay.sh kid3
 $SCRIPTS_PATH/auto-yay.sh cursor-bin
 $SCRIPTS_PATH/auto-yay.sh iw
