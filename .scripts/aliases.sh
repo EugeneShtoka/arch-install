45cf89c72386c5ee5b79784a2e94e8728cadd489
Eugene Shtoka <eshtoka@gmail.com>
2025-08-13 19:05:05 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -32,6 +32,7 @@ alias europa='setsid ~/Games/Europa/start.sh &>/dev/null'
 alias victoria='setsid ~/Games/Victoria\ 3/start &>/dev/null'
 alias rim-world='setsid ~/Games/RimWorld/start.sh &>/dev/null'
 alias bar='setsid ~/Games/Beyond\ All\ Reason/Beyond-All-Reason-1.2988.0.AppImage &>/dev/null'
+alias ='setsid ~/Games/Grim\ Dawn/start.sh &>/dev/null'
 
 # To maintain this as last item - it should be last in the list
 alias rename='setsid thunar --bulk-rename &>/dev/null'
