1c30beda1f4caf47809390f8aebb28fd83d2c8fc
Eugene Shtoka <eshtoka@gmail.com>
2025-08-24 18:47:19 +0200

    update

--- .scripts/bluetooth.sh
+++ .scripts/bluetooth.sh
@@ -3,13 +3,13 @@ function bluetooth_status() {
 }
 
 function is_bluetooth_connected() {
-    echo $(bluetoothctl info $HEADPHONES_MAC_ADDR | grep Connected | awk '{print $2}')
+    echo $(timeout 0.5s bluetoothctl info $HEADPHONES_MAC_ADDR | grep Connected | awk '{print $2}')
 }
 
 function show-headphones-message() {
     blStatus="$(bluetooth_status)"
     echo "$blStatus"
-    name=$(echo "$blStatus" | grep -oP '(?<=Name: ).*' )
+    name=$(echo "$blStatus" | grep -oP '(?<=Name: ).*')
     battery_level=$(echo "$blStatus" | grep -oP '(?<=Battery Percentage: ).*' | awk '{print $2}' | tr -d \(\))
     message="$(get_battery_status $battery_level)$(get_audio_status)"
     notify-send "$name $1" "$message" -i $ICONS_PATH/headphones.png -r 101043
