89cb7bb6234512b992dc2ce1182fcbea8bc971fd
Eugene Shtoka <eshtoka@gmail.com>
2025-08-26 15:31:05 +0200

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -115,5 +115,4 @@ $SCRIPTS_PATH/auto-yay.sh lutris
 $SCRIPTS_PATH/auto-yay.sh winetricks
 $SCRIPTS_PATH/auto-yay.sh xorg-xdpyinfo
 $SCRIPTS_PATH/auto-yay.sh tlp
-$SCRIPTS_PATH/auto-yay.sh cmatrix
 $SCRIPTS_PATH/auto-yay.sh i3lock-color
