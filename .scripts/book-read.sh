a1d59976e964adf0dd6b7caf7382e6cf11ae7c1d
Eugene Shtoka <eshtoka@gmail.com>
2025-09-05 10:35:49 +0200

    update

--- .scripts/book-read.sh
+++ .scripts/book-read.sh
@@ -3,23 +3,23 @@ dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-9'
 
 if [ -z "$1" ]; then
-    current_path=$LIBRARY_PATH
+	current_path=$LIBRARY_PATH
 else
-    current_path=$1
+	current_path=$1
 fi
 
 while true; do
-    local choice=$(ls -a "$current_path" | grep -v "^\.$" | rofi -i -theme "$dir/$theme.rasi" -dmenu -matching prefix)
+	choice=$(ls -a "$current_path" | grep -v "^\.$" | rofi -i -theme "$dir/$theme.rasi" -dmenu -matching prefix)
 
-    if [[ -z "$choice" ]]; then # User cancelled
-        return 1
-    fi
+	if [[ -z "$choice" ]]; then # User cancelled
+		return 1
+	fi
 
-    local full_path="$current_path/$choice"
-    if [[ -d "$full_path" ]]; then
-        current_path="$full_path"
-    elif [[ -f "$full_path" ]]; then
-        setsid cr3 "$full_path" >/dev/null 2>&1 &
-        return 0
-    fi
+	full_path="$current_path/$choice"
+	if [[ -d "$full_path" ]]; then
+		current_path="$full_path"
+	elif [[ -f "$full_path" ]]; then
+		setsid cr3 "$full_path" >/dev/null 2>&1 &
+		return 0
+	fi
 done
