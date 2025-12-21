d89977e77a4ac7a23f630b330fb0787b2ce10426
Eugene Shtoka <eshtoka@gmail.com>
2025-12-21 23:23:58 +0200

    update

--- .scripts/aliases-terminal.sh
+++ .scripts/aliases-terminal.sh
@@ -27,6 +27,7 @@ alias la='eza --icons -al'
 alias clr='/usr/bin/clear'
 alias psgrep='ps aux | grep -v grep | grep -i -e VSZ -e'
 alias mnt='sudo mount'
+alias scp='sudo cp'
 alias install=$SCRIPTS_PATH/app-search-and-install.sh
 alias uninstall=$SCRIPTS_PATH/app-uninstall.sh
 alias gmn=$SCRIPTS_PATH/gemini-call.sh
