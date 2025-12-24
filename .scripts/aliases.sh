24598e349db5a75f85def8c077968bb396bea76c
Eugene Shtoka <eshtoka@gmail.com>
2025-12-24 19:09:03 +0200

    update

--- .scripts/aliases.sh
+++ .scripts/aliases.sh
@@ -32,6 +32,7 @@ alias victoria='setsid ~/Games/Victoria\ 3/start &>/dev/null'
 alias rim-world='setsid ~/Games/RimWorld/start.sh &>/dev/null'
 alias bar='setsid ~/Games/Beyond\ All\ Reason/Beyond-All-Reason-1.2988.0.AppImage &>/dev/null'
 alias grim-dawn='setsid ~/Games/Grim\ Dawn/start.sh &>/dev/null'
+alias factorio='steam -applaunch 427520'
 
 # To maintain this as last item - it should be last in the list
 alias rename='setsid thunar --bulk-rename &>/dev/null'
