84b3c39c8e09099d42ae6c97f2d7ef6ac3433e83
Eugene Shtoka <eshtoka@gmail.com>
2026-01-31 13:18:17 +0400

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -119,3 +119,4 @@ $SCRIPTS_PATH/auto-yay.sh betterlockscreen
 $SCRIPTS_PATH/auto-yay.sh libgendesktop
 $SCRIPTS_PATH/auto-yay.sh foliate
 $SCRIPTS_PATH/auto-yay.sh noto-fonts-emoji
+$SCRIPTS_PATH/auto-yay.sh xorg-xev
