7efb9ab6e23c856a05c3188eecf5bda1143b5e15
Eugene Shtoka <eshtoka@gmail.com>
2026-02-03 16:09:00 +0400

    update

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -2,10 +2,5 @@
 # Switch to English layout before locking
 setxkbmap us -variant altgr-intl
 
-# Lock the screen
-# Use --nofork when called from xss-lock, regular mode for manual lock
-if [[ "$1" == "--nofork" ]]; then
-    i3lock --nofork -c 000000
-else
-    i3lock -c 000000
-fi
+# Lock the screen (--nofork works for both manual and xss-lock)
+i3lock --nofork -c 000000
