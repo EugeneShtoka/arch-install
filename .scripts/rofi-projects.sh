85605eb7eb2f2c1c03136dc09b12ec0984184418
Eugene Shtoka <eshtoka@gmail.com>
2026-02-11 19:31:30 +0200

    update

--- .scripts/rofi-projects.sh
+++ .scripts/rofi-projects.sh
@@ -4,4 +4,4 @@ dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-9'
 
 command=$(. $SCRIPTS_PATH/projects-list.sh | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
-zsh -ic $command
+alacritty -e zsh -ic $command
