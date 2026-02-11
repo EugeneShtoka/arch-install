bae9e5f00beaf0c8382ca27fbebd270062917f4a
Eugene Shtoka <eshtoka@gmail.com>
2026-02-11 16:16:16 +0200

    update

---
+++ .scripts/vim-projects-aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+alias figoro='cd $HOME/dev/figoro && nvim . && clr'
+alias blog='cd $HOME/dev/blog && nvim . && clr'
+alias keyboard='cd $HOME/dev/zmk-config && nvim . && clr'
+alias work-be='cd $HOME/dev/work/exbetz-be-api && nvim . && clr'
+alias system='cd $HOME && nvim . && clr'
+alias nvim-config='cd $HOME/.config/nvim && nvim . && clr'
