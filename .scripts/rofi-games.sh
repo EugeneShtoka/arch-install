769bfc6b6af9ce0157625790e2192536d53e1305
Eugene Shtoka <eshtoka@gmail.com>
2026-02-11 17:03:59 +0200

    update

--- .scripts/rofi-games.sh
+++ .scripts/rofi-games.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 dir="$HOME/.config/rofi/launchers/type-4"
-theme='style-9-columns'
+theme='style-9'
 
 command=$(. $SCRIPTS_PATH/games-list.sh | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 zsh -ic $command
