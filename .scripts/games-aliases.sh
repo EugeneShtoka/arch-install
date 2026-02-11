da41705853859cfd9cc9ca3798875d8657302be9
Eugene Shtoka <eshtoka@gmail.com>
2026-02-11 16:16:06 +0200

    update

---
+++ .scripts/games-aliases.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+alias against-the-storm='setsid ~/Games/AgainstTheStorm/start.sh &>/dev/null'
+alias europa='setsid ~/Games/Europa/start.sh &>/dev/null'
+alias victoria='setsid ~/Games/Victoria\ 3/start &>/dev/null'
+alias rim-world='setsid ~/Games/RimWorld/start.sh &>/dev/null'
+alias bar='setsid ~/Games/Beyond\ All\ Reason/Beyond-All-Reason-1.2988.0.AppImage &>/dev/null'
+alias grim-dawn='setsid ~/Games/Grim\ Dawn/start.sh &>/dev/null'
+alias factorio='steam -applaunch 427520'
