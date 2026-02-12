e1994795a27a72995854230466b5a5f7af3be9a8
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:19:24 +0200

    update

--- .scripts/monitor-setup-multi.sh
+++ .scripts/monitor-setup-multi.sh
@@ -7,9 +7,6 @@ if [[ "$monitorsCount" -eq 1 ]]; then
     echo "`date` connecting monitor $MONITOR_WORK_2" >> $LOG_PATH
     xrandr --output $MONITOR_WORK_2 --auto
     sleep 1
-    echo "`date` connecting monitor $MONITOR_WORK_3" >> $LOG_PATH
-    xrandr --output $MONITOR_WORK_3 --auto --right-of $MONITOR_WORK_2
-    sleep 1
     echo "`date` disabling laptop monitor $MONITOR_LAPTOP" >> $LOG_PATH
     xrandr --output $MONITOR_LAPTOP --off
 
