83f96213c7ab4c975e345a046b238e412eae4ea2
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:59:05 +0200

    update

--- .scripts/monitor-setup-external.sh
+++ .scripts/monitor-setup-external.sh
@@ -2,6 +2,14 @@
 
 source /home/eugene/.env
 
+MONITOR_LAPTOP=eDP-1
+MONITOR_EXTERNAL=$(xrandr | grep ' connected' | grep -v "^${MONITOR_LAPTOP} " | head -n 1 | awk '{print $1}')
+
+if [[ -z "$MONITOR_EXTERNAL" ]]; then
+    echo "`date` monitor-setup-external: no external monitor found" >> $LOG_PATH
+    exit 0
+fi
+
 monitorsCount=$(xrandr --listactivemonitors | grep 'Monitors:' | awk '{print $2}')
 if [[ "$monitorsCount" -eq 1 ]]; then
     echo "`date` connecting monitor $MONITOR_EXTERNAL" >> $LOG_PATH
@@ -10,5 +18,6 @@ if [[ "$monitorsCount" -eq 1 ]]; then
     echo "`date` disabling laptop monitor $MONITOR_LAPTOP" >> $LOG_PATH
     xrandr --output $MONITOR_LAPTOP --off
 
+    export MONITOR_LAPTOP MONITOR_EXTERNAL
     $SCRIPTS_PATH/workspaces-work-setup.sh
 fi
