b2f55ad2fe1f22439bb5e9d1cee77d00f2700976
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:34:01 +0200

    update

--- .scripts/monitor-setup-single.sh
+++ .scripts/monitor-setup-single.sh
@@ -5,5 +5,5 @@ source /home/eugene/.env
 echo "`date` enabling laptop monitor $MONITOR_LAPTOP"
 xrandr --output $MONITOR_LAPTOP --auto
 
-echo "`date` disconnecting monitor $MONITOR_WORK_2"
-xrandr --output $MONITOR_WORK_2 --off
+echo "`date` disconnecting monitor $MONITOR_EXTERNAL"
+xrandr --output $MONITOR_EXTERNAL --off
