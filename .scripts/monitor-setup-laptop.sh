77ad480852dcdb226507f8f271896114d53b2742
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:58:00 +0200

    update

--- .scripts/monitor-setup-laptop.sh
+++ .scripts/monitor-setup-laptop.sh
@@ -2,8 +2,13 @@
 
 source /home/eugene/.env
 
-echo "`date` enabling laptop monitor $MONITOR_LAPTOP"
+MONITOR_LAPTOP=${MONITOR_LAPTOP:-eDP-1}
+
+echo "`date` enabling laptop monitor $MONITOR_LAPTOP" >> $LOG_PATH
 xrandr --output $MONITOR_LAPTOP --auto
 
-echo "`date` disconnecting monitor $MONITOR_EXTERNAL"
-xrandr --output $MONITOR_EXTERNAL --off
+# Turn off all connected non-laptop monitors
+for monitor in $(xrandr | grep ' connected' | grep -v "^${MONITOR_LAPTOP} " | awk '{print $1}'); do
+    echo "`date` disconnecting monitor $monitor" >> $LOG_PATH
+    xrandr --output $monitor --off
+done
