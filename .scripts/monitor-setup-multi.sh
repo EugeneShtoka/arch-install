41617d7e5989bbdba1a6124ab42945c527760a73
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:16:49 +0200

    update

--- .scripts/monitor-setup-multi.sh
+++ .scripts/monitor-setup-multi.sh
@@ -3,12 +3,15 @@
 source /home/eugene/.env
 
 monitorsCount=$(xrandr --listactivemonitors | grep 'Monitors:' | awk '{print $2}')
-if [[ "$monitorsCount" -eq 1 ]]; then  
+if [[ "$monitorsCount" -eq 1 ]]; then
     echo "`date` connecting monitor $MONITOR_WORK_2" >> $LOG_PATH
-    xrandr --output $MONITOR_WORK_2 --auto --right-of $MONITOR_LAPTOP
+    xrandr --output $MONITOR_WORK_2 --auto
     sleep 1
     echo "`date` connecting monitor $MONITOR_WORK_3" >> $LOG_PATH
     xrandr --output $MONITOR_WORK_3 --auto --right-of $MONITOR_WORK_2
-    
+    sleep 1
+    echo "`date` disabling laptop monitor $MONITOR_LAPTOP" >> $LOG_PATH
+    xrandr --output $MONITOR_LAPTOP --off
+
     $SCRIPTS_PATH/workspaces-work-setup.sh
 fi
