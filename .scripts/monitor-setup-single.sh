a3a952296ff68a690b6b8e4214b76fdc180c10cd
Eugene Shtoka <eshtoka@gmail.com>
2026-02-12 17:19:33 +0200

    update

--- .scripts/monitor-setup-single.sh
+++ .scripts/monitor-setup-single.sh
@@ -7,6 +7,3 @@ xrandr --output $MONITOR_LAPTOP --auto
 
 echo "`date` disconnecting monitor $MONITOR_WORK_2"
 xrandr --output $MONITOR_WORK_2 --off
-
-echo "`date` disconnecting monitor $MONITOR_WORK_3"
-xrandr --output $MONITOR_WORK_3 --off
