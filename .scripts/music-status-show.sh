e6da4ef5879174a14d1ec9eccbf7b4c025fe886d
Eugene Shtoka <eshtoka@gmail.com>
2026-02-27 17:16:45 +0200

    update

--- .scripts/music-status-show.sh
+++ .scripts/music-status-show.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A2 "xesam:artist" | grep "string" | head -1 | sed 's/.*string "//;s/"//')
-title=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A1 "xesam:title" | grep "string" | head -1 | sed 's/.*string "//;s/"//')
+metadata=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null)
+
+artist=$(echo "$metadata" | grep -A2 '"xesam:artist"' | grep -v 'xesam:artist' | grep 'string "' | head -1 | sed 's/.*string "//;s/"//')
+title=$(echo "$metadata" | grep -A1 '"xesam:title"' | grep -v 'xesam:title' | grep 'string "' | head -1 | sed 's/.*string "//;s/"//')
 
 if [[ -n "$title" ]]; then
   if [[ -n "$artist" ]]; then
