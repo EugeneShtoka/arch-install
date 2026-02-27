8d53d8b851f79f8fc99389088f58d1c68f65622a
Eugene Shtoka <eshtoka@gmail.com>
2026-02-27 17:13:56 +0200

    update

--- .scripts/music-notify.sh
+++ .scripts/music-notify.sh
@@ -4,15 +4,6 @@ dbus-monitor --session "type='signal',interface='org.freedesktop.DBus.Properties
 while read -r line; do
   if echo "$line" | grep -q "Metadata"; then
     sleep 0.3
-    artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A2 "xesam:artist" | grep "string" | head -1 | sed 's/.*string "//;s/"//')
-    title=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null | grep -A1 "xesam:title" | grep "string" | head -1 | sed 's/.*string "//;s/"//')
-
-    if [[ -n "$title" ]]; then
-      if [[ -n "$artist" ]]; then
-        notify-send "$artist" "<span font='20px'>$title</span>" -r 101029
-      else
-        notify-send "Now Playing" "<span font='20px'>$title</span>" -r 101029
-      fi
-    fi
+    $SCRIPTS_PATH/music-status-show.sh
   fi
 done
