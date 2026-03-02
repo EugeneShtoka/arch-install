007fb5272c9b6ef7f98fb6d10ccacd369a05c5b1
Eugene Shtoka <eshtoka@gmail.com>
2026-03-02 12:46:52 +0200

    update

--- .scripts/git-squash.sh
+++ .scripts/git-squash.sh
@@ -1,6 +1,14 @@
 #!/bin/zsh
 
 git fetch origin
-git reset --soft $(git merge-base HEAD origin/$GIT_DEFAULT_BRANCH)
-git commit -m "squashed feature"
+base=$(git merge-base HEAD origin/$GIT_DEFAULT_BRANCH)
+
+if [[ -n $1 ]]; then
+  msg=$1
+else
+  msg=$(git log --reverse --format="%s" $base..HEAD)
+fi
+
+git reset --soft $base
+git commit -m "$msg"
 git push -f
