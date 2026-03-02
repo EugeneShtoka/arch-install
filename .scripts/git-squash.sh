54e356fc86da93818f9327523a9264336c34da97
Eugene Shtoka <eshtoka@gmail.com>
2026-03-02 10:28:05 +0200

    update

--- .scripts/git-squash.sh
+++ .scripts/git-squash.sh
@@ -4,13 +4,3 @@ git fetch origin
 git reset --soft $(git merge-base HEAD origin/$GIT_DEFAULT_BRANCH)
 git commit -m "squashed feature"
 git push -f
-----old version
-branchFullName=$(git branch --show-current);  
-git switch main;  
-git pull;  
-git checkout "$branchFullName";  
-git pull;  
-git reset "$(git merge-base main "${branchFullName}")";  
-git add .;  
-git commit -m "${branchFullName}-squash";  
-git push -f;
