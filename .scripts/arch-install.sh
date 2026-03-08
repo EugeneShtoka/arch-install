df49f7d428333d136f531cdf0db89cd1d4c7fd3b
Eugene Shtoka <eshtoka@gmail.com>
2026-03-08 12:22:11 +0200

    update

--- .scripts/arch-install.sh
+++ .scripts/arch-install.sh
@@ -11,7 +11,6 @@ mkdir -p ~/.zsh/plugins/sudo ~/.zsh/plugins/dirhistory
 curl -o ~/.zsh/plugins/sudo/sudo.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
 curl -o ~/.zsh/plugins/dirhistory/dirhistory.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/dirhistory/dirhistory.plugin.zsh
 
-git clone https://github.com/olivierverdier/zsh-git-prompt.git $ZSH_GIT_PROMPT_PLUGIN
 
 chsh -s $(which zsh)
 zsh $SCRIPTS_PATH/apps-install.sh
