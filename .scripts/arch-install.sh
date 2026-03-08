9950b62cd46e20c4bfcd00be6dcdc6f9854fa916
Eugene Shtoka <eshtoka@gmail.com>
2026-03-08 12:37:53 +0200

    update

--- .scripts/arch-install.sh
+++ .scripts/arch-install.sh
@@ -7,9 +7,9 @@ yes | sudo pacman -R xterm pipewire pipewire-pulse
 
 localectl set-locale LC_TIME=en_GB.UTF-8
 
-mkdir -p ~/.zsh/plugins/sudo ~/.zsh/plugins/dirhistory
-curl -o ~/.zsh/plugins/sudo/sudo.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
-curl -o ~/.zsh/plugins/dirhistory/dirhistory.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/dirhistory/dirhistory.plugin.zsh
+mkdir -p $ZSH_PLUGINS_PATH/sudo $ZSH_PLUGINS_PATH/dirhistory
+curl -o $ZSH_PLUGINS_PATH/sudo/sudo.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+curl -o $ZSH_PLUGINS_PATH/dirhistory/dirhistory.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/dirhistory/dirhistory.plugin.zsh
 
 
 chsh -s $(which zsh)
