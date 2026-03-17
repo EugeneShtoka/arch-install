4236cda92277f0e6e42ab3dbec21158c474ad8dc
Eugene Shtoka <eshtoka@gmail.com>
2026-03-17 12:25:34 +0200

    update

---
+++ .scripts/workspace-outputs.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+MONITOR_LAPTOP=eDP-1
+CONF=~/.config/i3/workspace-outputs.conf
+
+external=$(xrandr | grep ' connected' | grep -v "^${MONITOR_LAPTOP} " | head -n 1 | awk '{print $1}')
+
+if [[ -n "$external" ]]; then
+    cat > $CONF <<EOF
+workspace \$ws1 output $MONITOR_LAPTOP
+workspace \$ws2 output $MONITOR_LAPTOP
+workspace \$ws3 output $MONITOR_LAPTOP
+workspace \$ws4 output $external
+workspace \$ws5 output $external
+workspace \$ws6 output $external
+workspace \$ws7 output $external
+workspace \$ws8 output $external
+workspace \$ws9 output $external
+workspace \$ws10 output $external
+EOF
+else
+    > $CONF
+fi
+
+i3-msg reload
