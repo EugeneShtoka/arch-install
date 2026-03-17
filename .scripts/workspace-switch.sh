8167ce380ca6a93b3bb2a1222fe3e74fde81946c
Eugene Shtoka <eshtoka@gmail.com>
2026-03-17 17:10:21 +0200

    update

--- .scripts/workspace-switch.sh
+++ .scripts/workspace-switch.sh
@@ -1,4 +1,18 @@
 #!/bin/zsh
 
-ws=$(i3-msg -t get_workspaces | jq -r '.[].name' | rofi -dmenu -p "workspace")
-[[ -n "$ws" ]] && i3-msg workspace "$ws"
+rofi_dir="$HOME/.config/rofi/launchers/type-4"
+rofi_theme="style-9-wide"
+
+ws_list=$(i3-msg -t get_tree | jq -r '
+  [.. | objects | select(.type == "workspace") | select(.name != "__i3_scratch")] |
+  sort_by(.name | try tonumber catch .) |
+  .[] |
+  {
+    name: .name,
+    apps: ([.. | objects | select(.window != null) | .window_properties.class] | unique | join("  "))
+  } |
+  if .apps == "" then .name else "\(.name) : \(.apps)" end
+')
+
+ws=$(echo "$ws_list" | rofi -theme "${rofi_dir}/${rofi_theme}.rasi" -dmenu -p "workspace" -matching prefix)
+[[ -n "$ws" ]] && i3-msg workspace "$(echo "$ws" | awk '{print $1}')"
