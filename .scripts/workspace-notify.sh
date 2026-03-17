f7e0780e5b1a698e4fde1620cb3dcd9796677c01
Eugene Shtoka <eshtoka@gmail.com>
2026-03-17 16:36:44 +0200

    update

--- .scripts/workspace-notify.sh
+++ .scripts/workspace-notify.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
-i3-msg -t subscribe '["workspace"]' | while IFS= read -r line; do
-    ws=$(echo "$line" | jq -r 'if .change == "focus" then .current.name else empty end')
-    [[ -n "$ws" ]] && dunstify -r 9900 -t 1500 "Workspace $ws"
+while true; do
+    i3-msg -t subscribe '["workspace"]' | while IFS= read -r line; do
+        ws=$(echo "$line" | jq -r 'if .change == "focus" then .current.name else empty end')
+        [[ -n "$ws" ]] && dunstify -r 9900 -t 1500 "Workspace $ws"
+    done
+    sleep 1
 done
