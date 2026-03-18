dba419e69d6ceb19a0ec91b3d47f26dbee969424
Eugene Shtoka <eshtoka@gmail.com>
2026-03-18 11:34:00 +0200

    update

--- .scripts/workspace-goto.sh
+++ .scripts/workspace-goto.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 ws=$1
+echo "$ws" > /tmp/workspace-current
 i3-msg workspace "$ws"
+[[ "$(cat /tmp/workspace-current)" == "$ws" ]] || exit 0
 dunstctl close 9900 2>/dev/null
 notify-send -r 9900 -t 1500 "Workspace $ws"
