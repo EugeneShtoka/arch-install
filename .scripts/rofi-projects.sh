0a557d47773a4d0eac960696bbdd2db549e9f526
Eugene Shtoka <eshtoka@gmail.com>
2026-03-21 19:45:57 +0200

    update

--- .scripts/rofi-projects.sh
+++ .scripts/rofi-projects.sh
@@ -3,7 +3,7 @@
 dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-9'
 
-command=$(. $SCRIPTS_PATH/projects-list.sh | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+command=$(. $SCRIPTS_PATH/projects-list.sh | $SCRIPTS_PATH/rofi-run.sh -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 if [[ -n $command ]]; then
     wezterm cli spawn -- zsh -ic $command 2>/dev/null || wezterm start -- zsh -ic $command
 fi
