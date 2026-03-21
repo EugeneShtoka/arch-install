5f0812a22a4a5d5fcb86c7d9a3c92ae7b6ebe712
Eugene Shtoka <eshtoka@gmail.com>
2026-03-21 16:07:38 +0200

    update

--- .scripts/rofi-dev.sh
+++ .scripts/rofi-dev.sh
@@ -4,17 +4,26 @@ dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-9'
 
 typeset -A projects
+typeset -a ordered
 
-for d in $HOME/dev/*(N/); do
+# Load named projects from aliases-projects.sh first
+nvim_project() {
+	local name=$1 path=$2
+	projects[$name]=$path
+	ordered+=($name)
+}
+source $SCRIPTS_PATH/aliases-projects.sh
+
+# Append git repos from ~/dev and ~/dev/work not already listed
+for d in $HOME/dev/*(N/) $HOME/dev/work/*(N/); do
 	[[ ${d:t} == work ]] && continue
-	[[ -d $d/.git ]] && projects[${d:t}]=$d
+	[[ -d $d/.git ]] || continue
+	[[ -v projects[${d:t}] ]] && continue
+	projects[${d:t}]=$d
+	ordered+=(${d:t})
 done
 
-for d in $HOME/dev/work/*(N/); do
-	[[ -d $d/.git ]] && projects[${d:t}]=$d
-done
-
-name=$(print -l ${(k)projects} | sort | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
+name=$(print -l $ordered | rofi -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 [[ -z $name ]] && exit
 
 path=${projects[$name]}
