5c05420400c85d07a33b1f84f69d91967b2d7b18
Eugene Shtoka <eshtoka@gmail.com>
2026-03-22 23:17:14 +0200

    update

--- .scripts/root-notify-send.sh
+++ .scripts/root-notify-send.sh
@@ -8,5 +8,11 @@ function root-notify-send() {
     #Detect the id of the user
     local uid=$(id -u $user)
 
-    sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send "$@"
+    local monitor=$(sudo -u $user DISPLAY=$display xrandr --listmonitors | awk '
+        /DP-/ { ext_idx = $1+0; found_ext = 1 }
+        /eDP-/ { int_idx = $1+0 }
+        END { print (found_ext ? ext_idx : int_idx) }
+    ')
+
+    sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus notify-send -h "int:x-dunst-monitor:$monitor" "$@"
 }
