8711c8193c568b129a5edbd7e6c9c8f34d0a9a9a
Eugene Shtoka <eshtoka@gmail.com>
2026-03-22 23:14:53 +0200

    update

---
+++ .scripts/notify-lib.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Returns the dunst monitor index: external (DP-*) if connected, internal (eDP-*) otherwise
+get_notify_monitor() {
+    xrandr --listmonitors | awk '
+        /DP-/ { ext_idx = $1+0; found_ext = 1 }
+        /eDP-/ { int_idx = $1+0 }
+        END { print (found_ext ? ext_idx : int_idx) }
+    '
+}
+
+# Drop-in wrapper for notify-send that targets the correct monitor
+notify_send() {
+    local monitor=$(get_notify_monitor)
+    notify-send -h "int:x-dunst-monitor:$monitor" "$@"
+}
