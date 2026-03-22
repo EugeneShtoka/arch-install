4d21220181ecb469591c2b75cd8f09f307f2a928
Eugene Shtoka <eshtoka@gmail.com>
2026-03-22 23:16:39 +0200

    update

--- .scripts/workspace-goto.sh
+++ .scripts/workspace-goto.sh
@@ -5,4 +5,4 @@ source $SCRIPTS_PATH/notify-lib.sh
 ws=$1
 i3-msg workspace "$ws"
 dunstctl close 9900 2>/dev/null
-notify-send -r 9900 -t 1500 "Workspace $ws" 
+notify_send -r 9900 -t 1500 "Workspace $ws" 
