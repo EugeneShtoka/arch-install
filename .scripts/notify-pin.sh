521d804e40cb1bfa4fedac0fd07fde9be9839849
Eugene Shtoka <eshtoka@gmail.com>
2026-03-25 21:59:20 +0200

    update

--- .scripts/notify-pin.sh
+++ .scripts/notify-pin.sh
@@ -7,4 +7,8 @@ read -r summary body < <(dunstctl history | tr -d '\000-\037' | jq -r '
 
 [[ -z "$summary" && -z "$body" ]] && exit
 
+sed -i 's/^    origin = top-right/    origin = center/' ~/.config/dunst/dunstrc
+dunstctl reload 2>/dev/null
 notify-send -a "pinned" -t 0 "$summary" "$body"
+sed -i 's/^    origin = center/    origin = top-right/' ~/.config/dunst/dunstrc
+dunstctl reload 2>/dev/null
