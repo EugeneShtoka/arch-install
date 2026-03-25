fee72e5e5e5adc0f538813f031bafa022c92b6de
Eugene Shtoka <eshtoka@gmail.com>
2026-03-25 23:48:11 +0200

    update

--- .scripts/workspace-notify.sh
+++ .scripts/workspace-notify.sh
@@ -5,7 +5,7 @@ source $SCRIPTS_PATH/notify-lib.sh
 while true; do
     i3-msg -t subscribe '["workspace"]' | while IFS= read -r line; do
         ws=$(echo "$line" | jq -r 'if .change == "focus" then .current.name else empty end')
-        [[ -n "$ws" ]] && notify_send -r 9900 -t 1500 "Workspace $ws"
+        [[ -n "$ws" ]] && notify_send -r 9900 -t 1500 -a "system-notify" "Workspace $ws"
     done
     sleep 1
 done
