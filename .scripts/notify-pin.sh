8296199bf92a178cd515fc05992fc356d81074fe
Eugene Shtoka <eshtoka@gmail.com>
2026-03-25 21:54:54 +0200

    update

--- .scripts/notify-pin.sh
+++ .scripts/notify-pin.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-latest=$(dunstctl history | tr -d '\000-\037' | jq -r '.data[0] | sort_by(.timestamp.data) | last')
-summary=$(echo "$latest" | jq -r '.summary.data // ""')
-body=$(echo "$latest" | jq -r '.body.data // ""')
+read -r summary body < <(dunstctl history | tr -d '\000-\037' | jq -r '
+  .data[0] | sort_by(.timestamp.data) | last |
+  [(.summary.data // ""), (.body.data // "")] | @tsv
+')
 
 [[ -z "$summary" && -z "$body" ]] && exit
 
