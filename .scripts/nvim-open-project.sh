c1237cf65c27c9a8f507bde405312dac9faddb56
Eugene Shtoka <eshtoka@gmail.com>
2026-03-25 12:29:26 +0200

    update

--- .scripts/nvim-open-project.sh
+++ .scripts/nvim-open-project.sh
@@ -3,5 +3,5 @@
 name=$1
 path=$2
 
-cmd="wezterm cli set-tab-title 'nvim: $name' && cd '$path' && nvim . && clr"
+cmd="wezterm cli set-tab-title 'Neovim: $name' && cd '$path' && nvim . && clr"
 wezterm cli spawn -- zsh -ic $cmd 2>/dev/null || wezterm start -- zsh -ic $cmd
