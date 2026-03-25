4dacc2f5653b0b3f1a63d5a111dab89392c27873
Eugene Shtoka <eshtoka@gmail.com>
2026-03-25 22:08:36 +0200

    update

--- .scripts/notify-pin.sh
+++ .scripts/notify-pin.sh
@@ -11,4 +11,4 @@ read -r summary body < <(dunstctl history | tr -d '\000-\037' | jq -r '
 
 [[ -z "$summary" && -z "$body" ]] && exit
 
-notify-send -t 0 "$summary" "$body"
+notify-send -t 0 "[pinned] $summary" "$body"
