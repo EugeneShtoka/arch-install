1b5c9618d09847f722dda2b1160af662888a3b06
Eugene Shtoka <eshtoka@gmail.com>
2026-03-27 01:21:04 +0200

    update

--- .scripts/rofi-projects.sh
+++ .scripts/rofi-projects.sh
@@ -8,7 +8,7 @@ projects=$(. $SCRIPTS_PATH/projects-list.sh)
 selected=$(echo "$projects" | cut -d'|' -f1 | $SCRIPTS_PATH/rofi-run.sh -theme ${rofi_dir}/${theme}.rasi -dmenu -matching prefix)
 [[ -z $selected ]] && exit
 
-path=$(echo "$projects" | awk -F'|' -v name="$selected" '$1 == name {print $2}')
-[[ -z $path ]] && exit
+proj_path=$(echo "$projects" | awk -F'|' -v name="$selected" '$1 == name {print $2}')
+[[ -z $proj_path ]] && exit
 
-$SCRIPTS_PATH/nvim-open-project.sh "$selected" "$path"
+$SCRIPTS_PATH/nvim-open-project.sh "$selected" "$proj_path"
