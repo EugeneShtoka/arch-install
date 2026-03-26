22addb712dc0876e9a14f77cbd0eb3273adf4aa6
Eugene Shtoka <eshtoka@gmail.com>
2026-03-27 00:26:52 +0200

    update

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -3,10 +3,7 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  -- TODO: pinned to 3.37.x due to gitignore regression in 3.38+ (race condition in
-  -- git worktree init causes gitignored files to appear). Check if fixed and bump to '*'.
-  -- Broken commits: 4925f31 (refactor git), dfe9d8b (renderer). Issue introduced ~Nov 2025.
-  version = '~3.37',
+  version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
