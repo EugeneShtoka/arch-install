7018651ba202fed248f205223db1960eb641e04f
Eugene Shtoka <eshtoka@gmail.com>
2026-03-26 22:53:25 +0200

    update

--- .scripts/projects-list.sh
+++ .scripts/projects-list.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-grep -oP '(?<=nvim_project )\S+ +\S+' $SCRIPTS_PATH/aliases-projects.sh | awk '{print $1 "|" $2}'
+grep -oP '(?<=nvim_project )\S+ +\S+' $SCRIPTS_PATH/aliases-projects.sh \
+  | awk '{print $1 "|" $2}' \
+  | envsubst
