1501541964725a6a8c755df993af5fc85e604062
Eugene Shtoka <eshtoka@gmail.com>
2026-03-26 23:52:42 +0200

    update

--- .scripts/nvim-open-project.sh
+++ .scripts/nvim-open-project.sh
@@ -3,6 +3,6 @@
 name=$1
 dir=$2
 
-pane_id=$(/usr/bin/wezterm cli spawn -- /usr/bin/zsh -i)
+pane_id=$(/usr/bin/wezterm cli spawn -- /usr/bin/zsh -il)
 /usr/bin/wezterm cli activate-pane --pane-id $pane_id
 i3-msg '[class="org.wezfurlong.wezterm"] focus'
