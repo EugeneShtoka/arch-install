47a8901fc363526cbbda1f2786659701b5f19ee9
Eugene Shtoka <eshtoka@gmail.com>
2026-03-26 23:26:54 +0200

    update

--- .scripts/nvim-open-project.sh
+++ .scripts/nvim-open-project.sh
@@ -4,4 +4,12 @@ name=$1
 dir=$2
 
 cmd="wezterm cli set-tab-title 'Neovim: $name' && cd '$dir' && nvim . && clr"
-/usr/bin/wezterm cli spawn -- /usr/bin/zsh -ic $cmd 2>/dev/null || /usr/bin/wezterm start -- /usr/bin/zsh -ic $cmd
+log=~/.scripts.log
+echo "$(date) spawn: name=$name dir=$dir" >> $log
+/usr/bin/wezterm cli spawn -- /usr/bin/zsh -ic $cmd >> $log 2>&1
+spawn_exit=$?
+echo "$(date) spawn exit=$spawn_exit" >> $log
+if [[ $spawn_exit -ne 0 ]]; then
+  /usr/bin/wezterm start -- /usr/bin/zsh -ic $cmd >> $log 2>&1
+  echo "$(date) start exit=$?" >> $log
+fi
