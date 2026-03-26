5577b35b4ec3ee80a46d10e2ccf611ae72c2513f
Eugene Shtoka <eshtoka@gmail.com>
2026-03-27 00:01:13 +0200

    update

--- .scripts/nvim-open-project.sh
+++ .scripts/nvim-open-project.sh
@@ -4,6 +4,6 @@ name=$1
 dir=$2
 
 cmd="wezterm cli set-tab-title 'Neovim: $name' 2>/dev/null; cd '$dir' && nvim ."
-pane_id=$(wezterm cli spawn -- zsh -ilc $cmd)
-wezterm cli activate-pane --pane-id $pane_id
+pane_id=$(/usr/bin/wezterm cli spawn -- /usr/bin/zsh -ilc $cmd)
+/usr/bin/wezterm cli activate-pane --pane-id $pane_id
 i3-msg '[class="org.wezfurlong.wezterm"] focus'
