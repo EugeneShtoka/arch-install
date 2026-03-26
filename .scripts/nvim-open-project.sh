567178955e4e6e2ecc756131d41c3031ca4932a9
Eugene Shtoka <eshtoka@gmail.com>
2026-03-27 00:44:31 +0200

    update

--- .scripts/nvim-open-project.sh
+++ .scripts/nvim-open-project.sh
@@ -11,9 +11,10 @@ if [[ -S $XDG_RUNTIME_DIR/wezterm/sock ]]; then
     wezterm cli activate-pane --pane-id $pane_id
 else
     /usr/bin/wezterm start -- /usr/bin/zsh -ilc "$cmd" &
-    # wait for socket then set title
     until [[ -S $XDG_RUNTIME_DIR/wezterm/sock ]]; do sleep 0.1; done
-    wezterm cli set-tab-title "Neovim: $name" 2>/dev/null
+    until pane_id=$(wezterm cli list --format json 2>/dev/null | jq -r '.[0].pane_id // empty') && [[ -n $pane_id ]]; do sleep 0.1; done
+    wezterm cli set-tab-title --pane-id $pane_id "Neovim: $name"
+    wezterm cli activate-pane --pane-id $pane_id
 fi
 
 i3-msg '[class="org.wezfurlong.wezterm"] focus'
