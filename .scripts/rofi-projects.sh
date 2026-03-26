44e0faa869ee2c117136669fcd34d221ffaeedfd
Eugene Shtoka <eshtoka@gmail.com>
2026-03-26 22:50:57 +0200

    update

--- .scripts/rofi-projects.sh
+++ .scripts/rofi-projects.sh
@@ -6,4 +6,6 @@ theme='style-9'
 command=$(. $SCRIPTS_PATH/projects-list.sh | $SCRIPTS_PATH/rofi-run.sh -theme ${dir}/${theme}.rasi -dmenu -matching prefix)
 if [[ -n $command ]]; then
     wezterm cli spawn -- zsh -ic $command 2>/dev/null || wezterm start -- zsh -ic $command
+    (sleep 2 && $SCRIPTS_PATH/wezterm-tabs-update.sh) &
+    disown
 fi
