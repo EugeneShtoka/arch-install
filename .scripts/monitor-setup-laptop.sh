298d630a646c4da701a9d7d5198c90e4b738815b
Eugene Shtoka <eshtoka@gmail.com>
2026-03-29 11:15:42 +0300

    update

--- .scripts/monitor-setup-laptop.sh
+++ .scripts/monitor-setup-laptop.sh
@@ -1,16 +1,3 @@
 #!/bin/zsh
-
 source /home/eugene/.env
-
-MONITOR_LAPTOP=eDP-1
-
-echo "`date` enabling laptop monitor $MONITOR_LAPTOP" >> $LOG_PATH
-xrandr --output $MONITOR_LAPTOP --auto
-
-# Turn off all connected non-laptop monitors
-for monitor in $(xrandr | grep ' connected' | grep -v "^${MONITOR_LAPTOP} " | awk '{print $1}'); do
-    echo "`date` disconnecting monitor $monitor" >> $LOG_PATH
-    xrandr --output $monitor --off
-done
-
-$SCRIPTS_PATH/workspace-outputs.sh
+$SCRIPTS_PATH/monitor-setup.sh laptop
