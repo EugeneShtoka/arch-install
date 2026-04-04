56dbece220f72a2c2576bfddace9101b22f7b1b6
Eugene Shtoka <eshtoka@gmail.com>
2026-04-04 17:28:44 +0300

    update

---
+++ .scripts/mutt-oauth2-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+# One-time OAuth2 authorization for msmtp + goimapnotify
+# Reuses gmailieer's client_id/secret, gets https://mail.google.com/ scope
+
+CREDS=~/.local/share/mail/e.shtoka@gmail.com/.credentials.gmailieer.json
+TOKENFILE=~/.cache/mutt_oauth2/e.shtoka@gmail.com.gpg
+
+CLIENT_ID=$(python3 -c "import json; d=json.load(open('$CREDS')); print(d['client_id'])")
+CLIENT_SECRET=$(python3 -c "import json; d=json.load(open('$CREDS')); print(d['client_secret'])")
+
+python3 ~/.scripts/mutt_oauth2.py \
+  --verbose \
+  --authorize \
+  --authflow devicecode \
+  --provider google \
+  --email e.shtoka@gmail.com \
+  --client-id "$CLIENT_ID" \
+  --client-secret "$CLIENT_SECRET" \
+  "$TOKENFILE"
