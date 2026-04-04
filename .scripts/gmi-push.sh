7e886d1e3697712bea70604ffcfafb2803713ead
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 00:13:34 +0300

    update

--- .scripts/gmi-push.sh
+++ .scripts/gmi-push.sh
@@ -1,2 +1,9 @@
 #!/bin/zsh
-cd ~/.local/share/mail/e.shtoka@gmail.com && gmi push -q 2>/dev/null
+mail_dir=~/.local/share/mail/e.shtoka@gmail.com/mail
+moved=0
+for f in $mail_dir/new/*(N); do
+    mv "$f" $mail_dir/cur/"${f:t}:2,"
+    moved=1
+done
+(( moved )) && notmuch new 2>/dev/null
+(cd ~/.local/share/mail/e.shtoka@gmail.com && gmi push -q >/dev/null 2>&1) &!
