7c0d2845fec1aa9115d0faec93eeb5f5a4fb0cff
Eugene Shtoka <eshtoka@gmail.com>
2026-04-04 22:45:16 +0300

    update

--- .scripts/aliases-projects.sh
+++ .scripts/aliases-projects.sh
@@ -6,12 +6,12 @@ nvim_project() {
 	alias $name="$SCRIPTS_PATH/nvim-open-project.sh '$name' '$path'"
 }
 
-nvim_project work-be      $HOME/dev/work/exbetz-be-api
-nvim_project system       $HOME
-nvim_project nvim-config  $HOME/.config/nvim
-nvim_project i3-config    $HOME/.config/i3
-nvim_project terminal     $HOME/.config/wezterm
-nvim_project file-browser $HOME/.config/yazi
-nvim_project keyboard     $HOME/dev/zmk-config
-nvim_project figoro       $HOME/dev/figoro
-nvim_project blog         $HOME/dev/blog
+nvim_project work-be          $HOME/dev/work/exbetz-be-api
+nvim_project system           $HOME
+nvim_project nvim-config      $HOME/.config/nvim
+nvim_project i3-config        $HOME/.config/i3
+nvim_project terminal-config  $HOME/.config/wezterm
+nvim_project file-browser     $HOME/.config/yazi
+nvim_project keyboard         $HOME/dev/zmk-config
+nvim_project figoro           $HOME/dev/figoro
+nvim_project blog             $HOME/dev/blog
