75fe67872b512a5b5df0892fd11bf1ef41f6f31e
Eugene Shtoka <eshtoka@gmail.com>
2026-04-04 22:45:47 +0300

    update

--- .scripts/aliases-projects.sh
+++ .scripts/aliases-projects.sh
@@ -11,7 +11,7 @@ nvim_project system           $HOME
 nvim_project nvim-config      $HOME/.config/nvim
 nvim_project i3-config        $HOME/.config/i3
 nvim_project terminal-config  $HOME/.config/wezterm
-nvim_project file-browser     $HOME/.config/yazi
+nvim_project file-browser-config     $HOME/.config/yazi
 nvim_project keyboard         $HOME/dev/zmk-config
 nvim_project figoro           $HOME/dev/figoro
 nvim_project blog             $HOME/dev/blog
