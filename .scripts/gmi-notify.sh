17aa16b359c52fb587c74c6c7eef04dbaba3e9cb
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 16:17:19 +0300

    update

--- .scripts/gmi-notify.sh
+++ .scripts/gmi-notify.sh
@@ -4,5 +4,5 @@ latest=$(notmuch search --output=messages --limit=1 tag:inbox)
 msg=$(notmuch show --format=text "$latest")
 from=$(echo "$msg" | grep "^From:" | head -1 | cut -d' ' -f2-)
 subject=$(echo "$msg" | grep "^Subject:" | head -1 | cut -d' ' -f2-)
-body=$(echo "$msg" | sed -n '/^'$'\f''body{/,/^'$'\f''body}/p' | grep -v $'^\f' | grep -vE '^(Content-|part\{)' | grep -v '^ *$' | grep -v '(http' | tr '\n' ' ')
+body=$(echo "$msg" | sed -n '/^'$'\f''body{/,/^'$'\f''body}/p' | grep -v $'^\f' | grep -vE '^(Content-|part\{)' | grep -v '^ *$' | grep -vE 'https?://' | tr '\n' ' ')
 notify-send -i mail-unread "$subject" "$from\n$body"
