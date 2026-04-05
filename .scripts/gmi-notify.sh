d2f6b72e9e7067ef4b68b982b3476bd30a9a6e19
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 16:08:53 +0300

    update

--- .scripts/gmi-notify.sh
+++ .scripts/gmi-notify.sh
@@ -4,4 +4,5 @@ latest=$(notmuch search --output=messages --limit=1 tag:inbox)
 msg=$(notmuch show --format=text "$latest")
 from=$(echo "$msg" | grep "^From:" | head -1 | cut -d' ' -f2-)
 subject=$(echo "$msg" | grep "^Subject:" | head -1 | cut -d' ' -f2-)
-notify-send -i mail-unread "$from" "$subject"
+body=$(echo "$msg" | grep -v '^\f' | grep -vE '^(From|To|Cc|Subject|Date|Message-ID):' | grep -v '^ *$' | head -3 | tr '\n' ' ')
+notify-send -i mail-unread "$subject" "$from\n$body"
