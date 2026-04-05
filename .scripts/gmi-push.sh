4fdacee2303030e1cd14aa9524110c9b9b8b122d
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 15:48:22 +0300

    update

--- .scripts/gmi-push.sh
+++ .scripts/gmi-push.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
-account=$1
-cd ~/.local/share/mail/$account && gmi push -q >/dev/null 2>&1
+for account_dir in ~/.local/share/mail/*/; do
+    (cd $account_dir && gmi push -q >/dev/null 2>&1) &
+done
+wait
