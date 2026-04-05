c665a6859bc13717649985d61a737da51087cb7c
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 14:58:31 +0300

    update

--- .scripts/gmi-pull.sh
+++ .scripts/gmi-pull.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
-mail_dir=~/.local/share/mail/e.shtoka@gmail.com/mail
-cd ~/.local/share/mail/e.shtoka@gmail.com && gmi pull -q >/dev/null 2>&1
+account=$1
+mail_dir=~/.local/share/mail/$account/mail
+cd ~/.local/share/mail/$account && gmi pull -q >/dev/null 2>&1
 for f in $mail_dir/new/*(N); do
     mv "$f" $mail_dir/cur/"${f:t}:2,"
 done
