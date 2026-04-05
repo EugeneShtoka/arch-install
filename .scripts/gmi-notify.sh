8e8c07daebcad7ab1b5a145475474004739fb451
Eugene Shtoka <eshtoka@gmail.com>
2026-04-05 16:06:17 +0300

    update

--- .scripts/gmi-notify.sh
+++ .scripts/gmi-notify.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 latest=$(notmuch search --output=messages --limit=1 tag:inbox and tag:unread)
 [[ -z $latest ]] && exit 0
-from=$(notmuch show --format=json "$latest" | python3 -c "import sys,json; print(json.load(sys.stdin)[0][0][0]['headers']['From'])")
-subject=$(notmuch show --format=json "$latest" | python3 -c "import sys,json; print(json.load(sys.stdin)[0][0][0]['headers']['Subject'])")
+headers=$(notmuch show --format=text "$latest")
+from=${${(f)${headers[(r)From:*]}}[1]#From: }
+subject=${${(f)${headers[(r)Subject:*]}}[1]#Subject: }
 notify-send -i mail-unread "$from" "$subject"
