e36c1cca5b5d4bd44f5602a712acfbbf879aee82
Eugene Shtoka <eshtoka@gmail.com>
2026-04-06 15:10:09 +0300

    update

--- .scripts/file-manager-start.sh
+++ .scripts/file-manager-start.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
-setsid wezterm start -- yazi &>/dev/null
+if [[ $1 == --window ]]; then
+    setsid wezterm start --tab-title "Yazi" -- yazi &>/dev/null
+else
+    ~/.scripts/wezterm-focus-or-launch.sh "Yazi" yazi
+fi
