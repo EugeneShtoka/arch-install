682858e95182ada6e5172cb48dcfbc27b98b324a
Eugene Shtoka <eshtoka@gmail.com>
2026-04-12 17:57:07 +0300

    update

--- .scripts/apps-install-yay.sh
+++ .scripts/apps-install-yay.sh
@@ -131,5 +131,4 @@ $SCRIPTS_PATH/auto-yay.sh w3m
 $SCRIPTS_PATH/auto-yay.sh urlscan
 $SCRIPTS_PATH/auto-yay.sh lynx
 $SCRIPTS_PATH/auto-yay.sh libolm
-$SCRIPTS_PATH/auto-yay.sh mautrix-whatsapp-bin
 $SCRIPTS_PATH/auto-yay.sh element-desktop
