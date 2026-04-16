f9e6bf9236e94922e8192ce826fa1c8fb1d437e9
Eugene Shtoka <eshtoka@gmail.com>
2026-04-16 21:12:01 +0300

    update

--- .scripts/banking-browser.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-sudo /home/eugene/.scripts/netns-banking-setup.sh
-exec sudo ip netns exec direct sudo -u eugene \
-  env DISPLAY=:0 XAUTHORITY=$HOME/.Xauthority \
-  vivaldi --user-data-dir=$HOME/.config/vivaldi-banking --class=BankingBrowser "$@"
