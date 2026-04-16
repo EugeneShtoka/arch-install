7f1e5051bd836f5c6271bfb4734ebc30d1df1160
Eugene Shtoka <eshtoka@gmail.com>
2026-04-16 15:52:50 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -7,11 +7,6 @@ $SCRIPTS_PATH/ip-update.sh
 eval "$(ssh-agent)"
 $SCRIPTS_PATH/ssh-add-keys.sh
 
-# sleep 10
 $SCRIPTS_PATH/custom-scripts-auto-sync.sh &
-$SCRIPTS_PATH/gdrive-auto-sync.sh "$LIBRARY_PATH" "$GDRIVE_NAME":"$LIBRARY_GDRIVE_PATH" &
-$SCRIPTS_PATH/gdrive-auto-sync.sh "$MUSIC_PATH" "$GDRIVE_NAME":"$MUSIC_GDRIVE_PATH" &
-$SCRIPTS_PATH/gdrive-auto-sync.sh "$DOCUMENTS_PATH" "$GDRIVE_NAME":"$DOCUMENTS_GDRIVE_PATH" &
-$SCRIPTS_PATH/gdrive-auto-sync.sh "$DOCUMENTS_GALINA_PATH" "$GDRIVE_GALINA_NAME":"$DOCUMENTS_GALINA_GDRIVE_PATH" &
 
 wait
