22375d5e2333bf8bb801b8bc349e89c0e9e35a13
Eugene Shtoka <eshtoka@gmail.com>
2026-04-20 08:55:33 +0300

    update

--- .scripts/ssh-init.sh
+++ .scripts/ssh-init.sh
@@ -3,6 +3,7 @@
 check-ssh-agent() {
     [ -S "$SSH_AUTH_SOCK" ] && ssh-add -l &>/dev/null; [ $? -ne 2 ]
 }
+mkdir -p ~/.tmp
 export SSH_AUTH_SOCK=~/.tmp/ssh-agent.sock
 if ! check-ssh-agent; then
     rm -f ~/.tmp/ssh-agent.sock
