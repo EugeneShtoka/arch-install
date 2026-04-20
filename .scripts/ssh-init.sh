21cf5a7c76a95e8b39cbd8605713371213786ccd
Eugene Shtoka <eshtoka@gmail.com>
2026-04-20 08:53:47 +0300

    update

--- .scripts/ssh-init.sh
+++ .scripts/ssh-init.sh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
 check-ssh-agent() {
-    [ -S "$SSH_AUTH_SOCK" ] && { ssh-add -l >& /dev/null || [ $? -ne 2 ]; }
+    [ -S "$SSH_AUTH_SOCK" ] && ssh-add -l &>/dev/null; [ $? -ne 2 ]
 }
-check-ssh-agent || export SSH_AUTH_SOCK=~/.tmp/ssh-agent.sock
-check-ssh-agent || eval "$(ssh-agent -s -a ~/.tmp/ssh-agent.sock)" > /dev/null
+export SSH_AUTH_SOCK=~/.tmp/ssh-agent.sock
+if ! check-ssh-agent; then
+    rm -f ~/.tmp/ssh-agent.sock
+    eval "$(ssh-agent -s -a ~/.tmp/ssh-agent.sock)" > /dev/null
+fi
 
 $SCRIPTS_PATH/ssh-add-keys.sh
