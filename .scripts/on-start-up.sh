ab31ed8519cc9e8d75a013c6c38ae1e8ea887806
Eugene Shtoka <eshtoka@gmail.com>
2026-04-21 13:59:12 +0300

    update

--- .scripts/on-start-up.sh
+++ .scripts/on-start-up.sh
@@ -3,7 +3,8 @@
 source ~/.env
 
 echo "$(date) on-start-up $HOME $LOG_PATH $SCRIPTS_PATH" >>"$LOG_PATH"
-eval "$(ssh-agent)"
+mkdir -p ~/.tmp
+ssh-agent -a ~/.tmp/ssh-agent.sock > /dev/null
 $SCRIPTS_PATH/ssh-add-keys.sh
 
 $SCRIPTS_PATH/custom-scripts-auto-sync.sh &
