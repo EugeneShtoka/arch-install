e2b72dae3378e71233b413ee1b7c64ff63ca563a
Eugene Shtoka <eshtoka@gmail.com>
2026-04-26 17:00:11 +0300

    update

--- .scripts/clipboard-trim.sh
+++ .scripts/clipboard-trim.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 /usr/bin/xclip -o -selection clipboard \
   | /home/eugene/go/bin/clipkit --collapse --strip-borders --strip-timestamps \
-  | perl -0 -pe 's/\s+\z//' \
   | /usr/bin/xclip -i -selection clipboard \
-&& sleep 0.05 \
 && xdotool key --clearmodifiers ctrl+v
