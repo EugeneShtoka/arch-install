e1bac3a5a02beb78ccbfb1b6b384ac372feecc6a
Eugene Shtoka <eshtoka@gmail.com>
2026-05-01 13:59:19 +0300

    update

--- .scripts/music-play-list.sh
+++ .scripts/music-play-list.sh
@@ -49,5 +49,6 @@ echo $playlist_map
 if [[ -n "$choice" ]]; then
     $SCRIPTS_PATH/music-stop.sh
     pathToPlay="${playlist_map["$choice"]}"
+    echo $pathToPlay
     setsid cvlc "$pathToPlay" > /dev/null 2>&1 &
 fi
