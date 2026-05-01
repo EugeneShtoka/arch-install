c45cfa057d0f75891129020c2946150017c3389f
Eugene Shtoka <eshtoka@gmail.com>
2026-05-01 11:20:31 +0300

    update

--- .scripts/rofi-projects.sh
+++ .scripts/rofi-projects.sh
@@ -5,7 +5,7 @@ theme='style-9'
 
 projects=$(. $SCRIPTS_PATH/projects-list.sh)
 
-selected=$(echo "$projects" | cut -d'|' -f1 | $SCRIPTS_PATH/rofi-run.sh -theme ${rofi_dir}/${theme}.rasi -dmenu -matching prefix)
+selected=$(echo "$projects" | cut -d'|' -f1 | $SCRIPTS_PATH/rofi-freq.sh projects -theme ${rofi_dir}/${theme}.rasi -matching prefix)
 [[ -z $selected ]] && exit
 
 proj_path=$(echo "$projects" | awk -F'|' -v name="$selected" '$1 == name {print $2}')
