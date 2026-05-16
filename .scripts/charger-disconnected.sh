2d3ef11d40a3dbe567c0ae7dad767ac58a5af84a
Eugene Shtoka <eshtoka@gmail.com>
2026-05-16 21:42:17 +0300

    update

---
+++ .scripts/charger-disconnected.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+USR_HOME=/home/eugene
+source $USR_HOME/.env
+
+source $SCRIPTS_PATH/root-notify-send.sh
+source $SCRIPTS_PATH/battery.sh
+
+get_battery_info
+root-notify-send "Discharging $battery_level%" -i $ICONS_PATH/plug.png -r 101029
+paplay $USR_HOME/.sounds/discharging.mp3
