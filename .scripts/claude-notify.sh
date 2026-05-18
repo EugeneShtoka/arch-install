ab952c6be2399ecdcaaafdd70a70d5be17b16cce
Eugene Shtoka <eshtoka@gmail.com>
2026-05-18 11:30:04 +0300

    update

---
+++ .scripts/claude-notify.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+# Usage: claude-notify.sh <title> <body> <sound>
+# Throttles: skips if <15s since last alert
+
+THROTTLE_SECS=15
+THROTTLE_FILE=/tmp/claude-notify-last${PWD//\//_}
+
+title=$1
+body=$2
+sound=$3
+
+now=$(date +%s)
+
+if [[ -f $THROTTLE_FILE ]]; then
+    last=$(< $THROTTLE_FILE)
+    (( now - last < THROTTLE_SECS )) && exit 0
+fi
+
+echo $now > $THROTTLE_FILE
+notify-send "$title" "$body"
+paplay "$sound"
