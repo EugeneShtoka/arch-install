6e0de7c9e8995c666583292be83fc2aa6ec3c412
Eugene Shtoka <eshtoka@gmail.com>
2026-05-18 23:58:40 +0300

    update

--- .scripts/custom-scripts-auto-sync.sh
+++ .scripts/custom-scripts-auto-sync.sh
@@ -3,5 +3,16 @@
 source ~/.env
 
 echo "`date` custom-scripts-auto-sync" >> $LOG_PATH
-$SCRIPTS_PATH/custom-scripts-pull.sh
-$SCRIPTS_PATH/custom-scripts-push.sh
+$SCRIPTS_PATH/ssh-init.sh
+cd $HOME
+
+git add .
+[[ -n $(git diff --cached --name-only) ]] && git commit -m update
+
+git fetch
+git rebase origin/main
+git push
+
+chmod -R +x $SCRIPTS_PATH
+$SCRIPTS_PATH/custom-scripts-delete-license-and-readme.sh
+$SCRIPTS_PATH/zsh-reload-config.sh
