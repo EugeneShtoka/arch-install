ccf541d4cb33f9ee212ad4081767ce5562b11eba
Eugene Shtoka <eshtoka@gmail.com>
2026-06-08 15:07:04 +0300

    update

---
+++ .scripts/yt-play.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+podcast=0
+url=""
+
+for arg in "$@"; do
+  case "$arg" in
+    -p|--podcast) podcast=1 ;;
+    -*) print "Unknown flag: $arg" >&2; exit 1 ;;
+    *) url="$arg" ;;
+  esac
+done
+
+if [[ -z "$url" ]]; then
+  print "Usage: ${0:t} [-p|--podcast] <url>" >&2
+  exit 1
+fi
+
+tmpdir=$(mktemp -d /tmp/yt-play.XXXXXX)
+trap 'rm -rf "$tmpdir"' EXIT
+
+if (( podcast )); then
+  yt-dlp -x --audio-format mp3 \
+    --sponsorblock-remove all \
+    -o "$tmpdir/%(title)s.%(ext)s" \
+    "$url"
+else
+  yt-dlp \
+    --sponsorblock-remove all \
+    -o "$tmpdir/%(title)s.%(ext)s" \
+    "$url"
+fi
+
+files=("$tmpdir"/*(N))
+if (( ${#files} == 0 )); then
+  print "Download failed or produced no files." >&2
+  exit 1
+fi
+
+vlc "${files[1]}"
